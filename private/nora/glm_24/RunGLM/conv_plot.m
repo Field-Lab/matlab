d8e1fc86e7b2872b187b55c0445f4978fd58d6f3
Nora Brackbill <Nora@lovelight.stanford.edu>
2014-10-20 11:51:55 -0700

    Added a script I used to choose cells for Nishal to run CBP spike sorting on

--- private/nora/glm_24/RunGLM/conv_plot.m
+++ private/nora/glm_24/RunGLM/conv_plot.m
@@ -1,26 +1,26 @@
-cell='/OFFPar_737.mat';
-exp='2013-10-10-0/';
+cell='/OFFPar_5161.mat';
+exp='2012-08-09-3';
 
 debug=[1 2 3 4 5 7 9 11 20 30 40];
 NSEM_BPS=debug;
 WN_BPS=debug;
 
 type='NSEM_mapPRJ/';
-[StimulusPars, ~, ~, ~] = Directories_Params_v23('2012-08-09-3', 'NSEM', 'PRJ');
+[StimulusPars, ~, ~, ~] = Directories_Params_v23(exp, 'NSEM', 'PRJ');
 debug_time_NSEM=debug*StimulusPars.slv.seconds_pernovelblock;
 for i=1:length(debug)
-    load(['/Volumes/Analysis/nora/NSEM/GLM_Output/debug_fixedSP_rk1_linear_MU_PS_noCP_p8IDp8/standardparams/' type exp 'debug_blocks_' num2str(debug(i)) cell]);
+    load(['/Volumes/Analysis/nora/NSEM/GLM_Output/debug_fixedSP_rk1_linear_MU_PS_noCP_p8IDp8/standardparams/' type exp '/debug_blocks_' num2str(debug(i)) cell]);
     NSEM_BPS(i)=fittedGLM.xvalperformance.glm_normedbits;
     NSEM.TempFilter{i}=fittedGLM.linearfilters.Stimulus.time_rk1;
     NSEM.PSFilter{i}=fittedGLM.linearfilters.PostSpike.Filter;
 end
 
 type='WN_mapPRJ/';
-[StimulusPars, ~, ~, ~] = Directories_Params_v23('2012-08-09-3', 'WN', 'PRJ');
+[StimulusPars, ~, ~, ~] = Directories_Params_v23(exp, 'WN', 'PRJ');
 debug_time_WN=debug*StimulusPars.slv.seconds_pernovelblock;
 
 for i=1:length(debug)
-    load(['/Volumes/Analysis/nora/NSEM/GLM_Output/debug_fixedSP_rk1_linear_MU_PS_noCP_p8IDp8/standardparams/' type exp 'debug_blocks_' num2str(debug(i)) cell]);
+    load(['/Volumes/Analysis/nora/NSEM/GLM_Output/debug_fixedSP_rk1_linear_MU_PS_noCP_p8IDp8/standardparams/' type exp '/debug_blocks_' num2str(debug(i)) cell]);
     WN_BPS(i)=fittedGLM.xvalperformance.glm_normedbits;
     WN.TempFilter{i}=fittedGLM.linearfilters.Stimulus.time_rk1;
     WN.PSFilter{i}=fittedGLM.linearfilters.PostSpike.Filter;
@@ -39,7 +39,7 @@ colors=(1:length(debug))/length(debug);
 for i=1:length(debug)
     plot(WN.PSFilter{i}, 'Color',[1 1 1]*(1-colors(i)),'LineWidth',5*colors(i))
 end
-title('PS Filter Convergence')
+title('WN PS Filter')
 
 figure;
 hold on
@@ -47,7 +47,7 @@ colors=(1:length(debug))/length(debug);
 for i=1:length(debug)
     plot(WN.TempFilter{i}, 'Color',[1 1 1]*(1-colors(i)),'LineWidth',5*colors(i))
 end
-title('Temporal Filter Convergence')
+title('WN Temporal Filter')
 
 figure;
 hold on
@@ -55,7 +55,7 @@ colors=(1:length(debug))/length(debug);
 for i=1:length(debug)
     plot(NSEM.PSFilter{i}, 'Color',[1 1 1]*(1-colors(i)),'LineWidth',5*colors(i))
 end
-title('PS Filter Convergence')
+title('NSEM PS Filter')
 
 figure;
 hold on
@@ -63,4 +63,4 @@ colors=(1:length(debug))/length(debug);
 for i=1:length(debug)
     plot(NSEM.TempFilter{i}, 'Color',[1 1 1]*(1-colors(i)),'LineWidth',5*colors(i))
 end
-title('Temporal Filter Convergence')
+title('NSEM Temporal Filter')
