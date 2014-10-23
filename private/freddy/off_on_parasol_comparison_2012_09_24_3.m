6903ec4a620a6e5ba183ab370706ee84cc561486
Frederick Kellison-Linn <fred.kl@me.com>
2014-10-23 11:18:45 -0700

    add new analysis script

---
+++ private/freddy/off_on_parasol_comparison_2012_09_24_3.m
@@ -0,0 +1,10 @@
+on_parasol = [2 33 168 197 229 303 332 436 601 647 919 4909 6603 6633 6769 ...
+    6936 6995 7008 7173 7218 7474 7507 7549];
+
+off_parasol = [212 421 903 5568 6605 7052 7639 7532];
+
+somaStimThreshs_bielec1_on = zeros(size(on_parasol,2),6);
+somaStimThreshs_singleelec_on = zeros(size(on_parasol,2),6);
+
+somaStimThreshs_bielec1_off = zeros(size(on_parasol,2),6);
+somaStimThreshs_singleelec_off = zeros(size(on_parasol,2),6);
