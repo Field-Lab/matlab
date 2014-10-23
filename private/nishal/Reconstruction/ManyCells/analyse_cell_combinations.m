36addae77f1a2ae19230e056cb33f7dc332236d0
Vision <vision@bertha.stanford.edu>
2014-10-23 12:35:27 -0700

    Some plot changes in reconstruciton work.

--- private/nishal/Reconstruction/ManyCells/analyse_cell_combinations.m
+++ private/nishal/Reconstruction/ManyCells/analyse_cell_combinations.m
@@ -1,14 +1,23 @@
 On = load('/Volumes/Analysis/nishal/recons_WN_whole_ON2.mat');
+On2 = load('/Volumes/Analysis/nishal/recons_WN_whole_ON_nobias.mat');
 Off = load('/Volumes/Analysis/nishal/recons_WN_whole_OFF.mat');
+Off2 = load('/Volumes/Analysis/nishal/recons_WN_whole_OFF_nobias.mat');
 OnOff  = load('/Volumes/Analysis/nishal/recons_WN_whole_PARASOL.mat');
+OnOff2 = load('/Volumes/Analysis/nishal/recons_WN_whole_OnOFF_nobias.mat');
+
+
+%%
 recons_idx=[18000:21000]; % Doubt 
 
 h=fspecial('gaussian',4,2);
 
-figure
+
 for idx=recons_idx
     idx
-subplot(3,3,1);
+close all
+figure
+
+subplot(4,3,1);
 imagesc(On.mov_recons_full(:,:,idx));
 colormap gray
 axis image
@@ -16,7 +25,7 @@ caxis([-0.5,0.5]);
 colorbar
 %title('Spatial')
 
-subplot(3,3,2);
+subplot(4,3,2);
 imagesc(Off.mov_recons_full(:,:,idx));
 colormap gray
 caxis([-0.5,0.5]);
@@ -25,14 +34,14 @@ axis image
 
 
 
-subplot(3,3,3);
+subplot(4,3,3);
 imagesc(OnOff.mov_recons_full(:,:,idx));
 colormap gray
 axis image
 caxis([-0.5,0.5]);
 colorbar
 
-subplot(3,3,4);
+subplot(4,3,4);
 imagesc(On.mov_pred_full(:,:,idx));
 colormap gray
 caxis([-0.5,0.5]);
@@ -43,7 +52,7 @@ b=On.mov_pred_full(:,:,idx);
 co=corr(a(:),b(:));
 title(sprintf('Corr: %f',co));
 
-subplot(3,3,5);
+subplot(4,3,5);
 imagesc(Off.mov_pred_full(:,:,idx));
 colormap gray
 axis image
@@ -54,7 +63,7 @@ b=Off.mov_pred_full(:,:,idx);
 co=corr(a(:),b(:));
 title(sprintf('Corr: %f',co));
 
-subplot(3,3,6)
+subplot(4,3,6)
 imagesc(OnOff.mov_pred_full(:,:,idx));
 colormap gray
 axis image
@@ -65,7 +74,7 @@ b=OnOff.mov_pred_full(:,:,idx);
 co=corr(a(:),b(:));
 title(sprintf('Corr: %f',co));
 
-subplot(3,3,7)
+subplot(4,3,7)
 ppxx=imfilter(On.mov_recons_full(:,:,idx),h,'same').*(On.mov_recons_full(:,:,idx)~=0);
 imagesc(ppxx)
 colormap gray
@@ -77,7 +86,7 @@ b=On.mov_pred_full(:,:,idx);
 co=corr(a(:),b(:));
 title(sprintf('Corr: %f',co));
 
-subplot(3,3,8)
+subplot(4,3,8)
 ppxx=imfilter(Off.mov_recons_full(:,:,idx),h,'same').*(Off.mov_recons_full(:,:,idx)~=0);
 imagesc(ppxx)
 colormap gray
@@ -89,7 +98,7 @@ b=Off.mov_pred_full(:,:,idx);
 co=corr(a(:),b(:));
 title(sprintf('Corr: %f',co));
 
-subplot(3,3,9)
+subplot(4,3,9)
 ppxx=imfilter(OnOff.mov_recons_full(:,:,idx),h,'same').*(OnOff.mov_recons_full(:,:,idx)~=0);
 imagesc(ppxx)
 colormap gray
@@ -100,6 +109,41 @@ a=ppxx(:);
 b=OnOff.mov_pred_full(:,:,idx);
 co=corr(a(:),b(:));
 title(sprintf('Corr: %f',co));
+
+subplot(4,3,10);
+imagesc(On2.mov_pred_full(:,:,idx));
+colormap gray
+caxis([-0.5,0.5]);
+colorbar
+axis image
+a=imfilter(On.mov_recons_full(:,:,idx),h,'same').*(On.mov_recons_full(:,:,idx)~=0);
+b=On2.mov_pred_full(:,:,idx);
+co=corr(a(:),b(:));
+title(sprintf('Corr: %f',co));
+
+subplot(4,3,11);
+imagesc(Off2.mov_pred_full(:,:,idx));
+colormap gray
+axis image
+caxis([-0.5,0.5]);
+colorbar
+a=imfilter(Off.mov_recons_full(:,:,idx),h,'same').*(On.mov_recons_full(:,:,idx)~=0);
+b=Off2.mov_pred_full(:,:,idx);
+co=corr(a(:),b(:));
+title(sprintf('Corr: %f',co));
+
+
+subplot(4,3,12)
+imagesc(OnOff2.mov_pred_full(:,:,idx));
+colormap gray
+axis image
+caxis([-0.5,0.5]);
+colorbar
+a=imfilter(OnOff.mov_recons_full(:,:,idx),h,'same').*(On.mov_recons_full(:,:,idx)~=0);
+b=OnOff2.mov_pred_full(:,:,idx);
+co=corr(a(:),b(:));
+title(sprintf('Corr: %f',co));
+
 
 
 pause
