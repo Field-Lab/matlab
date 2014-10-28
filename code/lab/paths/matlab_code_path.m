4758439e87b3062c8642744a99dde3fffb3ee809
vision <vision@brokedown.stanford.edu>
2014-10-28 15:00:57 -0700

    Update shared paths

--- code/lab/paths/matlab_code_path.m
+++ code/lab/paths/matlab_code_path.m
@@ -5,6 +5,7 @@ function the_path = matlab_code_path
 %
 %
 % 2010-01  gauthier
-%
+% 2014-10  grosberg - transition to git file 
 
-the_path = '~/Dropbox/Lab/Development/matlab-standard/';
+% the_path = '~/Dropbox/Lab/Development/matlab-standard/';
+the_path = fullfile(mfilename('fullpath'),'../../../../');
