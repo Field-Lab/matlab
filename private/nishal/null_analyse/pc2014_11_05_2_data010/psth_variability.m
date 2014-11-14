176e286db69f83b44935b8b1309ad198552abca9
vision <vision@tennessee.stanford.edu>
2014-11-14 02:59:03 -0800

    Further null analysis

--- private/nishal/null_analyse/pc2014_11_05_2_data010/psth_variability.m
+++ private/nishal/null_analyse/pc2014_11_05_2_data010/psth_variability.m
@@ -1,7 +1,7 @@
 
 
 % RE FORMAT spkCondColl
-
+movie_time=size(condMovies{1},1);
 spkCondCollformat(4).spksColl=[];
 for icond=1:4
     spksColl=zeros(movie_time,nTrials);
@@ -25,19 +25,49 @@ psthBinSize=10;
 psthSmoothen=5;
 for icond=1:4
 [timeLogData{icond},psthData{icond}]=  psth_calc(( spkCondCollformat(icond).spksColl),psthBinSize,'nonoverlap');
-[timeLogModel{icond},psthModel{icond}]=  psth_calc(( spkCondCollModel(icond).spksColl),psthBinSize,'nonoverlap');
-psthModel{icond}=conv(psthModel{icond},(1/psthSmoothen)*ones(psthSmoothen,1),'same');
 psthData{icond}=conv(psthData{icond},(1/psthSmoothen)*ones(psthSmoothen,1),'same');
+
+%[timeLogModel{icond},psthModel{icond}]=  psth_calc(( spkCondCollModel(icond).spksColl),psthBinSize,'nonoverlap');
+%psthModel{icond}=conv(psthModel{icond},(1/psthSmoothen)*ones(psthSmoothen,1),'same');
+
 end
 
-figure;
-for icond=1:4
-    subplot(4,1,icond);
-    plot(timeLogData{icond},psthData{icond},'b');
-    hold on
-    plot(timeLogModel{icond},psthModel{icond},'r');
-    ylim([0,1.2*max(psthData{1})])
-    title(sprintf('PSTH var: Data %f: LNP Model: %f',var(psthData{icond}),var(psthModel{icond})));
-    legend('Data','LNP Model');
-   
+% figure;
+% for icond=1:4
+%     subplot(4,1,icond);
+%     plot(timeLogData{icond},psthData{icond},'b');
+%     hold on
+%     plot(timeLogModel{icond},psthModel{icond},'r');
+%     ylim([0,1.2*max(psthData{1})])
+%     title(sprintf('PSTH var: Data %f: LNP Model: %f',var(psthData{icond}),var(psthModel{icond})));
+%     legend('Data','LNP Model');
+%    
+% end
+
+%%
+% PSTH plots
+col='mbkg';
+figure('Color','w');
+
+subplot(2,1,1);
+for icond=1:nConditions
+
+xPoints = spkCondColl(icond).xPoints;
+yPoints = spkCondColl(icond).yPoints;
+nTrials1=max(yPoints(:));
+plot(xPoints/20000, yPoints+(nConditions-icond)*nTrials1,col(icond));
+hold on
+ylim([0,nConditions*nTrials]);
+xlim([0,12]);
+title(sprintf('%s: data0009 vis ID: %d, Avg Spk rates (%0.02f,%0.02f,%0.02f) spks/sec',cond_str{icond},InterestingCell_vis_id(ref_cell_number),spkCondColl(1).avgSpkRate,spkCondColl(2).avgSpkRate,spkCondColl(3).avgSpkRate));
 end
+
+subplot(2,1,2);
+for icond=1:3
+    plot(timeLogData{icond}/120,psthData{icond},col(icond));
+    hold on
+    ylim([0,1.2*max(psthData{1})])
+end
+xlabel('Time in Seconds');
+title(sprintf('PSTHs Cell %d',InterestingCell_vis_id(ref_cell_number)));
+legend('Original','Null for On Parasol','Null for Off Parasol','Location','best');
