772f667fe351e521b5fd3d178ae79ec2b4c2a132
Vision <vision@bertha.stanford.edu>
2014-11-25 23:31:18 -0800

    On seconds day of experiment. Tuesday, 11:31PM, Nov 25

---
+++ private/nishal/null_analyse/startup_null_analyse_bertha.m
@@ -0,0 +1,18 @@
+%% Startup file for defining matlab and java paths
+disp('Running startup file Bertha...');
+
+% Java library
+javaaddpath('/Volumes/Lab/Development/vision7/Vision.app/Contents/Resources/Java/Vision.jar');
+
+% matlab code paths
+addpath(genpath('~/Nishal/matlab/code'));
+
+addpath(genpath('~/Nishal/matlab/private/lauren/MATLAB_code')); 
+
+
+
+addpath(genpath('~/Nishal/matlab/private/nishal/plotSpikeRaster_v1'));
+
+% set some default plot 
+set(0, 'DefaultAxesFontSize', 18, 'DefaultAxesFontName', 'Helvetica')
+
