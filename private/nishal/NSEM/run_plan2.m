72837be9932b9d4d967cf3202ec1f73bb75aa52a
vision <vision@tennessee.stanford.edu>
2014-12-02 18:40:05 -0800

    CBP on Dec 2

---
+++ private/nishal/NSEM/run_plan2.m
@@ -0,0 +1,76 @@
+
+
+
+startup_analyse_tenessee
+%%
+
+
+dataset='2014-11-24-0';
+
+%analysis_datafile = sprintf('%s/data001-from-data000/data001-from-data000',dataset);
+analysis_datafile = sprintf('%s/data000/data000',dataset);
+imov=4;
+bin_datafile=sprintf('/Volumes/Data/%s/data00%d',dataset,imov);
+
+vision_id=1234;
+
+no_cells=5;
+noise=0.25;
+
+run_ss
+
+clear all
+
+%%
+
+dataset='2014-11-24-0';
+
+%analysis_datafile = sprintf('%s/data001-from-data000/data001-from-data000',dataset);
+analysis_datafile = sprintf('%s/data000/data000',dataset);
+imov=4;
+bin_datafile=sprintf('/Volumes/Data/%s/data00%d',dataset,imov);
+
+vision_id=2314;
+
+no_cells=5;
+noise=0.25;
+
+run_ss
+
+clear all
+
+%%
+
+dataset='2014-11-24-0';
+
+%analysis_datafile = sprintf('%s/data001-from-data000/data001-from-data000',dataset);
+analysis_datafile = sprintf('%s/data000/data000',dataset);
+imov=4;
+bin_datafile=sprintf('/Volumes/Data/%s/data00%d',dataset,imov);
+
+vision_id=1039;
+
+no_cells=5;
+noise=0.25;
+
+run_ss
+
+clear all
+
+%%
+
+dataset='2014-11-24-0';
+
+%analysis_datafile = sprintf('%s/data001-from-data000/data001-from-data000',dataset);
+analysis_datafile = sprintf('%s/data000/data000',dataset);
+imov=4;
+bin_datafile=sprintf('/Volumes/Data/%s/data00%d',dataset,imov);
+
+vision_id=1621;
+
+no_cells=5;
+noise=0.25;
+
+run_ss
+
+clear all
