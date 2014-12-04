f72fbaa6d76d1d3f5f36f15dffcd2244fff01a93
Colleen Rhoades <crhoades227@gmail.com>
2014-12-04 12:53:22 -0800

    Plotting mean graphs

--- private/colleen/Motion Computation Code/Figure Code/graphSpeed.m
+++ private/colleen/Motion Computation Code/Figure Code/graphSpeed.m
@@ -1,4 +1,4 @@
-function graphSpeed(stdOnP, stdOffP, stdOnM, stdOffM, delta, tag)
+function graphSpeed(meanOnP, meanOffP, meanOnM, meanOffM,stdOnP, stdOffP, stdOnM, stdOffM, delta, tag)
 axes_lim = [0 0.55];
 hFig = figure;
 % set(hFig, 'Position', [680 680 600*1.5 435*1.5])
@@ -50,9 +50,7 @@ hFig = figure;
 
 
 % subplot_c(2,3,[1 3])
-title('Low Speed Variablity')
-xlabel('OFF Parasol STD/Speed')
-ylabel('ON Midget STD/Speed')
+
 plot(2.95*ones(size(stdOnP(delta==192))), stdOnM(delta==192), 'ko', 'MarkerFaceColor','g')
 hold on
 plot(2*ones(size(stdOnP(delta==96))), stdOffP(delta==96), 'ko', 'MarkerFaceColor','r')
@@ -93,5 +91,92 @@ title({'Variability in Speed Estimate in Different Cell Types';tag});
 
 s = sprintf('%c', char(176));
 legend(['Speed of 42.6' s '/sec'],['Speed of 21.3' s '/sec'], ['Speed of 10.7' s '/sec'], 'location', 'northwest')
+
+
+%% Mean
+
+figure
+ set(gcf, 'Position', [280 216 1238 500]);
+subplot(1,3,1)
+plot(meanOnP(delta == 48), ones(size(meanOnP(delta == 48))), 'ko', 'MarkerFaceColor', 'r')
+hold on
+plot(meanOffP(delta == 48), 1.1*ones(size(meanOffP(delta == 48))), 'ko', 'MarkerFaceColor', 'b')
+plot(meanOnM(delta == 48), 1.2*ones(size(meanOnM(delta == 48))), 'ko', 'MarkerFaceColor', 'g')
+plot(10.7,0.9, 'ko', 'MarkerFaceColor', 'k')
+
+set(gca, 'ytick' ,[0.9,1,1.1,1.2]);
+set(gca, 'yticklabel', {'True Speed','ON Parasol', 'OFF Parasol', 'ON Midget'})
+ylim([0.85 1.25])
+xlim([10.4, 11.1])
+
+subplot(1,3,2)
+plot(meanOnP(delta == 96), ones(size(meanOnP(delta == 96))), 'ko', 'MarkerFaceColor', 'r')
+hold on
+plot(meanOffP(delta == 96), 1.1*ones(size(meanOffP(delta == 96))), 'ko', 'MarkerFaceColor', 'b')
+plot(meanOnM(delta == 96), 1.2*ones(size(meanOnM(delta == 96))), 'ko', 'MarkerFaceColor', 'g')
+plot(21.3,0.9, 'ko', 'MarkerFaceColor', 'k')
+ylim([0.85 1.25])
+set(gca, 'ytick' ,[]);
+s = sprintf('%c', char(176));
+xlabel(['Speed Estimates (', s, '/sec)'])
+xlim([20 22.5])
+
+subplot(1,3,3)
+plot(meanOnP(delta == 192), ones(size(meanOnP(delta == 192))), 'ko', 'MarkerFaceColor', 'r')
+hold on
+plot(meanOffP(delta == 192), 1.1*ones(size(meanOffP(delta == 192))), 'ko', 'MarkerFaceColor', 'b')
+plot(meanOnM(delta == 192), 1.2*ones(size(meanOnM(delta == 192))), 'ko', 'MarkerFaceColor', 'g')
+plot(42.6,0.9, 'ko', 'MarkerFaceColor', 'k')
+set(gca, 'ytick' ,[]);
+xlim([39.1, 46.1])
+
+ylim([0.85 1.25])
+% xlim([40, 220])
+set(gcf,'color','w');
+suptitle('Speed Estimates Of Different Cell Types');
+
+%% Equal Scale
+figure
+ set(gcf, 'Position', [280 216 1238 500]);
+subplot(1,3,1)
+plot(meanOnP(delta == 48), ones(size(meanOnP(delta == 48))), 'ko', 'MarkerFaceColor', 'r')
+hold on
+plot(meanOffP(delta == 48), 1.1*ones(size(meanOffP(delta == 48))), 'ko', 'MarkerFaceColor', 'b')
+plot(meanOnM(delta == 48), 1.2*ones(size(meanOnM(delta == 48))), 'ko', 'MarkerFaceColor', 'g')
+plot(10.7,0.9, 'ko', 'MarkerFaceColor', 'k')
+
+set(gca, 'ytick' ,[0.9,1,1.1,1.2]);
+set(gca, 'yticklabel', {'True Speed','ON Parasol', 'OFF Parasol', 'ON Midget'})
+ylim([0.85 1.25])
+xlim([7.2, 14.2])
+
+subplot(1,3,2)
+plot(meanOnP(delta == 96), ones(size(meanOnP(delta == 96))), 'ko', 'MarkerFaceColor', 'r')
+hold on
+plot(meanOffP(delta == 96), 1.1*ones(size(meanOffP(delta == 96))), 'ko', 'MarkerFaceColor', 'b')
+plot(meanOnM(delta == 96), 1.2*ones(size(meanOnM(delta == 96))), 'ko', 'MarkerFaceColor', 'g')
+plot(21.3,0.9, 'ko', 'MarkerFaceColor', 'k')
+ylim([0.85 1.25])
+set(gca, 'ytick' ,[]);
+s = sprintf('%c', char(176));
+xlabel(['Speed Estimates (', s, '/sec)'])
+xlim([17.8, 24.8])
+
+subplot(1,3,3)
+plot(meanOnP(delta == 192), ones(size(meanOnP(delta == 192))), 'ko', 'MarkerFaceColor', 'r')
+hold on
+plot(meanOffP(delta == 192), 1.1*ones(size(meanOffP(delta == 192))), 'ko', 'MarkerFaceColor', 'b')
+plot(meanOnM(delta == 192), 1.2*ones(size(meanOnM(delta == 192))), 'ko', 'MarkerFaceColor', 'g')
+plot(42.6,0.9, 'ko', 'MarkerFaceColor', 'k')
+set(gca, 'ytick' ,[]);
+xlim([39.1, 46.1])
+
+ylim([0.85 1.25])
+% xlim([40, 220])
+set(gcf,'color','w');
+suptitle('Speed Estimates Shown with Equal X Scaling');
+
+
+
 
 
