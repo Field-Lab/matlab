114601296c81c96cce6ae7a4dd04aca1b2292fac
Nora Brackbill <nbrackbill@gmail.com>
2014-12-05 12:28:42 -0800

    Major timing issues. Hopefully they're fixed now.

--- private/nora/RGB_triplet_test.m
+++ private/nora/RGB_triplet_test.m
@@ -23,3 +23,47 @@ RGB=[datarun.vision.timecourses(cell).r ...
 
 [U,S,V]=svd(RGB);
 RGB_SVD=V(:,1);
+
+%% Spikes times in terms of frames
+
+spikes=datarun.spikes{2};
+spikes_frame=zeros(size(spikes));
+for i=1:length(spikes)
+    spikes_frame(i)=find(t_frame==max(t_frame(t_frame<spikes(i))));
+end
+
+
+%% BW STA
+
+STA_BW=zeros(32,32,30);
+for j=1:length(spikes_frame)
+    frame_sp=spikes_frame(j);
+    if frame_sp>=30 && frame_sp<216000
+        STA_BW=STA_BW+fitmovie(:,:,(frame_sp-29):frame_sp);
+    end
+end
+STA_BW=STA_BW-min(STA_BW(:));
+STA_BW=STA_BW/max(abs(STA_BW(:)));
+for i=1:30 
+    imagesc(STA_BW(:,:,i)); 
+    colormap gray; 
+    axis image; 
+    pause(0.1); 
+end
+
+%% COLOR STA
+
+STA_color=zeros(32,32,3,30);
+for j=1:length(spikes_frame)
+    frame_sp=spikes_frame(j);
+    if frame_sp>=30 && frame_sp<216000
+        STA_color=STA_color+fitmovie_color(:,:,:,(frame_sp-29):frame_sp);
+    end
+end
+STA_color=STA_color-min(STA_color(:));
+STA_color=STA_color/max(STA_color(:));
+for i=1:30
+    imagesc(STA_color(:,:,:,i));  
+    axis image; 
+    pause(0.1); 
+end
