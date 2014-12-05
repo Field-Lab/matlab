0f8c6589ddb34795d4436fb5614b806510635e6e
Colleen Rhoades <crhoades227@gmail.com>
2014-12-05 14:09:02 -0800

    organizing

--- private/colleen/Motion Computation Code/Efficient Motion Comp/accelerationModel.m
+++ private/colleen/Motion Computation Code/Efficient Motion Comp/accelerationModel.m
@@ -1,14 +1,6 @@
 % acceleration
 noise = random('Normal',0,1,[1000,1])
-x_raw = linspace(1,10,1000);
+x_raw = linspace(1,24,1000);
 
-y = (x_raw'+ noise).^2;
-figure;
-plot(y,x_raw', '.')
-set(gcf, 'color', 'w')
+y = x.^2;
 
-extra_noise = 10*rand([500,1])
-hold on
-y = y(1 : 2 : end);  % => 1 4 7 10
-plot(y,extra_noise, '.')
-axis off
