c7a562ba30bd831fe27a9a717e2e98be096f42a6
Nora Brackbill <nbrackbill@gmail.com>
2014-12-05 17:16:52 -0800

    Found another issue relating to the indices for adjusting the spikes times

--- private/nora/RGB_triplet_test.m
+++ private/nora/RGB_triplet_test.m
@@ -25,19 +25,34 @@ RGB=[datarun.vision.timecourses(cell).r ...
 RGB_SVD=V(:,1);
 
 %% Spikes times in terms of frames
-
+t_frame = t_frame_interpAH(datarun.triggers);
 spikes=datarun.spikes{2};
 spikes_frame=zeros(size(spikes));
 for i=1:length(spikes)
     spikes_frame(i)=find(t_frame==max(t_frame(t_frame<spikes(i))));
 end
 
+%% How I do it in the code
+
+spikes=datarun.spikes{2};
+spikes_adj=spikes;
+n_block=0;
+for i=1:(length(datarun.triggers)-1)
+    actual_t_start=datarun.triggers(i);
+    supposed_t_start=n_block*100/120;
+    idx1=spikes > actual_t_start;
+    idx2=spikes < datarun.triggers(i+1);
+    spikes_adj(find(idx2.*idx1))=spikes(find(idx2.*idx1))+supposed_t_start-actual_t_start;
+    n_block=n_block+1;
+end
+spikes_frame_2=floor(spikes_adj*120);
+
 
 %% BW STA
 
 STA_BW=zeros(32,32,30);
-for j=1:length(spikes_frame)
-    frame_sp=spikes_frame(j);
+for j=1:length(spikes_frame_2)
+    frame_sp=spikes_frame_2(j);
     if frame_sp>=30 && frame_sp<216000
         STA_BW=STA_BW+fitmovie(:,:,(frame_sp-29):frame_sp);
     end
