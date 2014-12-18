39c5126a90e3cc60e972f090e42fca4da440bb00
Nora Brackbill <nbrackbill@gmail.com>
2014-12-18 12:35:27 -0800

    Tested prediction versus original code and it matched

---
+++ private/nora/GLM_scripts/Tests/GLM_prediction_test_script.m
@@ -0,0 +1,13 @@
+%% Load the GLM fit
+load('/Volumes/Analysis/nora/NSEM/GLM_Output/old_fits/rk1_MU_PS_noCP_p8IDp8/standardparams/NSEM_mapPRJ/2012-08-09-3/OFFPar_1471.mat');
+
+%% Load the datarun to compare to
+datarun=load_data('2012-08-09-3/data005');
+datarun=load_neurons(datarun);
+
+%% Load the movie to make predictions for
+[testmovie] = loadmoviematfile(fittedGLM.cellinfo.exp_nm , 'NSEM', '8pix_Identity_8pix','testmovie');
+
+%% Evaluate
+x=nishal_test(fittedGLM, datarun, testmovie{1,1}.matrix, 61);
+plotraster(x,fittedGLM,1,0,1)
