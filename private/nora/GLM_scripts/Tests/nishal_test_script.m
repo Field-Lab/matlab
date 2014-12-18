437759d22854c6f86c8f0dc85e0022c30fb779d1
Nora Brackbill <nbrackbill@gmail.com>
2014-12-18 12:03:06 -0800

    I did a couple tests on the GLM prediction code and it seems fine.
    I am going to wait until Nishal figures out the cell numbers thing before I
    keep working on it because right now its a bit of a mess.

--- private/nora/GLM_scripts/Tests/nishal_test_script.m
+++ private/nora/GLM_scripts/Tests/nishal_test_script.m
@@ -1,15 +1,15 @@
-%% Loading
-load('/Volumes/Analysis/nora/nishal_glmfits/15min/316.mat');
+%% Load the GLM fit
+load('/Volumes/Analysis/nora/nishal_glmfits/30min/7742.mat');
 
-%%
+%% Load the datarun to compare to
 datarun=load_data('2014-11-05-2/data010_from_data009_nps');
 datarun=load_neurons(datarun);
 
-%%
+%% Load the movie to make predictions for
 testmovie_filename='/Volumes/Data/2014-11-05-2/visual/18.rawMovie';
 testmovie=get_rawmovie(testmovie_filename,5760);
 testmovie=permute(testmovie,[2 3 1]);
 
-%% Eval
+%% Evaluate
 x=nishal_test(fittedGLM, datarun, testmovie, 30);
 plotraster(x,fittedGLM,1,0,1)
