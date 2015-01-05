888ccfbe200286da51e6411bf1420f7c2793ee8a
Nora <nbrackbill@gmail.com>
2015-01-05 13:41:02 -0800

    Made usable GLM prediction code for rest of lab

--- private/nora/GLM_scripts/Figures/plotraster.m
+++ private/nora/GLM_scripts/Figures/plotraster.m
@@ -1,13 +1,31 @@
-function plotraster(xval, fittedGLM, labels, separate, PSTH)
+function plotraster(xval, fittedGLM, varargin)
+% Optional Paremeters
+% raster_length in seconds
+% labels, true or false, whether to label the axes
+% separate, true or false, whether to plot rasters in the same figure
+% PSTH, true or false, whether to plot the PSTH
+
+p=inputParser;
+addRequired(p,'xval',@isstruct);
+addRequired(p,'fittedGLM', @isstruct);
+addParameter(p,'raster_length',10, @isnumeric);
+addParameter(p,'labels',false, @islogical);
+addParameter(p,'separate',false, @islogical);
+addParameter(p,'PSTH',true, @islogical);
+parse(p,xval, fittedGLM,varargin{:});
 
 dt = fittedGLM.t_bin;
-bins     = 120 * 48 * fittedGLM.bins_per_frame;
+bins     = fittedGLM.GLMPars.approx_refresh_hz * p.Results.raster_length * fittedGLM.bins_per_frame;
 time     = dt*[1:bins];
-rec_rast = xval.rasters.recorded(:,1:bins);
-sim_rast = xval.rasters.glm_sim(:,1:bins);
+try
+    rec_rast = xval.rasters.recorded(:,1:bins);
+    sim_rast = xval.rasters.glm_sim(:,1:bins);
+catch
+    error('The rasters are not as long as the raster_length parameter')
+end
 trials   = size(rec_rast,1);
 
-if separate
+if p.Results.separate
     hFig1=figure;
     set(hFig1, 'Position', [100 100 800 250])
     hold on
@@ -16,7 +34,7 @@ if separate
         plot(rec1, i_trial, 'k.')
     end
     ylim([1 trials])
-    if labels
+    if p.Results.labels
         xlabel('Time (seconds)')
         ylabel('Trials')
     else
@@ -33,7 +51,7 @@ if separate
         end
     end
     ylim([1 trials])
-    if labels
+    if p.Results.labels
         xlabel('Time (seconds)')
         ylabel('Trials')
     else
@@ -42,10 +60,9 @@ if separate
     
 else
     
-
     hFig=figure;
     
-    if PSTH
+    if p.Results.PSTH
         set(hFig, 'Position', [100 100 1000 500])
         subplot(2,1,1)
     else
@@ -67,13 +84,13 @@ else
     ylim([1 trials*2])
     xlim([time(1) time(end)])
     
-    if labels
+    if p.Results.labels
         xlabel('Time (seconds)')
         set(gca,'YTickLabel',{'1','Data',num2str(trials),'Model'},'YTick',[1 trials/2 trials 3*trials/2])
         ylabel('Trials')
     end
    
-    if PSTH
+    if p.Results.PSTH
         runtrials=find(runawaytrial);
         N=length(runtrials);
         convolve=150;
