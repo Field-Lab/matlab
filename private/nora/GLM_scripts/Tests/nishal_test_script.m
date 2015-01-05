7c52888cb86aaf16a3ed2e200b292986f03f6b7e
Nora <nbrackbill@gmail.com>
2015-01-05 09:51:07 -0800

    Commit before starting new year work

--- private/nora/GLM_scripts/Tests/nishal_test_script.m
+++ private/nora/GLM_scripts/Tests/nishal_test_script.m
@@ -1,6 +1,3 @@
-%% Load the GLM fit
-load('/Volumes/Analysis/nora/nishal_glmfits/30min/7742.mat');
-
 %% Load the datarun to compare to
 datarun=load_data('2014-11-05-2/data010_from_data009_nps');
 datarun=load_neurons(datarun);
@@ -10,6 +7,7 @@ testmovie_filename='/Volumes/Data/2014-11-05-2/visual/18.rawMovie';
 testmovie=get_rawmovie(testmovie_filename,5760);
 testmovie=permute(testmovie,[2 3 1]);
 
-%% Evaluate
+%% Evaluate and Load the GLM fit
+load('/Volumes/Analysis/nora/nishal_glmfits/15min/7726.mat');
 x=nishal_test(fittedGLM, datarun, testmovie, 30);
 plotraster(x,fittedGLM,1,0,1)
