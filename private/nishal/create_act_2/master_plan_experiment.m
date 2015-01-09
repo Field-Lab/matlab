d85c23e1589bb409d47b0e63859d4676406b71db
Vision <vision@bertha.stanford.edu>
2015-01-09 03:57:28 -0800

    Spatial nulling code added

--- private/nishal/create_act_2/master_plan_experiment.m
+++ private/nishal/create_act_2/master_plan_experiment.m
@@ -25,6 +25,26 @@ dest_raw='/Volumes/Data/stimuli/movies/null_space/';
 movies=cell(20,1);
 % 'solver' .. 1 for LSQR, 2 for CRAIG, 3 for Fourier 
 
+%% Spatial nulling
+cell_params=struct();
+cell_params.type_name_inp='nc3';%'userCellList';
+cell_params.cell_list=[]%[3888,2825,1820,4129, 5346,5671,5161,1278, 3828,3574,4036,3572, 503,560,797,1009,487,181,901]; % if type_name_inp = 'userCellList' 
+cell_params.use_fits=2;
+cell_params.STAlen=14;
+
+mov_params=struct();
+mov_params.mov_type='bw-precomputed';
+mov_params.movie_time=120*10;
+mov_params.mean=0.5*255;
+mov_params.deviation=0.48*255;
+mov_params.scaling_loss=0.01; % a number in [0,1], fraction of values that is changed by scaling.
+
+
+solver=4; % Solver 4 used for spatial nulling!
+[mov_orignial,mov_modify_new]=null_space_movie2(datafile,cell_params,mov_params,solver);
+
+
+
 %% BW - 10 sec movie cell type
 % global_vars2
 % var64=64;
@@ -69,6 +89,7 @@ mov_params.scaling_loss=0.05; % a number in [0,1], fraction of values that is ch
 
 solver=3;
 [mov_orignial,mov_modify_new]=null_space_movie2(datafile,cell_params,mov_params,solver);
+
 %%
 cell_params=struct();
 cell_params.type_name_inp='userCellList';
