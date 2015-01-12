027d4422b0c741211313ae8c559177e86e3ee711
Nishal Shah <bhaishahster@gmail.com>
2015-01-12 05:20:58 -0800

    Copied the code/GLMpredict from master and pasted and that way, updated the code that Nora edited into my branch..

--- code/projects/GLM_predict/example_script.m
+++ code/projects/GLM_predict/example_script.m
@@ -1,24 +1,23 @@
 %% add path to nora's folder for GLM code
-location_of_git_repo='/home/vision/Nishal/matlab/';
+location_of_git_repo='/Users/Nora/Documents/MATLAB/matlab';
 addpath(genpath([location_of_git_repo '/private/nora']));
 
-% Java library
-javaaddpath('/Volumes/Lab/Development/vision7/Vision.app/Contents/Resources/Java/Vision.jar');
-
 %% Fit the GLM from a classification run or other white noise run
-fittedGLM=glm_fit_from_WNrun({316}, '2014-11-05-2/data009_nps', 'RGB-10-2-0.48-11111-32x32', 900, '~/Nishal/GLM_fits');
+%fittedGLM=glm_fit_from_WNrun({316,7726}, '2014-11-05-2/data009_nps', 'RGB-10-2-0.48-11111-32x32', 900, '~/Desktop');
 % Or load a GLM fit that already exists
-% load('/Volumes/Analysis/nora/nishal_glmfits/15min/316.mat');
+ load('/Volumes/Analysis/nora/nishal_glmfits/15min/316.mat');
 
 %% Load the rawMovie to make predictions for
 testmovie_filename='/Volumes/Data/2014-11-05-2/visual/18.rawMovie';
 testmovie=get_rawmovie(testmovie_filename,5760);
 testmovie=permute(testmovie,[2 3 1]);
 
-%% Load the datarun to compare (where the above movie was actually run)
+%% If you want to compare to a datarun
 datarun=load_data('2014-11-05-2/data010_from_data009_nps');
 datarun=load_neurons(datarun);
+x=GLM_predict(fittedGLM, testmovie, 30, datarun);
+plotraster(x,fittedGLM,'labels',true,'raster_length',12)
 
-%% Evaluate the GLM fit and plot the rasters
-x=GLM_predict(fittedGLM, datarun, testmovie, 30);
-plotraster(x,fittedGLM,'labels',true,'raster_length',48)
+%% If you just want to make a prediction
+x=GLM_predict(fittedGLM, testmovie, 30);
+plotraster(x,fittedGLM,'labels',true,'raster_length',12, 'start_time',12)
