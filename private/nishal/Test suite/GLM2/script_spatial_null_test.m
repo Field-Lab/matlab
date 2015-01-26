5df841b0a1fb04655df3caafe4c93fa85b2fe61d
Vision <vision@bertha.stanford.edu>
2015-01-25 16:18:09 -0800

    Spatial null with STA front end in GLM

--- private/nishal/Test suite/GLM2/script_spatial_null_test.m
+++ private/nishal/Test suite/GLM2/script_spatial_null_test.m
@@ -9,17 +9,53 @@ addpath(genpath('../GLM'));
 
 %%
 
-% clear spatial_method
-% spatial_method=cell(4,1);
-% spatial_method{1}.struct_orig=[];
-% spatial_method{1}.struct_null=[];
+clear spatial_method
+spatial_method=cell(4,1);
+spatial_method{1}.struct_orig=[];
+spatial_method{1}.struct_null=[];
 
-%icell_list=0;
-for cellID=[4246,4291,4726,4789,4921,5059,5177,5326,5581,6006,6076,6391,6541,6725,6812,6826,6829,6856,7188,7532,7533,7651,7652,7726];%[3152,3331,3365,3620,3637,3692,3901,3902,3903,3904,3916,4129]%
+icell_list=0;
+for cellID=[3152,3331,3365,3620,3637,3692,3901,3902,3903,3904,3916,4129,4246,4291,4726,4789,4921,5059,5177,5326,5581,6006,6076,6391,6541,6725,6812,6826,6829,6856,7188,7532,7533,7651,7652,7726];%]%
 try
     icell_list=icell_list+1
 %fittedGLM=glm_fit_from_WNrun({cellID}, '2014-11-05-2/data009_nps', 'RGB-10-2-0.48-11111-32x32', 900, '~/Nishal/GLM_fits');
 load(sprintf('/Volumes/Analysis/nora/nishal_glmfits/15min/%d.mat',cellID));
+
+%% Replace fitted linear filter with STA - better filter?
+
+sta_filter=fittedGLM.cellinfo.WN_STA;
+cell_params.STAlen=30;
+sta_filt{1}=sta_filter;
+[new_stas,totalMaskAccept,CellMasks]=clipSTAs(sta_filt,cell_params);
+
+sta_filter=squeeze(sum(sta_filter,3));
+xcoords = fittedGLM.linearfilters.Stimulus.x_coord;
+ycoords = fittedGLM.linearfilters.Stimulus.y_coord;
+totalMaskAccept=totalMaskAccept(ycoords,xcoords);
+sta_filter = sta_filter(ycoords,xcoords,:).*repmat(totalMaskAccept,[1,1,30]);
+sta_filter(:,:,1:14)=0;
+fittedGLM.linearfilters.Stimulus.Filter = sta_filter;
+
+h1= figure;
+subplot(1,2,1);
+imagesc(fittedGLM.linearfilters.Stimulus.Filter(:,:,27));
+colormap gray
+caxis([min(fittedGLM.linearfilters.Stimulus.Filter(:)),max(fittedGLM.linearfilters.Stimulus.Filter(:))]);
+
+subplot(1,2,2);
+ssta_dummy = zeros(121,30);
+for itime =1:30
+    xx=fittedGLM.linearfilters.Stimulus.Filter(:,:,itime);
+    ssta_dummy(:,itime)=xx(:);
+end
+
+s=svd(ssta_dummy);
+subplot(1,2,2);
+plot(s,'*');
+title(sprintf('Cell %d',cellID));
+
+print(h1,'-depsc',sprintf('/Volumes/Analysis/nishal/Spatial_null/GLM_STA/cell_%d_linear_filter.eps',cellID));
+
 %% Test cell
 WNtime=120*24;
 WNmovie =double(rand(32,32,WNtime)>0.5)-0.5;
@@ -80,15 +116,19 @@ plotSpikeRaster(logical(x.rasters.glm_sim))
     %% Generate rasters
 x=GLM_predict(fittedGLM, testmovie, 100);
 
-plotraster(x,fittedGLM,'labels',true,'raster_length',24,'start_time',0)
+hFig = plotraster(x,fittedGLM,'labels',true,'raster_length',24,'start_time',0)
 % figure;
 % plotSpikeRaster(logical(x.rasters.glm_sim))
 original_struct= sqrt(var(x.rate(1:floor(end/2))));
 null_struct=sqrt(var(x.rate(floor(end/2)+1:end)));
-title(sprintf('Original struct %f , Null struct %f',original_struct,null_struct));
+title(sprintf('Cell: %d Null method %d Original struct %f , Null struct %f',cellID,ispatial_method, original_struct,null_struct));
 
 spatial_method{ispatial_method}.struct_orig(icell_list)=original_struct;
 spatial_method{ispatial_method}.struct_null(icell_list)=null_struct;
+
+print(hFig,'-depsc',sprintf('/Volumes/Analysis/nishal/Spatial_null/GLM_STA/cell_%d_null_method_%d.eps',cellID,ispatial_method));
+
+
     end
     
 catch
@@ -100,7 +140,7 @@ end
 
 %% 
 cols='rgbkm';
-figure;
+hFig=figure;
 subplot(1,2,1)
 for ispatial_method=1:5
 loglog(spatial_method{ispatial_method}.struct_orig,spatial_method{ispatial_method}.struct_null,'*','Color',cols(ispatial_method));
@@ -122,4 +162,7 @@ legend('best frame','Diff of Gaussian','Low rank approx','common temporal','spat
 xlabel('Structure in Original');
 ylabel('Sructure in Null');
 
-save('~/Nishal/Statial_null.mat','spatial_method');
+
+print(hFig,'-depsc',sprintf('/Volumes/Analysis/nishal/Spatial_null/GLM_STA/Scatter_collection.eps'));
+
+save('/Volumes/Analysis/nishal/Spatial_null/Statial_null_data.mat','spatial_method');
