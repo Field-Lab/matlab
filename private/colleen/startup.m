bba979938775d2e2e59f52199468697a21f71b80
Vision <vision@bertha.stanford.edu>
2015-01-26 16:26:06 -0800

    changes to stv code

--- private/colleen/startup.m
+++ private/colleen/startup.m
@@ -13,7 +13,7 @@ addpath(genpath('~/matlab'));
 addpath(genpath('/Users/vision/Desktop/GitHub code repository/utilities'))
 
 % Use genpath2 to recursively add everything in matlab-standard to your path, but exclude .svn directories
-addpath(genpath2(['../../../code'], {'.svn'}));
+addpath(genpath(['../../../code']));
 
 % Local Java builds: Vision, Cell-Finder
 % We'll have to fix vision_path_stable function to point to the right place on wharf-d
