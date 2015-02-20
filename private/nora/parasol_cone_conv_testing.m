fbc03963c21b0495a3dee747c7d465ea3d369ed3
Nora <nbrackbill@gmail.com>
2015-02-19 18:11:04 -0800

    Made prelim code for comparing parameters across cells

--- private/nora/parasol_cone_conv_testing.m
+++ private/nora/parasol_cone_conv_testing.m
@@ -1,79 +1,67 @@
-thresh=0.2;
+thresh=0.3;
+% 
+% single_cone_datasets={...
+% '2013-08-19-4'
+% '2013-10-10-1',...
+% '2014-04-10-2',...
+% '2014-06-04-7'};
+
+% '2012-09-13-2',... 5.5 mm
 
 single_cone_datasets={...
-'2011-05-11-6',...
-'2011-06-24-6',...
-'2011-10-25-5',...
-'2011-12-13-2',...
-'2012-04-13-1',...
-'2012-08-21-2',...
-'2012-09-06-0',...
-'2012-09-13-2',...
-'2012-09-21-2',...
-'2012-09-24-5',...
-'2013-08-19-4',...
-'2013-08-19-5 ',...
-'2013-10-10-1',...
-'2014-04-10-2',...
-'2014-06-04-7'};
+'2011-05-11-6/data002'};
+
+cone_folders={'data002-bayes-msf_5.00-BW-2-4'};
 
 single_cone_eccentricity=[];
 
 for i_dataset=1:1 %length(single_cone_datasets)
     
-    % Find cone data
-    cone_data=dir(['/Volumes/Analysis/' single_cone_datasets{i_dataset} '/cone_data']);
-    for i_conerun=3:3 %length(cone_data)
-        found=false; i=1;
-        while ~found
-            if strcmp(cone_data(i_conerun).name(i:i+3),'data')
-                piece=cone_data(i_conerun).name(i:i+6);
-                found=true;
-            else
-                i=i+1;
-            end
-        end
-        datarun=load_data([single_cone_datasets{i_dataset} '/' piece]);
-        datarun=load_neurons(datarun);
-        datarun=load_params(datarun);
-        datarun=load_cones_njb(datarun, cone_data(i_conerun).name);
-        
-        % find on and off parasols
-        if 0
-        for i_type=1:length(datarun.cell_types)
-            %$ datarun.cell_types{i_type}.name
-            if any(strncmpi(datarun.cell_types{i_type}.name, {'onpar', 'on par'},5))
-                on_par=datarun.cell_types{i_type}.cell_ids;
-                disp(datarun.cell_types{i_type}.name)
-            elseif any(strncmpi(datarun.cell_types{i_type}.name, {'offpar', 'off par'},5))
-                off_par=datarun.cell_types{i_type}.cell_ids;
-                disp(datarun.cell_types{i_type}.name)
-            end
-        end
-        end
-        
-        [indices,~,~ ]=get_cell_indices(datarun, {'ON parasol', 'OFF parasol'});
-        
-        ON_cones=zeros(length(indices{1}),1);
-        OFF_cones=zeros(length(indices{2}),1);
-        
-        % find # of cones for each
-        for i_cell=1:length(indices{1})
-            cone_weights=datarun.cones.weights(:,indices{1}(i_cell));
-            ON_cones(i_cell)=sum(cone_weights>max(cone_weights)*thresh);
-            clear cone_weights
-        end
-        for i_cell=1:length(indices{2})
-            cone_weights=datarun.cones.weights(:,indices{2}(i_cell));
-            OFF_cones(i_cell)=sum(cone_weights>max(cone_weights)*thresh);
-            clear cone_weights
-        end
-        
-        % save in some data structure
-        cone_data{i_dataset}.ON.mean=mean(ON_cones);
-        cone_data{i_dataset}.ON.stderror=std(ON_cones)/sqrt(length(ON_cones));
-        cone_data{i_dataset}.OFF.mean=mean(OFF_cones);
-        cone_data{i_dataset}.OFF.stderror=std(OFF_cones)/sqrt(length(OFF_cones));
-        
+    datarun=load_data(single_cone_datasets{i_dataset});
+    datarun=load_neurons(datarun);
+    datarun=load_params(datarun);
+    datarun=load_sta(datarun);
+    datarun=load_cones_njb(datarun, cone_folders{i_dataset});
+    
+    % find on and off parasols
+    try
+        [indices{1},~,~ ]=get_cell_indices(datarun, 'ON parasol');
+    catch 
+        warning('No ON parasols')
     end
+    try
+        [indices{2},~,~ ]=get_cell_indices(datarun, 'OFF parasol');
+    catch
+        warning('No OFF parasols')
+    end
+
+    ON_cones=zeros(length(indices{1}),1);
+    OFF_cones=zeros(length(indices{2}),1);
+    
+    %% find # of cones for each
+    for i_cell=1:length(indices{2})
+        cone_weights=datarun.cones.weights(:,indices{2}(i_cell));
+        sig_cones=cone_weights>max(cone_weights)*thresh;
+        imagesc(datarun.stas.stas{indices{2}(i_cell)}(:,:,1,5)); colormap gray;
+        hold on;
+        plot(datarun.cones.centers(sig_cones,1),datarun.cones.centers(sig_cones,2),'+');
+        hold off;
+        pause()
+        OFF_cones(i_cell)=sum(sig_cones);
+        clear cone_weights
+    end
+    
+    %%
+    for i_cell=1:length(indices{2})
+        cone_weights=datarun.cones.weights(:,indices{2}(i_cell));
+        OFF_cones(i_cell)=sum(cone_weights>max(cone_weights)*thresh);
+        clear cone_weights
+    end
+    
+    % save in some data structure
+    cone.ON.mean(i_dataset)=mean(ON_cones);
+    cone.ON.stderror(i_dataset)=std(ON_cones)/sqrt(length(ON_cones));
+    cone.OFF.mean(i_dataset)=mean(OFF_cones);
+    cone.OFF.stderror(i_dataset)=std(OFF_cones)/sqrt(length(OFF_cones));
+
 end
