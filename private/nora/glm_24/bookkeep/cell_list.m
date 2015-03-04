681d3d41179ddcf8c11a72f86f1671946a9a092a
Nora <nbrackbill@gmail.com>
2015-03-03 16:04:23 -0800

    Formatted more data for Carlos and added all cells function for GLM fits

--- private/nora/glm_24/bookkeep/cell_list.m
+++ private/nora/glm_24/bookkeep/cell_list.m
@@ -7,33 +7,34 @@ function [exp_nm,cid_cell,expname]  = cell_list( expnumber, cellselectiontype)
 
 
 %%% Identify exp_nm  %%%
-if expnumber == 1, exp_nm ='2012-08-09-3';  end
-if expnumber == 2, exp_nm ='2012-09-27-3';  end
-if expnumber == 3, exp_nm ='2013-08-19-6';  end 
-if expnumber == 4, exp_nm ='2013-10-10-0';  end 
-if expnumber == 5, exp_nm ='2014-06-04-0';  end 
+if expnumber == 1, exp_nm ='2012-08-09-3'; expname = 'expA';  end
+if expnumber == 2, exp_nm ='2012-09-27-3'; expname = 'expB';  end
+if expnumber == 3, exp_nm ='2013-08-19-6'; expname = 'expC';  end 
+if expnumber == 4, exp_nm ='2013-10-10-0'; expname = 'expD';  end 
+if expnumber == 5, exp_nm ='2014-06-04-0'; expname = 'expE';  end 
 
 
 %%% Find appropriate CID %%%
 if strcmp(cellselectiontype, 'shortlist')    
-    if expnumber == 1, expname = 'expA'; cid_cell = {1471 3676  5086 5161 841 1426 1772 2101 1276}; end % 3676 and 1772 doesnt work for power raise on Bertha
-    if expnumber == 2, expname = 'expB'; cid_cell = {1 31 301 1201 1726 91 1909 2360 6858};  end
-    if expnumber == 3, expname = 'expC'; cid_cell = {2824 3167 3996 5660 6799 737 1328 1341 2959  5447}; end 
-    if expnumber == 4, expname = 'expD'; cid_cell =  {32 768 2778 4354 5866 7036 346 1233 3137  5042 5418};end %cid_cell = { 3137  5042 5418}
-    if expnumber == 5, expname = 'expE'; cid_cell = {901 5086}; end
+    if expnumber == 1, cid_cell = {1471 3676  5086 5161 841 1426 1772 2101 1276}; end % 3676 and 1772 doesnt work for power raise on Bertha
+    if expnumber == 2, cid_cell = {1 31 301 1201 1726 91 1909 2360 6858};  end
+    if expnumber == 3, cid_cell = {2824 3167 3996 5660 6799 737 1328 1341 2959  5447}; end 
+    if expnumber == 4, cid_cell =  {32 768 2778 4354 5866 7036 346 1233 3137  5042 5418};end %cid_cell = { 3137  5042 5418}
+    if expnumber == 5, cid_cell = {901 5086}; end
 end
 
 
 if strcmp(cellselectiontype, 'debug')    
-    if expnumber == 1, expname = 'expA'; cid_cell = {3676}; end %{5086 841}
-    if expnumber == 2, expname = 'expB'; cid_cell = {1 6858};  end
-    if expnumber == 3, expname = 'expC'; cid_cell = {2824  5447}; end 
-    if expnumber == 4, expname = 'expD'; cid_cell = { 5866 5418};end
-    if expnumber == 5, expname = 'expE'; cid_cell = {901 5086}; end
+    if expnumber == 1, cid_cell = {3676}; end %{5086 841}
+    if expnumber == 2, cid_cell = {1 6858};  end
+    if expnumber == 3, cid_cell = {2824  5447}; end 
+    if expnumber == 4, cid_cell = { 5866 5418};end
+    if expnumber == 5, cid_cell = {901 5086}; end
 end
 
 if strcmp(cellselectiontype,'all')
-     if expnumber == 1, expname = 'expA'; cid_cell = {1292,1321,1471,1771,1786,2211,2506,2851,3361,3676,3843,4336,4367,4576,467,497,5086,5161,5386,5851,5986,6256,7066,7292,7606,1205,121,1276,1352,1426,1502,1772,1921,2101,2312,2641,3152,3226,3647,3799,4021,4216,4366,4456,4711,4756,5131,5132,5134,5431,5777,5778,5783,586,5914,5972,6257,650,6721,7068,7171,7291,7607,7756,841}; end
+    load('all_cells.mat');
+    cid_cell = all_cells{expnumber};
 end
 
 
