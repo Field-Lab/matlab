1053c4807463c53220ed7986669c6b8702fee3db
Nishal Shah <bhaishahster@gmail.com>
2015-03-03 20:41:17 -0800

    Contrast explore

---
+++ private/nishal/create_act_2/pc2015_02_24_2_data006_contrast_explore.m
@@ -0,0 +1,69 @@
+
+% Nishal P. Shah , September 2014
+
+% Have a dataset ready with cells you want to nullify grouped under a
+% common name(use Vision software).
+% This is 2015-02-24-2/data012
+% See 35be7e17c3ec9ef0626a7d21aa3a40b991579b5d to retore stats of code. 
+startup_bertha
+
+startup_rooster
+
+datafile='2015-02-24-2/streamed/data006/data006';%'nishal/2014-08-20-2/data001/data001';
+
+% type_name_inp = 'userCellList' for a list of cells.
+
+no_images_per_movie=10;
+start_image=10;
+tag='pc2015-02-24-2_test';
+destination_mat=['/Volumes/Analysis/nishal/',tag];
+if(~exist(destination_mat,'dir'))
+mkdir(destination_mat);
+end
+
+dest_raw='/Volumes/Data/stimuli/movies/null_space/';
+
+movies=cell(20,1);
+% 'solver' .. 1 for LSQR, 2 for CRAIG, 3 for Fourier 
+
+
+%% Iterated Spatial nulling - has interval parameter - remember to carefully decide the scaling (not stretching) when doing Iterated methods.
+cell_params=struct();
+cell_params.type_name_inp='userCellList';%'userCellList';
+cell_params.cell_list=[5269,3706,2417,5192,7651,5897];%[5119,4172,3093,1263,273,1426,5268,17,3277]%[3888,2825,1820,4129, 5346,5671,5161,1278, 3828,3574,4036,3572, 503,560,797,1009,487,181,901]; % if type_name_inp = 'userCellList' 
+cell_params.STAlen=14;
+cell_params.sta_spatial=sprintf('%s/stas_spatial.mat',destination_mat);
+cell_params.use_fits=2; % 2, 0,0,2
+cell_params.sta_spatial_method=4;%1,2 ,3,4
+% cell_params.sta_spatial_method = 1 for just using 4th frame, 2 is for fitting spatial STA. 
+% Use cell_params.use_fits=2 (clipping) if cell_params.sta_spatial_method = 1 and 
+% use cell_params.use_fits=0 (no processing of STA) if
+% cell_params.sta_spatial_method = 2;
+% STA spatial null Method 3 = low rank, 4 = average waveform and use it ..  
+
+mov_params=struct();
+mov_params.mov_type='bw-precomputed';
+mov_params.movie_time=120*10/8;
+mov_params.mean=0.5*255;
+mov_params.deviation=0.48*255;
+
+mov_params.mdf_file = '/Volumes/Analysis/stimuli/white-noise-xml/BW-20-8-0.48-11111-16x16.xml';
+mov_params.stixel=20;
+
+mov_params.interval = 8; % Not important to have this parameter. Default is 1. When we want repeated frames, just set this interval (This just controls the blank Frames), and select the movie_time appropriately.
+
+% Post process. Default is stretch. If using default, need to give only mov_params.scaling_loss parameter.
+mov_params.post_process_method = 'scale'; % or, 'stretch'
+mov_params.scale = 0.48/0.48;
+%mov_params.scaling_loss=0.05; % a number in [0,1], fraction of values that is changed by scaling.
+
+solver=8; % Solver 4 used for spatial nulling, 8 for iterated spatial nulling
+[mov_orignial,mov_modify_new]=null_space_movie2(datafile,cell_params,mov_params,solver);
+
+movies{1}=mov_orignial;
+movies{2}=mov_modify_new;
+mov_idx=1;
+write_movie_idx(destination_mat,movies{mov_idx},mov_idx,mov_params.stixel);
+mov_idx=2;
+write_movie_idx(destination_mat,movies{mov_idx},mov_idx,mov_params.stixel);
+
