ae3f8806d2165d22f74fc891cbf04d25086e9e47
Nishal Shah <bhaishahster@gmail.com>
2015-03-04 23:52:40 -0800

    Cone connectivity

--- private/nishal/coneAnalysis/LRT_separate.m
+++ private/nishal/coneAnalysis/LRT_separate.m
@@ -1,4 +1,7 @@
-cellID=2;
+cellID=9;
+figure;
+imagesc(datarun.stas.rfs{cellID});
+
 
 cones= find(datarun.cones.weights(:,cellID)==1);
 coneInp = datarun.cone_inputs(:,cones);
@@ -40,6 +43,52 @@ LRT_mat(cone1,cone2)=LRT;
 end
 end
 
-threshold=-166; % minimum violations
+threshold=-166; % choose threshold according to minimum violations
 Connectivity = LRT_mat>threshold;
 Connectivity(logical(eye(noCones)))=0
+% Cones 2, 5 for cell ID 2 must be connected
+%%
+
+coneInp_SpkTrig = coneInp(logical(y'),:);
+covar = double(coneInp_SpkTrig'*coneInp_SpkTrig)/sum(y>0);
+
+idx=logical(1-eye(noCones));
+lam=0.15;
+cvx_begin
+variable c_sp(noCones,noCones) semidefinite
+subject to
+
+minimize (norm(covar-c_sp)+lam*(sum(abs(c_sp(idx)))) ) 
+cvx_end
+
+
+Connectivity2 = (abs(c_sp)>0.0001);
+Connectivity2(logical(eye(noCones)))=0
+
+
+figure;
+subplot(1,4,1);
+covar(logical(eye(noCones)))=0;
+imagesc(covar);
+axis image
+colorbar
+
+
+subplot(1,4,2);
+c_sp(logical(eye(noCones)))=0;
+imagesc(c_sp);
+axis image
+colorbar
+
+
+subplot(1,4,3);
+Connectivity2(logical(eye(noCones)))=0;
+imagesc(Connectivity2);
+axis image
+colorbar
+
+subplot(1,4,4);
+Connectivity(logical(eye(noCones)))=0;
+imagesc(Connectivity);
+axis image
+colorbar
