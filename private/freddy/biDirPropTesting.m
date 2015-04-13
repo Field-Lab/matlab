aa5cee5df08222b52dbb23e300e2e0e106324183
Frederick Kellison-Linn <fred.kl@me.com>
2015-04-13 10:22:35 -0700

    add test for bi-directional propegation

---
+++ private/freddy/biDirPropTesting.m
@@ -0,0 +1,16 @@
+temp = load('~/git_code/matlab/private/lauren/MATLAB_code/analysis/dataset-specific/axonBundleThresholds_byPattern_2012_09_24_3_data008.mat');
+axonBundleThresholds = temp.axonBundleThresholds_byPattern_2012_09_24_3_data008;
+
+i = 1;
+estimates = [];
+for i = 1:368
+    
+    bundleMeans = abs(getBundleVoltagesAStar('/Volumes/Analysis/2014-11-24-2/data007/', i, false));
+    avg = mean(bundleMeans(1:5, 1));
+    index = find(bundleMeans(:,1) > 20, 1, 'first');
+    %index = find(bundleMeans(:,1) > 5 * avg, 1, 'first');
+    disp(num2str(bundleMeans(index, 3)));
+    
+    estimates = [estimates; bundleMeans(index, 3)];
+    
+end
