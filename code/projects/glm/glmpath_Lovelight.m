fa272258bc9d418ba0f9c5f291215796d924fc68
Vision <vision@bertha.stanford.edu>
2015-04-20 22:07:26 -0700

    trying to figure out directories on bertha

--- code/projects/glm/glmpath_Lovelight.m
+++ code/projects/glm/glmpath_Lovelight.m
@@ -2,7 +2,7 @@
 % Calls NSEM_BaseDirectories
 %%
 function glmpath_Lovelight
-copyfile('/home/vision/Nora/matlab/code/projects/glm/NSEM_BaseDirectories_Bertha.m','/home/vision/Nora/matlab/code/projects/glm/NSEM_BaseDirectories.m') %BERTHA
+copyfile('/home/vision/Nora/matlab/code/projects/glm/NSEM_BaseDirectories_Lovelight.m','/home/vision/Nora/matlab/code/projects/glm/NSEM_BaseDirectories.m') %BERTHA
 % copyfile('/Users/Nora/Documents/MATLAB/matlab/code/projects/glm/NSEM_BaseDirectories_Lovelight.m' , '/Users/Nora/Documents/MATLAB/matlab/code/projects/glm/NSEM_BaseDirectories.m')
 BD = NSEM_BaseDirectories;
 glmwrapdir     = BD.GLM_codehome ;
