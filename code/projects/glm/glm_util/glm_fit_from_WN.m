6db8cdab09a55fde183e9a908d964e05a3775b75
Vision <vision@bertha.stanford.edu>
2015-05-05 16:03:38 -0700

    Revert "Pillow replication"
    This reverts commit 3924ba91443471c6043c346e36d74b634f58ee56.

--- code/projects/glm/glm_util/glm_fit_from_WN.m
+++ code/projects/glm/glm_util/glm_fit_from_WN.m
@@ -14,7 +14,7 @@
 % The lab codebase, addpath(genpath('Repo location /matlab/code/lab'))
 % The glm code folder, addpath(genpath('Repo location /matlab/code/projects/glm))
 
-function [fittedGLM] = glm_fit_from_WN(cells, dataset, stim, varargin)
+function [fittedGLM] = glm_fit_from_WN(cells, dataset, stim_description, varargin)
 
 % INPUTS
 
@@ -22,10 +22,7 @@ function [fittedGLM] = glm_fit_from_WN(cells, dataset, stim, varargin)
 % glm_fit_from_WN(cells, dataset, stim_description, optional: stim_length, d_save)
 % dataset='2014-11-05-2/data009_nps';
 % cells=[2372,2523]
-% stim: either a string like 'RGB-10-2-0.48-11111-32x32' 
-%    or the matfile movie
-%    with a frame for every 1/120 seconds (regardless of interval, so will have repeated frames if interval >1) 
-%    and stim size x time if black and white or stim size x 3 x time if RGB
+% stim_description 'RGB-10-2-0.48-11111-32x32'
 
 % OPTIONAL KEYWORDS
 % stim_length, optional, default 15 min
@@ -35,8 +32,8 @@ function [fittedGLM] = glm_fit_from_WN(cells, dataset, stim, varargin)
 
 % Parse optional input
 p = inputParser;
-p.addParameter('stim_length', 900)
-p.addParameter('d_save', 0)
+p.addParamValue('stim_length', 900)
+p.addParamValue('d_save', 0)
 p.parse(varargin{:});
 stim_length = p.Results.stim_length;
 d_save = p.Results.d_save;
@@ -53,32 +50,23 @@ if isstr(d_save)
     if ~exist(d_save,'dir'), mkdir(d_save); end
 end
 
-% Get Movie
-if isstr(stim)
-    disp(['Using ' stim ' XML file'])
-    xml_file=['/Volumes/Analysis/stimuli/white-noise-xml/' stim '.xml'];
-    dashes=find(stim=='-');
-    WN_type=stim(1:dashes(1)-1);
-    interval = str2double(stim(dashes(2)+1:dashes(3)-1));
-    fitframes=stim_length*120/interval;
-    
-    % Load Movie
-    disp('Loading Stimulus Movies')
-    [temp_fitmovie,height,width,~,~] = get_movie(xml_file, datarun.triggers, fitframes);
-    fitmovie=zeros(height,width,3,fitframes);
-    for i=1:fitframes
-        fitmovie(:,:,:,i)=temp_fitmovie(:,:,:,ceil(i/interval));
-    end
-    if strcmp(WN_type, 'BW')
-        fitmovie = squeeze(fitmovie(:,:,1,:));
-    end
-else
-    fitmovie = stim;
+% Get Movie Params
+disp(['Using ' stim_description ' XML file'])
+xml_file=['/Volumes/Analysis/stimuli/white-noise-xml/' stim_description '.xml'];
+dashes=find(stim_description=='-');
+WN_type=stim_description(1:dashes(1)-1);
+interval = str2double(stim_description(dashes(2)+1:dashes(3)-1));
+fitframes=stim_length*120/interval;
+
+% Load Movie
+disp('Loading Stimulus Movies')
+[temp_fitmovie,height,width,~,~] = get_movie(xml_file, datarun.triggers, fitframes);
+fitmovie_color=zeros(height,width,3,fitframes);
+for i=1:fitframes
+    fitmovie_color(:,:,:,i)=temp_fitmovie(:,:,:,ceil(i/interval));
 end
-
-disp(size(fitmovie))
+disp(size(fitmovie_color))
 clear temp_fitmovie height width i
-
 
 %% Load Cell Specific Elements   Spikes and STA
 for i_cell = 1:length(cells)
@@ -93,12 +81,10 @@ for i_cell = 1:length(cells)
     RGB = RGB_weights(datarun,master_idx);
     glm_cellinfo.RGB=RGB;
     
-    if size(fitmovie,3) == 3
-        % Turn RGB movie into greyscale movie
-        fitmovie=squeeze(RGB(1)*fitmovie_color(:,:,1,:)+ ...
-            RGB(2)*fitmovie_color(:,:,2,:)+ ...
-            RGB(3)*fitmovie_color(:,:,3,:));
-    end
+    % Turn RGB movie into greyscale movie
+    fitmovie=squeeze(RGB(1)*fitmovie_color(:,:,1,:)+ ...
+        RGB(2)*fitmovie_color(:,:,2,:)+ ...
+        RGB(3)*fitmovie_color(:,:,3,:));
     
     % Spike loading
     spikes = datarun.spikes{master_idx};
@@ -111,7 +97,6 @@ for i_cell = 1:length(cells)
         WN_STA = temp_STA;
         clear temp_STA
     end
-    WN_STA = squeeze(WN_STA);
     center_coord = round(datarun.vision.sta_fits{master_idx}.mean);
     center.y_coord = center_coord(1);
     center.x_coord = size(fitmovie,1) - center_coord(2);
