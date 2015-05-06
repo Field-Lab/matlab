55bf10b86562f79b9b86045d1dbf1a3b883318bb
Vision <vision@bertha.stanford.edu>
2015-05-06 16:20:16 -0700

    bertha changes

--- private/nora/pillow_test/pillow_test.m
+++ private/nora/pillow_test/pillow_test.m
@@ -12,4 +12,7 @@ datarun = load_data('2005-04-26-1/data005-pca/data005/data005');
 datarun = load_params(datarun);
 on_par = datarun.vision.cell_types{1,33}.cell_ids;
 
-fittedGLM = glm_fit_from_WN(on_par, '2005-04-26-1/data005-pca/data005/data005', 'BW-20-1-0.48-11111', 'stim_length', 1200, 'd_save', '/Volumes/Lab/Users/Nora/pillow_test');
+on_par2 = on_par(on_par > 5626);
+
+
+fittedGLM = glm_fit_from_WN(on_par2, '2005-04-26-1/data005-pca/data005/data005', 'BW-20-1-0.48-11111', 'stim_length', 1200, 'd_save', '/Volumes/Lab/Users/Nora/pillow_test');
