b6c862d7b23547b23fa2309659da4b12184bab9d
Colleen Rhoades <rhoades@stanford.edu>
2015-05-06 10:09:17 -0700

    Plot DS results broken down by spatial and temporal pattern

--- private/colleen/New Cell Types/DS/runDS.m
+++ private/colleen/New Cell Types/DS/runDS.m
@@ -1,129 +1,51 @@
 % ds_cells
-run_opt.data_set = '2015-04-14-2';
+clear
+run_opt.data_set = '2015-04-09-1';
 run_opt.data_run = 3; % 12-19 for 2007-03-27, 2-11 for 2007-08-24, 13-17 for 2005-04-26
-if strcmp(run_opt.data_set, '2015-04-14-2')
-    datarun{1}.names.rrs_params_path='/Volumes/Analysis/2015-04-14-2/data003-mapped-data002/data003-mapped-data002.params';
-    datarun{2}.names.rrs_neurons_path=('/Volumes/Analysis/2015-04-14-2/data003-mapped-data002/data003-mapped-data002.neurons');
-    datarun{2}.names.stimulus_path=('/Volumes/Data/2015-04-14-2/Visual/s03');
+if strcmp(run_opt.data_set, '2015-04-09-1')
+    datarun{1}.names.rrs_params_path='/Volumes/Analysis/2015-04-09-1/data015-mapped-data011/data015-mapped-data011.params';
+    datarun{2}.names.rrs_neurons_path=('/Volumes/Analysis/2015-04-09-1/data015-mapped-data011/data015-mapped-data011.neurons');
+    datarun{2}.names.stimulus_path=('/Volumes/Data/2015-04-09-1/Visual/s15');
 end
 opt=struct('verbose',1,'load_params',1,'load_neurons',1,'load_obvius_sta_fits',true);
 datarun=load_data(datarun,opt);
 datarun=map_cell_types(datarun, struct('map',[1 2],'verbose',true));
-datarun{2}=load_stim(datarun{2},'correction_incomplet_run', 0);
+datarun{2}=load_stim(datarun{2},'correction_incomplet_run', 0, 'trigger_iti_thr', 0.0006); % manually set threshold until got right number of triggers
+% Something went wrong with the first two stimuli
+% datarun{2}.stimulus.triggers= [datarun{2}.stimulus.triggers(1:33) 187.0086, datarun{2}.stimulus.triggers(34:70) 396.0097 datarun{2}.stimulus.triggers(71:72) 412.5063 datarun{2}.stimulus.triggers(73:72); % Remove the first trigger
+% datarun{2}.stimulus.trials = datarun{2}.stimulus.trials(3:end); % Remove the first two trials
 
-% k=1; kmin=1; kmax=size(datarun{2}.stimulus.trials,2); hk=loop_slider(k,kmin,kmax);
-% 
-% while k
-%     if ~ishandle(hk)
-%         break % script breaks until figure is closed
-%     end
-%     for z = 1:size(datarun{2}.stimulus.trials,2)
-%         
-%         k=round(get(hk,'Value'));
-%         spatial = datarun{2}.stimulus.trials(z).SPATIAL_PERIOD
-%         temporal = datarun{2}.stimulus.trials(z).TEMPORAL_PERIOD
-%         direction = datarun{2}.stimulus.trials(z).DIRECTION
-%         
-%         
-%         
-%         
-%         %         run_opt.config_num = findStimType(datarun, run_opt.contrast, run_opt.direction);
-        cell_indices1=get_cell_indices(datarun{1}, [4802]);
-        cell_indices2=get_cell_indices(datarun{2},[4802]);
-%         cell_x_pos = cellfun( @(X) X.mean(1), datarun{1}.vision.sta_fits); % x axis position of all STA cells
-%         [~, cell_sort_idx] = sort(cell_x_pos(cell_indices1)); % indicies of how to sort
-%         
-%         %cell_indices sorted by their x coordinate of the RF from the STA
-%         cell_indices1 = cell_indices1(cell_sort_idx); % cell_indices1 is now indexes in order from lowest to highest firing rate
-%         cell_indices2 = cell_indices2(cell_sort_idx);
-%         
-%         % Find trial start and stop times
-%         %             start = 0;
-%         %             stop = mean(datarun{2}.stimulus.triggers(2:2:end) - datarun{2}.triggers(1:2:end));
-%         tr=datarun{2}.stimulus.triggers; % all start triggers
-%         run_opt.config_num = 1;
-%         t=find(datarun{2}.stimulus.trial_list==run_opt.config_num); %find the times when all the stimulus type 2 starts
-%         tr=tr(1);
-%         tr= 0:temporal/120:5;
-%         start = 0;
-%         stop = temporal/120;
-%         
-%         %         if size(run_opt.cell_type,2) == 1
-%         %         cell_indices1 = [cell_indices1{1}];
-%         %         cell_indices2 = [cell_indices2{1}];
-%         %         %     cell_x_pos = [cell_x_pos{1}];
-%         %         [~, cell_sort_idx] = sort(cell_x_pos(cell_indices1)); % indicies of how to sort
-%         
-%         %     else
-%         %
-%         %         cell_indices1 = [cell_indices1{1}, cell_indices1{2}];
-%         %         cell_indices2 = [cell_indices2{1}, cell_indices2{2}];
-%         %         %     cell_x_pos = [cell_x_pos{1}, cell_x_pos{2}];
-%         %         [~, cell_sort_idx] = sort(cell_x_pos(cell_indices1)); % indicies of how to sort
-%         %     end
-%         
-%         %cell_indices sorted by their x coordinate of the RF from the STA
-%         cell_indices1= cell_indices1(cell_sort_idx); % cell_indices1 is now indexes in order from lowest to highest firing rate
-%         cell_indices2 = cell_indices2(cell_sort_idx);
-%         
-%         
-%         
-%         run_opt.raster = 1;
-%         if run_opt.raster %raster
-%             %         cell_indices1 = [cell_indices1{1}, cell_indices1{2}];
-%             %         cell_indices2 = [cell_indices2{1}, cell_indices2{2}];
-%             %         cell_x_pos = [cell_x_pos{1}, cell_x_pos{2}];
-%             %         [~, cell_sort_idx] = sort(cell_x_pos(cell_indices1)); % indicies of how to sort
-%             
-%             %cell_indices sorted by their x coordinate of the RF from the STA
-%             cell_indices1= cell_indices1(cell_sort_idx); % cell_indices1 is now indexes in order from lowest to highest firing rate
-%             cell_indices2 = cell_indices2(cell_sort_idx);
-%             
-%             
-%             % Takes in start and stop time (0-0.7274)
-%             % Spikes of the cell with the lowest firing rate first
-%             % start time of each stimulus type trigger
-%             % Finds the spikes that happened on a cell from stimulus onset to end
-%             % Plot those spike times on the x axis versus the trial number on the y
-%             % axis
-%             % If tracking motion, the cell should respond to the bar at the same
-%             % time on every trial
-%             
-%             psth_r = psth_raster(start,stop,datarun{2}.spikes{cell_indices2(1)}',tr);
-%             
-%             % Title is the cell id according to vision and the mean firing rate
-%             title(sprintf('Cell ID: %d   Direction:%d   Temporal: %d   Spatial: %d', datarun{2}.cell_ids(cell_indices2(1)), direction, temporal, spatial ))
-%             xlabel('ms')
-%             drawnow
-%                         uiwait;
-%         end
-%     end
-% end
+
+cell_to_run = 889;
+        cell_indices1=get_cell_indices(datarun{1}, [cell_to_run]);
+        cell_indices2=get_cell_indices(datarun{2},[cell_to_run]);
 
 % get spikes per bin
 
-     tr=datarun{2}.triggers; % all start triggers
-     tr=tr(1:10:end-1); % all start triggers
+     tr=datarun{2}.stimulus.triggers; % all start triggers
         run_opt.config_num = 1;
-%         t=find(datarun{2}.stimulus.trial_list==run_opt.config_num); %find the times when all the stimulus type 2 starts
-%         tr=tr(1);
 
-        
+get_stimulus_triggers(datarun)
+       
 n = datarun{2}.spikes{cell_indices2(1)}';
 psth_r = [];
-for z = 1:size(tr,1)
+for z = 1:size(tr,2)
+        ind = find(tr(z) == datarun{2}.triggers);
         spatial = datarun{2}.stimulus.trials(z).SPATIAL_PERIOD;
         temporal = datarun{2}.stimulus.trials(z).TEMPORAL_PERIOD;
         direction = datarun{2}.stimulus.trials(z).DIRECTION;
-                sub_spacing= 0:temporal/120:4.99;
+        interval = 5;
+        num_triggers = interval/(temporal/120);
+        sub_spacing= datarun{2}.triggers(ind:ind+num_triggers) - datarun{2}.triggers(ind);
         start = 0;
         stop = temporal/120;
 
         
 
         
-for i=1:length(sub_spacing),
+for i=1:length(sub_spacing)-1,
         h=n-tr(z)-sub_spacing(i);  
+        stop = sub_spacing(i+1) - sub_spacing(i);
         hh=find(h>=start & h<=stop);
         psth_r=[psth_r; (h(hh)*1000)', repmat(length(sub_spacing)-i,[length(hh),1]);];
         
@@ -135,3 +57,71 @@ psth_r = [];
 end
 
 
+k=1; kmin=1; kmax=size(psth_mat,2); hk=loop_slider(k,kmin,kmax);
+
+% while k
+%     if ~ishandle(hk)
+%         break % script breaks until figure is closed
+%     end
+%         
+%         k=round(get(hk,'Value'));
+%         
+%         plot(psth_mat{k}(:,1), psth_mat{k}(:,2), '.k')
+%         title({['Cell: ', num2str(cell_to_run)]; ['Spatial Period: ', num2str(datarun{2}.stimulus.trials(k).SPATIAL_PERIOD)]; ['Temporal Period: ', num2str(datarun{2}.stimulus.trials(k).TEMPORAL_PERIOD)]; ['Direction: ', num2str(datarun{2}.stimulus.trials(k).DIRECTION)]})
+% drawnow
+% hold off
+% uiwait;
+% end
+
+by_trial = zeros(size(tr,2),4);
+for i = 1:size(tr,2)
+    by_trial(i,1) = datarun{2}.stimulus.trials(i).DIRECTION;
+        by_trial(i,2) = datarun{2}.stimulus.trials(i).SPATIAL_PERIOD;
+        by_trial(i,3) = datarun{2}.stimulus.trials(i).TEMPORAL_PERIOD;
+        by_trial(i,4) = size(psth_mat{i},1);
+
+end
+
+spat64 = find(by_trial(:,3) == 60);
+figure;
+for i = spat64
+    plot(by_trial(i,1), by_trial(i,4), 'k.')
+    hold on
+end
+
+spat64 = find(by_trial(:,3) == 30);
+figure;
+for i = spat64
+    plot(by_trial(i,1), by_trial(i,4), 'k.')
+    hold on
+end
+
+spat64 = find(by_trial(:,3) == 15);
+figure;
+for i = spat64
+    plot(by_trial(i,1), by_trial(i,4), 'k.')
+    hold on
+end
+
+spat64 = find(by_trial(:,2) == 64);
+figure;
+for i = spat64
+    plot(by_trial(i,1), by_trial(i,4), 'k.')
+    hold on
+end
+
+spat64 = find(by_trial(:,2) == 32);
+figure;
+for i = spat64
+    plot(by_trial(i,1), by_trial(i,4), 'k.')
+    hold on
+end
+
+spat64 = find(by_trial(:,2) == 16);
+figure;
+for i = spat64
+    plot(by_trial(i,1), by_trial(i,4), 'k.')
+    hold on
+end
+
+    
