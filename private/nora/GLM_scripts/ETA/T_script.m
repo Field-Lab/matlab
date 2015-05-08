3e32ccb506984f0bf378eecf11c16e5d2f09a0d6
Nora <nbrackbill@gmail.com>
2015-05-08 14:49:20 -0700

    cahnged path for bertha

--- private/nora/GLM_scripts/ETA/T_script.m
+++ private/nora/GLM_scripts/ETA/T_script.m
@@ -1,3 +1,3 @@
 [testmovie, res] = res_spikes_raster(2,'ON');
-save('/Users/Nora/Desktop/Exp2ON_movie_and_res.mat')
+save('/Volumes/Lab/Users/Nora/Exp2ON_movie_and_res.mat')
 res_spikes_plot(testmovie, res)
