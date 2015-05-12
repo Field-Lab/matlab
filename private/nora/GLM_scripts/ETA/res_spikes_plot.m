cd9887ddfd88c4577384f4330227ba5dc1323760
Nora <nbrackbill@gmail.com>
2015-05-12 12:23:04 -0700

    glm ETA videos

--- private/nora/GLM_scripts/ETA/res_spikes_plot.m
+++ private/nora/GLM_scripts/ETA/res_spikes_plot.m
@@ -1,9 +1,15 @@
 function res_spikes_plot(testmovie, res)
 
+default_colors = get(gca,'ColorOrder');
+
 frames = length(testmovie);
-writerObj = VideoWriter('/Users/Nora/Desktop/glm_OFF_ResSpikes_WN.avi');
+writerObj = VideoWriter('/Users/Nora/Desktop/glm_ON_error_colored.avi');
 writerObj.FrameRate = 20;
 open(writerObj);
+
+n_cells = length(res.centers);
+
+colors = zeros(n_cells,3);
 
 for i_frame = 1:frames
     imagesc(testmovie(:,:,i_frame)')
@@ -13,7 +19,16 @@ for i_frame = 1:frames
     hold on
     idx = (i_frame-1)*10+(1:10);
     response = sum(res.spikes(:,idx),2);
-    scatter(res.centers(:,2), res.centers(:,1), 1+0.1*response, 'r', 'filled');
+    
+    neg = sum(response<0);
+    pos = sum(response>0) ;
+    
+    colors(response<0,:) = repmat(default_colors(1,:), neg, 1); 
+    colors(response>0,:) = repmat(default_colors(2,:), pos, 1); 
+    
+    scatter(res.centers(:,2), res.centers(:,1), 1+0.1*abs(response), colors, 'filled');
+    
+    title('Red: more spikes, Blue: less spikes recorded than predicted')
     F = getframe;
     writeVideo(writerObj, F);
     clf
