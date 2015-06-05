f07159abe0695ccd74651c0bce91aa8d1193cbe7
Colleen Rhoades <rhoades@stanford.edu>
2015-06-05 15:41:50 -0700

    change parameters

--- private/colleen/neurons_vision_to_matlab.m
+++ private/colleen/neurons_vision_to_matlab.m
@@ -1,22 +1,24 @@
 clear
-
-referenceRawDataFilePath = '/Volumes/Archive/2010-08-20-1/data001/';
+date = '2015-04-14-2';
+referenceRawDataFilePath = ['/Volumes/Data/' date, '/data008/'];
 rawDataFile = edu.ucsc.neurobiology.vision.io.RawDataFile(referenceRawDataFilePath);
 
-referenceNeuronFilePath = '/Volumes/Analysis/2010-08-20-1/data001-nwpca-copy/data001-nwpca/data001-nwpca.neurons';
+referenceNeuronFilePath = ['/Volumes/Analysis/', date, '/data008-cr/data008.neurons'];
 oldNeuronFile = edu.ucsc.neurobiology.vision.io.NeuronFile(referenceNeuronFilePath);
 ttlTimes = oldNeuronFile.getTTLTimes();
 
-newNeuronFilePath = '/Volumes/Analysis/2010-08-20-1/data001-cr/data001-cr.neurons';
+newNeuronFilePath = ['/Volumes/Analysis/', date, '/data008-cr/data008-cr.neurons'];
 newNeuronFile = initVisionNeuronFile(referenceRawDataFilePath, newNeuronFilePath, ttlTimes);
 
-referenceParamsFilePath = '/Volumes/Analysis/2010-08-20-1/data001-nwpca-copy/data001-nwpca/data001-nwpca.params';
+specify_cells = 1;
+if specify_cells == 0
+referenceParamsFilePath = ['/Volumes/Analysis/', date, '/data002-gdf/data002/data002.params'];
 datarun.names.rrs_neurons_path=[referenceNeuronFilePath];
 datarun.names.rrs_params_path=[referenceParamsFilePath];
 opt=struct('verbose',1,'load_params',1,'load_neurons',1,'load_obvius_sta_fits',false, 'load_sta', 0, 'load_sta_params', 0, 'load_all',false);
 datarun=load_data(datarun,opt);
 
-cell_type = {'OFF large 2'};
+cell_type = {'OFF parasol'};
 cell_type_index= zeros(1,size(cell_type,2));
 for num_cell_types = 1:size(cell_type,2)
     for i = 1:size(datarun.cell_types,2)
@@ -33,6 +35,27 @@ cell_specification = datarun.cell_types{cell_type_index}.cell_ids;
 toKeep = [
     cell_specification
     ];
+else
+    
+toKeep = [
+1561
+2302
+3486
+5391
+7156
+7157
+396
+2242
+3035
+3361
+4853
+6076
+6993
+7486
+7487
+7488
+    ];
+end
 
 for i = 1:length(toKeep)
     spikeTimes = oldNeuronFile.getSpikeTimes(toKeep(i));
