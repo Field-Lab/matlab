2d0cfa66ca0a3b33cbe5d8e7888ade7e6e3efdb2
Lauren Grosberg <lauren.grosberg@gmail.com>
2015-06-08 13:32:23 -0700

    Update data paths in a test script to run Gonzalo's spike sorting algorithm
    Also had to remove semi-colons in fillDefaultValues to correct the syntax in
    spike_sorting_GM/Utils/fillDefaultValues.m

--- code/projects/electrical_stim/spike_sorting_GM/Utils/fillDefaultValues.m
+++ code/projects/electrical_stim/spike_sorting_GM/Utils/fillDefaultValues.m
@@ -15,13 +15,13 @@ input.tracesInfo.I = I;
 
 input.neuronInfo.nNeurons = length(input.neuronInfo.neuronIds);
 
-try(input.params.degPolRule;)
+try(input.params.degPolRule)
     
 catch
     input.params.degPolRule=[1 2 8 17 20 25];
 end
 
-try(input.params.Tdivide;)
+try(input.params.Tdivide)
     
 catch
     input.params.Tdivide=[0 floor(input.tracesInfo.T/2) input.tracesInfo.T];
