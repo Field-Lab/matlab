126f13f8dbc583feef20edb4c2ad2e9e329e7b7d
Nora <nbrackbill@gmail.com>
2015-06-08 12:38:04 -0700

    fixed a typo in glm_parameters.m

--- code/projects/glm/glm_util/glm_parameters.m
+++ code/projects/glm/glm_util/glm_parameters.m
@@ -3,15 +3,9 @@ function [GLMT, GLMP] = glm_parameters
 %% GLM Type
 
 % Spatial Filter Type. Choose FixedSP, Rk1 or Rk2
-
-
-
-
-
 % GLMT.stimfilter_mode = 'fixedSP_rk1_linear'; GLMT.CONVEX = true;
 % GLMT.stimfilter_mode = 'rk1'; GLMT.CONVEX = false; 
 GLMT.stimfilter_mode = 'rk2'; GLMT.CONVEX = false;
-
 
 % Coupling on or off?
 GLMT.CouplingFilters = false;
@@ -49,10 +43,6 @@ GLMP.stimfilter.note2 = 'ROI_length: will also be size of spatial filter if we a
 GLMP.stimfilter.note3 = 'Frames: Time duration of the fitted stim filter in frames';
 GLMP.stimfilter.note4 = 'Frames: Time duration of the fitted stim filter in frames';
 
-
-
-
-
 % Optimization
 GLMP.optimization.tolfun   = 5;
 GLMP.optimization.tolx     = 9;
@@ -61,60 +51,27 @@ GLMP.optimization.note1    = 'tolfun: significant digits of MLE function being o
 GLMP.optimization.note2    = 'tolx: significant digits of the input variables of the MLE function';
 
 % Post-spike and Coupling Basis settings
-
-GLMPars.spikefilters.ps_note = 'parameters regarding the post-spike filter';
-GLMPars.spikefilters.cp_note = 'parameters regaring coupling filters';
-GLMPars.spikefilters.note0 = 'all parameters related to raised sinusoidal humps';
-GLMPars.spikefilters.note1 = 'basis built by prep_spikefilterbasisGP / create_histbasis as of 2014-05-3';
-GLMPars.spikefilters.ps.ms  = 100 ;      %% post spike filter time length in millisecs
-GLMPars.spikefilters.cp.ms  = 100 ;      %% cp spike filter time length in millisecs
+GLMP.spikefilters.ps_note = 'parameters regarding the post-spike filter';
+GLMP.spikefilters.cp_note = 'parameters regaring coupling filters';
+GLMP.spikefilters.note0 = 'all parameters related to raised sinusoidal humps';
+GLMP.spikefilters.note1 = 'basis built by prep_spikefilterbasisGP / create_histbasis as of 2014-05-3';
+GLMP.spikefilters.ps.ms  = 100 ;      %% post spike filter time length in millisecs
+GLMP.spikefilters.cp.ms  = 100 ;      %% cp spike filter time length in millisecs
 %GLMPars.spikefilters.spcng_psf = pi/2;  %% it could be set as pi, but pi/2 is better for "uniform" sampling.
 %GLMPars.spikefilters.spcng_cp  = pi/2;  %% it could be set as pi, but pi/2 is better for "uniform" sampling.
-GLMPars.spikefilters.BiDirect_CP     = false;
-GLMPars.spikefilters.ps.filternumber = 10;
-GLMPars.spikefilters.cp.filternumber = 8;
-GLMPars.spikefilters.ps.spacing      = pi/2;
-GLMPars.spikefilters.cp.spacing      = pi/2;
-GLMPars.spikefilters.ps.bstretch     = 0.05;
-GLMPars.spikefilters.ps.alpha        = 0;
-GLMPars.spikefilters.cp.bstretch     = 0.05;
-GLMPars.spikefilters.cp.alpha        = 0;
-GLMPars.spikefilters.ps.fratio = .5  ;  % legacy afraid to take out
-GLMPars.spikefilters.cp.fratio = .4  ;  % legacy afraid to take out
-GLMPars.spikefilters.cp.n_couplings = 6  ;
-
-GLMPars.others.fitblockchange = false;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+GLMP.spikefilters.BiDirect_CP     = false;
+GLMP.spikefilters.ps.filternumber = 10;
+GLMP.spikefilters.cp.filternumber = 8;
+GLMP.spikefilters.ps.spacing      = pi/2;
+GLMP.spikefilters.cp.spacing      = pi/2;
+GLMP.spikefilters.ps.bstretch     = 0.05;
+GLMP.spikefilters.ps.alpha        = 0;
+GLMP.spikefilters.cp.bstretch     = 0.05;
+GLMP.spikefilters.cp.alpha        = 0;
+GLMP.spikefilters.ps.fratio = .5  ;  % legacy afraid to take out
+GLMP.spikefilters.cp.fratio = .4  ;  % legacy afraid to take out
+GLMP.spikefilters.cp.n_couplings = 6  ;
+GLMP.others.fitblockchange = false;
 end
 
 
