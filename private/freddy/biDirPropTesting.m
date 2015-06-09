a6bdb4ac62693379c140f89290859801e3f7f519
Freddy Kellison-Linn <fred.kl@me.com>
2015-06-09 13:26:56 -0700

    various bug fixes

--- private/freddy/biDirPropTesting.m
+++ private/freddy/biDirPropTesting.m
@@ -1,16 +1,17 @@
-temp = load('~/git_code/matlab/private/lauren/MATLAB_code/analysis/dataset-specific/axonBundleThresholds_byPattern_2012_09_24_3_data008.mat');
-axonBundleThresholds = temp.axonBundleThresholds_byPattern_2012_09_24_3_data008;
-
 i = 1;
 estimates = [];
-for i = var(18:end)
+patterns = [1:218];
+for i = patterns(1:end)
     
-    bundleMeans = abs(getBundleVoltagesAStar('/Volumes/Analysis/2015-04-09-2/data003/', i, false));
+    bundleMeans = abs(getBundleVoltagesAStar('/Volumes/Analysis/2015-05-27-0/data002/', i, false));
     avg = mean(bundleMeans(1:5, 1));
     index = find(bundleMeans(:,1) > 20, 1, 'first');
     %index = find(bundleMeans(:,1) > 5 * avg, 1, 'first');
     disp(num2str(bundleMeans(index, 3)));
-    
-    estimates = [estimates; bundleMeans(index, 3)];
+    bMean = bundleMeans(index, 3);
+    if isempty(bMean)
+        bMean = 0;
+    end
+    estimates = [estimates; bMean];
     
 end
