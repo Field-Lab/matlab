bb650c9c9f969bad7bca7cc5b2777994302c5981
Lauren Grosberg <lauren.grosberg@gmail.com>
2015-06-09 17:23:23 -0700

    Add comments to electrical_stim spike_sorting_GM examples

--- code/projects/electrical_stim/spike_sorting_GM/Example2.m
+++ code/projects/electrical_stim/spike_sorting_GM/Example2.m
@@ -3,9 +3,25 @@ cvx_solver Mosek
 % pathToAnalysisData = '/Users/gomena/Research/EJBigData/EJ-2014-11-05-Processed/data005/';
 pathToAnalysisData = '/Volumes/Analysis/2014-11-05-3/data005/';
 
-neuronIds = [2418 2434 2913 2763];
-patternNo = 1465;
-recElecs  = [170 233 194 185];
+neuronIds = [2418 2434 2913 2763]; % Enter the neurons that you want to 
+                                   % analyze. *An elecResp file with each 
+                                   % neuron number must exist*
+patternNo = 1465;    % Stimulation pattern number. Should list just one.
+recElecs  = [170 233 194 185]; % You can list any number. If you don't list
+                               % any recElecs, then the script gets the recording
+                               % electrodes from the elecResp file. If you
+                               % list electrodes here, the script does not
+                               % use the electrodes from elecResp. Instead,
+                               % a function (prefElectrodes) is used to
+                               % assign a main recording electrode to each
+                               % of the neuronIds (chosen from this list of
+                               % recElecs). The main recording electrode
+                               % for each neuron is the electrode with the
+                               % largest signal from the visual stim
+                               % template. Other electrodes are used in the
+                               % heuristics to improve the sigmoidal fit.
+                               % Listing more recElecs is going to increase
+                               % the algorithm runtime.
 
 
 %Default minimum and maximum recording times. If the first part of the
@@ -15,26 +31,30 @@ Tmax     = 40;
 
 % Load Templates from ElecResp. If recElecs are not supplied, then will be
 % found from the 'goodElecs' field.
-[templates recElecs] = makeTemplatesFromElecResp(pathToAnalysisData,patternNo,neuronIds,recElecs);
-%translate templates if by some reason there are weird template offsets.
-templates = translateTemplate(templates,6,2,2);
+[templates, recElecs] = makeTemplatesFromElecResp(pathToAnalysisData,...
+    patternNo,neuronIds,recElecs); %This function will output one electrode per neuron
+
+% translate templates if by some reason there are weird template offsets.
+templates = translateTemplate(templates,6,2,2); % The minimum of each 
+                        % template should always align with sample point 10
 
 %for each neuron, find the 'preFerred' electrode
-prefElectrodes = PreferredElectrodes(templates);
+prefElectrodes = PreferredElectrodes(templates); % Looks for the template with the maximum signal for each neuron
 input.neuronInfo.prefElectrodes =   prefElectrodes;
 
 %Parameters to find a Axonal activation authomatically. 
 %if includeAxonBreakpoint=0 no axonal breakpoint will be included
-input.params.load.findAxon.includeAxonBreakpoint  =   1;
+input.params.load.findAxon.includeAxonBreakpoint  =   0; % For now, set to zero because the axonal breakpoint solution is not working
 input.params.load.findAxon.numberOmit             =   4;
 input.params.load.findAxon.lMovingAverage         =   1;
 input.params.load.findAxon.TrangeAxon             =  [11 40];
 %If one, look for decreases in the minimum of the eigenvalues of the
 %variance (space) of the energy plot. If equal 2 looks for increases
 input.params.load.findAxon.typeEigenvalue         =   2;
-%cleanData = 1 erases the first trial of each movie
+% cleanData = 1 erases the first trial of each movie (for cases where some
+% of the trials did not result in electrical stimulation as intended)
 input.params.load.cleanData                       =   1;
-%if collapseTrialsSameCondition = 1 Collapse movies 2*j and 2*j-1
+% if collapseTrialsSameCondition = 1 Collapse movies 2*j and 2*j-1
 input.params.load.collapseTrialsSameCondition     =   0;
 
 input.tracesInfo.Trange        =    [Tmin Tmax];
@@ -43,6 +63,7 @@ input.neuronInfo.neuronIds     =    neuronIds;
 input.neuronInfo.templates     =    templates;
 
 input = loadData(input,pathToAnalysisData,patternNo);
+
 
 input = fillDefaultValues(input);
 
--- code/projects/electrical_stim/spike_sorting_GM/Utils/fillDefaultValues.m
+++ code/projects/electrical_stim/spike_sorting_GM/Utils/fillDefaultValues.m
@@ -1,35 +1,45 @@
 function input = fillDefaultValues(input)
 
 
-input.tracesInfo.J = size(input.tracesInfo.data,1);
-input.tracesInfo.E = length(input.tracesInfo.recElecs);
-input.tracesInfo.T = size(input.tracesInfo.data{1,1},2);
+input.tracesInfo.J = size(input.tracesInfo.data,1); % Number of conditions (stimulus amps)
+input.tracesInfo.E = length(input.tracesInfo.recElecs); % Number of recording electrodes
+input.tracesInfo.T = size(input.tracesInfo.data{1,1},2); % Number of samples
 
 for j=1:input.tracesInfo.J 
-    I(j) = size(input.tracesInfo.data{j,1},1);
+    I(j) = size(input.tracesInfo.data{j,1},1); % For each amplitude condition, the number of trials may or may not be the same. Therefore the number of trials is a function of J
 end
 input.tracesInfo.I = I;
 input.neuronInfo.nNeurons = length(input.neuronInfo.neuronIds);
+input.params.sampRate = 20000;
 
-input.params.sampRate                =    20000;
-
-input.params.initial.degPolRule   =  [1 50];
+% Defines how many conditions must be within a particular breakpoint to use
+% an artifact model of various degrees. length(degPolRule) = max polynomial
+% fit minus one. For example, [1 50] uses a 0 order model for all cases
+% between breakpoints with <=1 conditions, and a 1 order (linear) model for
+% all cases with between 2 and 50 conditions.
+input.params.initial.degPolRule   =  [1 4 15 50]; % Default to include a cubic polynomial model to account 
+                                        % for axon bundle activation. Future defaults might not need a 
+                                        % cubic model if there is a better way to approximate the axon 
+                                        % bundle activation shape.%[1 50]; 
 input.params.initial.Tdivide      =  [0 floor(input.tracesInfo.T/2) input.tracesInfo.T];
 
 
 Trange = input.tracesInfo.Trange;
-input.params.initial.TfindRange        =    [Trange(1)+5 Trange(2)-5];
+input.params.initial.TfindRange        =    [Trange(1)+5 Trange(2)-5]; % Range of times for which we actually search for spikes
 input.params.initial.TfindRangeRel     =    input.params.initial.TfindRange - input.tracesInfo.Trange(1) +1;
-input.params.initial.rho               =    0;
+input.params.initial.rho               =    0; % Initialization parameter. Degree of sparseness that is imposed on the initial solution.
 
+% parameters imposed on the initial solution
 input.params.initial.Newton.beta  = 0.5;
 input.params.initial.Newton.a     = 0.5;
 input.params.initial.Newton.thres = 0.01;
 
+% don't change, decreases the sensitivity of the sigmoidal fit to low
+% probability spiking. 
 input.params.Gibbs.thresLogistic = 0.15*ones(1,input.tracesInfo.J);
 
-
-
+% heuristic parameters - addresses cases of high/low activation in
+% subsequent stimulation conditions.
 input.params.Heuristic.ResampleAlltimes = 1;
 input.params.Heuristic.ExtrapolateAlltimes = 1;
 input.params.Heuristic.ActivationThres  = 0.3;
