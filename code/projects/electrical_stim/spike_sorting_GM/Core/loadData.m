163ad4c1f7bc416e42dcb392dc719fd12f93c0a8
Gonzalo Mena <gem2131@columbia.edu>
2015-06-10 18:51:18 -0700

    New version of loadData

--- code/projects/electrical_stim/spike_sorting_GM/Core/loadData.m
+++ code/projects/electrical_stim/spike_sorting_GM/Core/loadData.m
@@ -1,11 +1,23 @@
 function input = loadData(input,pathToAnalysisData,patternNo)
+% loadData loads data from a movies adds manually an Axonal Bundle Breakpoint to the input structure, defined as the
+% last condition j for which there is no observed traveling wave following stimulation.
+%  inputs:   path:pathToAnalysisData, pattern: patternNo and input with a few
+% values (input.tracesInfo.Trange, input.params.findAxon. (see example2.m
+% input.params.load.cleanData, input.params.load.collapseTrialsSameCondition )
+%            
+% output:  The same input, with data loaded (new information in fields
+% input.tracesInfo, input.stimInfo). Traces will be available in
+% input.tracesInfo.data{e,j}
+% usage:  
+%
+% Gonzalo Mena 6/2015 
+
 
 input.names.path               =    pathToAnalysisData;
 input.stimInfo.patternNo       =    patternNo;
 
 recElecs                    = input.tracesInfo.recElecs;
 Trange                      = input.tracesInfo.Trange;
-neuronIds                   = input.neuronInfo.neuronIds;
 TrangeAxon                  = input.params.load.findAxon.TrangeAxon;
 includeAxonBreakpoint       = input.params.load.findAxon.includeAxonBreakpoint;
 cleanData                   = input.params.load.cleanData;
@@ -17,50 +29,53 @@ movieNos  = sort(movieNos);
 
 
 dataTraces    = NS_ReadPreprocessedData(pathToAnalysisData, '', 0, patternNo,...
-                movieNos(1), 99999);
+    movieNos(1), 99999);
 firstArtifact = mean(dataTraces,1);
 
 for m = 1:length(movieNos)
     
     dataTraces = NS_ReadPreprocessedData([pathToAnalysisData], '', 0, patternNo,...
-            movieNos(m), 99999);
-      
+        movieNos(m), 99999);
+    
     
     subtractionMatrix = repmat(firstArtifact,[size(dataTraces,1) 1]);
-
-    PowerMatrix = 0;
-    meanT = mean(mean(dataTraces(:,:,TrangeAxon(1):TrangeAxon(2))-subtractionMatrix(:,:,TrangeAxon(1):TrangeAxon(2)),3),1);
     
-    [~,EIm_viewT]   = ei2matrix(meanT);
-    for t = TrangeAxon(1):TrangeAxon(2)
-      
-    	meanData     = mean(dataTraces(:,:,t)-subtractionMatrix(:,:,t),1);
-        [~,EIm_view] = ei2matrix(meanData);
-        PowerMatrix  = PowerMatrix+(EIm_view-EIm_viewT).^2;
-       
-    end
+    if includeAxonBreakpoint
+        PowerMatrix = 0;
+        meanT = mean(mean(dataTraces(:,:,TrangeAxon(1):TrangeAxon(2))-subtractionMatrix(:,:,TrangeAxon(1):TrangeAxon(2)),3),1);
         
-    PowerMatrix  = PowerMatrix; 
-    
-    Powers{m}    = PowerMatrix;
-    
-    
-    for e = 1:length(recElecs)
+        
+        [~,EIm_viewT]   = ei2matrix(meanT);
+        for t = TrangeAxon(1):TrangeAxon(2)
             
+            meanData     = mean(dataTraces(:,:,t)-subtractionMatrix(:,:,t),1);
+            [~,EIm_view] = ei2matrix(meanData);
+            PowerMatrix  = PowerMatrix+(EIm_view-EIm_viewT).^2;
+            
+        end
+        
+        PowerMatrix  = PowerMatrix;
+        
+        Powers{m}    = PowerMatrix;
+        
+    end
+    for e = 1:length(recElecs)
+        
         data{m,e}=squeeze(dataTraces(:,recElecs(e),Trange(1):Trange(2)));
-     
-    end    
+        
+    end
     [amps channelsWithStim stimAmpVectors channelsConnected elecCurrentStep currentRangesUsed] = ...
         getStimAmps(pathToAnalysisData, patternNo, movieNos(m));
-     
-     listAmps(m,:)              = amps';
-     listStimElecs(m,:)         = channelsWithStim;
-     listCurrentRangesUsed(m,:) = currentRangesUsed;
-     
-
+    
+    listAmps(m,:)              = amps';
+    listStimElecs(m,:)         = channelsWithStim;
+    listCurrentRangesUsed(m,:) = currentRangesUsed;
+    
+    
 end
-
-input.tracesInfo.Powers = Powers;
+if includeAxonBreakpoint
+    input.tracesInfo.Powers = Powers;
+end
 breakStimElecs          = findBreakStimElecs(listCurrentRangesUsed);
 [breakRecElecs]         = findBreakRecElecs(breakStimElecs,recElecs,listStimElecs);
 
@@ -72,15 +87,15 @@ else
 end
 
 for e=1:length(recElecs)
-        
-    breakAxon{e}    = AxonBr; 
+    
+    breakAxon{e}    = AxonBr;
     breakPoints{e}  = unique(sort([breakRecElecs{e} breakAxon{e}]));
-
+    
 end
 
 
 if cleanData
-[data] = cleanTrials(data);
+    [data] = cleanTrials(data);
 end
 
 if collapseTrialsSameCondition
