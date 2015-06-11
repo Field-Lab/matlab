8d6d0a143a3f65b88e4c32449cb8d59fc01e3f31
Vision <vision@bertha.stanford.edu>
2015-06-11 11:51:08 -0700

    path set for _Bertha calls, wrap now has quick check call as well

--- code/projects/glm/NSEM_BaseDirectories_Bertha.m
+++ code/projects/glm/NSEM_BaseDirectories_Bertha.m
@@ -26,7 +26,7 @@ BASEDIR.BlockedSpikes         = '/Volumes/Lab/Users/akheitman/NSEM_Home/BlockedS
 BASEDIR.Cell_Selection        = '/Volumes/Lab/Users/akheitman/NSEM_Home/Cell_Selection';
 BASEDIR.GLM_output_raw        = '/Volumes/Lab/Users/akheitman/NSEM_Home/GLMOutput_Raw';
 BASEDIR.GLM_output_analysis   = '/Volumes/Lab/Users/akheitman/NSEM_Home/GLMOutput_Analysis';
-BASEDIR.GLM_codehome          = '/home/vision/akheitman/matlab/code/glm/';
+BASEDIR.GLM_codehome          = '/home/vision/akheitman/github_chichilnisky/matlab/code/projects/glm/';
 BASEDIR.GLM_troubleshootplots = sprintf('%s/troubleshootingplots', BASEDIR.GLM_codehome); 
 
 
