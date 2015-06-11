e403ae477e3671b94ab9c0193920054456482c22
Vision <vision@bertha.stanford.edu>
2015-06-11 11:30:39 -0700

    code runs on alligator

--- code/projects/glm/glm_wrap.m
+++ code/projects/glm/glm_wrap.m
@@ -38,6 +38,30 @@
 %  subR_createraster
 %  subR_concat_fitmovie_fromblockedcell
 %  subR_visionSTA_to_xymviCoord
+
+% Sample Call and Output to verify that it works
+%{
+exps = 3;
+stimtypes = [1]; % white noise only  (2 is natural scens)
+celltypes = [1]; % only ON Parasol
+cell_subset = 'debug';
+glm_settings{1}.type = 'debug';
+glm_settings{1}.name = 'true';
+runoptions.replace_existing = true;
+glm_wrap(exps,stimtypes,celltypes,cell_subset,glm_settings,runoptions)
+
+%%% Should have the following minimization sequence  
+### running: WN expC ONPar_2824: debug_fixedSP_rk1_linear_MU_PS_noCP_p8IDp8/standardparams ###
+
+                                Norm of      First-order 
+ Iteration        f(x)          step          optimality   CG-iterations
+     0            1297.56                         2e+04                
+     1            1297.56             10          2e+04           4
+     2           -42251.5            2.5       2.56e+03           0
+     3           -45320.3        4.13912       5.15e+03           7
+
+Local minimum possible.
+%}
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function glm_wrap(exps,stimtypes,celltypes,cell_subset,glm_settings, runoptions)
