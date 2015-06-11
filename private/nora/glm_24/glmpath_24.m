a00a3be55b7b89b5e074e9312e7ee9e6a9f5db4d
Colleen Rhoades <rhoades@stanford.edu>
2015-06-10 17:13:15 -0700

    glam startup file, added my own startup file stuff to it

--- private/nora/glm_24/glmpath_24.m
+++ private/nora/glm_24/glmpath_24.m
@@ -8,11 +8,13 @@ restoredefaultpath;
 % addpath(corelabpath_string)
 % NB 06-11-2014
 
+% BD = NSEM_BaseDirectories;
 
-BD = NSEM_BaseDirectories;
+glmcodedir = '/Users/colleen/matlab/private/nora/glm_24/';
+generalcodedir = '/Users/colleen/matlab/private/nora/';
 
-glmcodedir     = BD.GLM_codehome ;
-generalcodedir = BD.general_codehome;
+% glmcodedir     = BD.GLM_codehome ;
+% generalcodedir = BD.general_codehome;
 % silly path stuff
 path(sprintf('%s', glmcodedir), path)
 path(sprintf('%s/RunGLM', glmcodedir), path)
@@ -25,5 +27,42 @@ path(sprintf('%s/Metrics_Evaluation', glmcodedir), path)
 path(sprintf('%s/chaitu/mfiles', glmcodedir), path)
 path(sprintf('%s/generalcomputations_AKH', generalcodedir), path)
 
+addpath('../../utilities')
+
+% addpath(genpath2('/home/vision/alex/matlab/code', {'.svn'}));
+% addpath(genpath2('matlab/private/alex/matlab/code', {'.svn'}));
+
+% visstable()
+%addpath('/home/vision/Colleen/matlab/utilities')
+%addpath(genpath2('/home/vision/Colleen/matlab/code', {'.svn'}));
+%visstable()
+%addpath('/home/vision/alex/matlab/')
+%addpath(genpath('/home/vision/alex/matlab/private/alex/retina-subunits-master'))
+
+% Java library
+% javaaddpath('/Applications/Vision.app/Contents/Resources/Java/Vision.jar');
+javaaddpath('/Volumes/Lab/Development/vision7/Vision.app/Contents/Resources/Java/Vision.jar');
+
+% matlab code paths 
+addpath(genpath('/Users/colleen/matlab/code'));
+addpath(genpath('~/matlab'));
+
+% Add utilities so that you have genpath2.m available
+addpath(genpath('/Users/colleen/matlab/utilities'))
+
+% Use genpath2 to recursively add everything in matlab-standard to your path, but exclude .svn directories
+addpath(genpath(['../../../code']));
+
+% Local Java builds: Vision, Cell-Finder
+% We'll have to fix vision_path_stable function to point to the right place on wharf-d
+visstable();
+
+% set some default plot stuff
+set(0, 'DefaultAxesFontSize', 18, 'DefaultAxesFontName', 'Helvetica')
+% set figure background to white
+get(0, 'Factory');
+set(0, 'defaultfigurecolor', [1,1,1]);
+
+addpath(genpath('/Users/colleen/matlab/private/colleen'));
 
 clear all; close all;
