f6994d5cc722b4a875c70b707d7d2908f4996a4e
Vision <vision@bertha.stanford.edu>
2015-06-12 16:07:18 -0700

    restarted work on post-filter nonlinearity.  Good hold of parameterization of logistic, searching on logistic parameters with fmincon

--- code/projects/glm/develop/postfilterNL/track.m
+++ code/projects/glm/develop/postfilterNL/track.m
@@ -1,4 +1,22 @@
-Version 13 will focus first on building code in Develop folder
+glm_postfilterNL_LNonly
+(focus on parametrically searching the non-linearity using likelihood)
+
+Version 0:
+- 2015-06-11
+- no need to optimize offset MU .. keep as is
+- no need to play with VU term of Generalized Logistic
+- standard 2 par logistic looks like enough 
+- fix lcif.stim (0) = 1.   no gain while null space
+- still need to plot
+
+
+glm_postfilterNL_wrap:
+
+
+
+Version 13: 2015-06-11
+focus first on building code in Develop folder
+directories smoothed out
 
 Version 12 was done before 2015-06-11
 Old code which rescaled the PS Filter.
