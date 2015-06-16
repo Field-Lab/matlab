a34e4a928240f203003bda5e1a03423f977c63f2
Freddy Kellison-Linn <fred.kl@me.com>
2015-06-16 10:29:03 -0700

    add analysis script

---
+++ private/freddy/biDirPropTesting.m
@@ -0,0 +1,17 @@
+i = 1;
+estimates = [];
+patterns = [1:216];
+for i = patterns(1:end)
+    
+    bundleMeans = abs(getBundleVoltagesAStar('/Volumes/Analysis/2015-04-14-0/data002/', i, false));
+    avg = mean(bundleMeans(1:5, 1));
+    index = find(bundleMeans(:,1) > 20, 1, 'first');
+    %index = find(bundleMeans(:,1) > 5 * avg, 1, 'first');
+    disp(num2str(bundleMeans(index, 3)));
+    bMean = bundleMeans(index, 3);
+    if isempty(bMean)
+        bMean = 0;
+    end
+    estimates = [estimates; bMean];
+    
+end
