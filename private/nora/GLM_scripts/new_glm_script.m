3c3ae7e3160dd58a561f7b87028cc02ccdbfb1dc
Vision <vision@bertha.stanford.edu>
2015-06-17 19:43:28 -0700

    worked no contrast stuff on bertha

--- private/nora/GLM_scripts/new_glm_script.m
+++ private/nora/GLM_scripts/new_glm_script.m
@@ -1,21 +1,20 @@
 % addpath(genpath('/Users/Nora/Documents/MATLAB/matlab/code/projects/glm'))
 addpath(genpath('/home/vision/Nora/matlab/code/projects/glm')) %BERTHA
-
 glmpath_Lovelight
 
 %% Common changes
 
-changes{1}{2}.type = 'filter_mode';
-changes{1}{2}.name ='rk1';
+changes{1}{1}.type = 'filter_mode';
+changes{1}{1}.name ='rk1';
 
-changes{2}{2}.type = 'filter_mode';
-changes{2}{2}.name ='rk1';
+changes{1}{2}.type = 'Subunits';
+changes{1}{2}.name ='ON';
 
-% changes{1}.type = 'debug';
-% changes{1}.name ='true';
+changes{1}{3}.type = 'debug';
+changes{1}{3}.name ='false';
 
-changes{1}{1}.type = 'CouplingFilters';
-changes{1}{1}.name ='ON';
+changes{2}{1}.type = 'CouplingFilters';
+changes{2}{1}.name ='ON';
 
 changes{2}{1}.type = 'CouplingFilters';
 changes{2}{1}.name ='OFF';
@@ -32,6 +31,8 @@ changes{3}{1}.name ='ON';
 changes{4}{1}.type = 'CouplingFilters';
 changes{4}{1}.name ='OFF';
 
+runoptions.replace_existing = true;
+
 
 %% Other inputs
 experiments = [1]; % 1-4
@@ -40,7 +41,6 @@ celltypes = [1]; % 1 is ON, 2 is OFF
 cellsubset = 'all'; % options are debug, shortlist, or all;
 
 
-for changes_index = 1:2
-     glm_wrap(1,2,1,'all', changes{changes_index});
-     glm_wrap(3,2,2,'all', changes{changes_index});
+for changes_index = 1
+     glm_wrap(1,1,1,'all', changes{changes_index}, runoptions);
 end
