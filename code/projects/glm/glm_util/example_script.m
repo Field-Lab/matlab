80cc47e674477deef329d97b149e7058cb86b508
Nora <nbrackbill@gmail.com>
2015-06-18 17:34:14 -0700

    Added a plotrasters code for glm_util and fixed a scaling issue in glm_predict

---
+++ code/projects/glm/glm_util/example_script.m
@@ -0,0 +1,19 @@
+%% Fit the GLM to a white noise run
+alex_test = glm_fit_from_WN(800, '2015-03-09-2/d05-27-norefit/data008-from-d05-d27/data008-from-d05-d27', 'BW-10-8-0.48-11111-32x32');
+
+%% Look at the model output
+plotfilters(alex_test)
+
+%% Load up the Natural Scenes test movie
+idx = 1:120;
+for i = 1:30
+   load(['/Volumes/Data/Stimuli/movies/eye-movement/current_movies/NSbrownian_6000/matfiles/movie_chunk_' num2str(i) '.mat']); 
+   testmovie(:,:,idx) = movie;
+   idx = idx+120;
+end
+% Downsample and take the middle to match the white noise run
+testmovie_cut = imresize(testmovie(81:240,:,:), 0.2); 
+
+%% Make predictions!
+xval = glm_predict(alex_test, testmovie_cut);
+plotrasters(xval, alex_test)
