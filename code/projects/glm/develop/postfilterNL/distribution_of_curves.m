37152b60af8e0e54bf876034f4caeb70adb4c45a
Vision <vision@bertha.stanford.edu>
2015-06-26 18:54:09 -0700

    more NL analysis

--- code/projects/glm/develop/postfilterNL/distribution_of_curves.m
+++ code/projects/glm/develop/postfilterNL/distribution_of_curves.m
@@ -4,7 +4,7 @@ X_vec_ofK = [-4 -2 -1 -.5 0 .5 1 2 4];
 
 SP0 = linspace(-4,4,100);
 for i_K = 1:length(K_vec)
-    figure; LW = 2
+    figure; LW = 4
     for i_X0 = 1:length(X_vec_ofK)
         K = K_vec(i_K);
         X0 = X_vec_ofK(i_X0)/ K;
@@ -13,7 +13,7 @@ for i_K = 1:length(K_vec)
         ymax = 1.2* (max(logistic_vals)-1) + 1;
         
         subplot(3,3, i_X0); hold on
-        plot(SP0, logistic_vals, 'r','linewidth', LW)
+        plot(SP0, logistic_vals, 'b','linewidth', LW)
         ylim([0,ymax]);  set(gca, 'ytick', [1]);
         set(gca,'fontsize',10)
         title(sprintf('Offset: %d, Slope: %1.1e', X_vec_ofK(i_X0),K));
