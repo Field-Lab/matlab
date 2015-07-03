88bc398d6e6a444fa219863ac29c3d968acdaa33
Georges Goetz <ggoetz@stanford.edu>
2015-07-02 17:59:42 -0700

    Added error calculation to the STAs.
    Now Vision can handle STA fits with the resulting output files.

--- private/ggoetz/io/cell_array_to_vision_sta.m
+++ private/ggoetz/io/cell_array_to_vision_sta.m
@@ -1,26 +1,37 @@
-function vsta = cell_array_to_vision_sta(carr, r, stixelsz)
+function vsta = cell_array_to_vision_sta(ta, e_ta, r, stixelsz)
 %CELL_ARRAY_TO_VISION_STA Converts a cell array STA to the vision format.
 %
-%  VSTA = CELL_ARRAY_TO_VISION_STA(CARR, R) Converts the sta cell array 
-%  CARR to a format that Vision can handle. R is the refresh rate of the
-%  STA specified in seconds. Stixel size defaults to 10.
+%  VSTA = CELL_ARRAY_TO_VISION_STA(STA, E_STA R) Converts the sta cell 
+%  array STA with associated error E_TA to a format that Vision can 
+%  handle. R is the refresh rate of the STA specified in seconds. 
+%  STA Stixel size defaults to 10.
+%  E_STA can be empty.
 %
 %  VSTA = CELL_ARRAY_TO_VISION_STA(..., SZ) Further lets you specify the
 %  stixel size.
 
 import edu.ucsc.neurobiology.vision.stimulus.*
 
-if nargin == 2
+if nargin == 3
     stixelsz = 10;
 end
+if isempty(e_ta)
+    specifyerror = false;
+else
+    specifyerror = true;
+end
 
-vsta = STA(length(carr), size(carr{1},1), size(carr{1}, 2), r, stixelsz, stixelsz);
-vstaframe = STAFrame(size(carr{1},1), size(carr{1}, 2), stixelsz, stixelsz);
+vsta = STA(length(ta), size(ta{1},1), size(ta{1}, 2), r, stixelsz, stixelsz);
+vstaframe = STAFrame(size(ta{1},1), size(ta{1}, 2), stixelsz, stixelsz);
 
-for k = 1:length(carr)
-    cframe = permute(carr{k}, [3, 2, 1]);
-    vstaframe = STAFrame(size(carr{1}, 2), size(carr{1}, 1), stixelsz, stixelsz);
-    vstaframe.setBuffer(cframe(:) - 0.5);
+for k = 1:length(ta)
+    cframe = permute(ta{k}, [3, 2, 1]);
+    vstaframe = STAFrame(size(ta{1}, 2), size(ta{1}, 1), stixelsz, stixelsz);
+    vstaframe.setBuffer(cframe(:));
+    if specifyerror
+        cerror = permute(e_ta{k}, [3, 2, 1]);
+        vstaframe.setErrorBuffer(cerror(:));
+    end
     vsta.setFrame(k-1, vstaframe);
 end
 
