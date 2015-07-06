df6fe242455b52b141937353a47bf66e746bf15b
Colleen Rhoades <rhoades@stanford.edu>
2015-07-06 16:37:35 -0700

    wrote so it's more generalizable to different datasets

--- private/colleen/New Cell Types/DS/runDS.m
+++ private/colleen/New Cell Types/DS/runDS.m
@@ -18,10 +18,11 @@ clear
 %%%%%%%%%%%%%%%%%%%%%%%%%% INPUTS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Load the Data for the DS run
-run_opt.data_set = '2015-05-27-5';
-run_opt.data_run = 'data003';
+run_opt.data_set = '2008-04-30-1';
+run_opt.data_run = 'data019';
+location = 'Archive';
 
-interval = 5; % each stimulus was displayed for 5 seconds
+interval = 4; % each stimulus was displayed for 5 seconds
 
 % Where to save the data
 filepath= ['/Users/colleen/Desktop/DS/', run_opt.data_set, '/', run_opt.data_run, '/'];
@@ -29,17 +30,18 @@ filepath= ['/Users/colleen/Desktop/DS/', run_opt.data_set, '/', run_opt.data_run
 % You can give the cells as all of them (datarun{2}.cell_ids) or give
 % specific vision ids
 % Find the cell to run by mapping a large cell EI from a white noise run
-% cells = datarun{2}.cell_ids;
-cells = 18;
+cells = 5287; % 'all' or give a vector of vision cell ids
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%% END INPUTS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-datarun{1}.names.rrs_params_path=['/Volumes/Analysis/2015-05-27-5/', run_opt.data_run, '/', run_opt.data_run, '.params'];
-datarun{2}.names.rrs_neurons_path=['/Volumes/Analysis/2015-05-27-5/', run_opt.data_run, '/', run_opt.data_run, '.neurons'];
-datarun{2}.names.stimulus_path=['/Volumes/Data/2015-05-27-5/Visual/s', run_opt.data_run(end-1:end)];
-opt=struct('verbose',1,'load_params',1,'load_neurons',1,'load_obvius_sta_fits',true);
+datarun{1}.names.rrs_params_path=['/Volumes/Analysis/', run_opt.data_set, '/', run_opt.data_run, '/', run_opt.data_run, '.params'];
+datarun{1}.names.rrs_sta_path=['/Volumes/Analysis/', run_opt.data_set, '/', run_opt.data_run, '/', run_opt.data_run, '.sta'];
+
+datarun{2}.names.rrs_neurons_path=['/Volumes/Analysis/', run_opt.data_set, '/', run_opt.data_run, '/', run_opt.data_run, '.neurons'];
+datarun{2}.names.stimulus_path=['/Volumes/',location,'/', run_opt.data_set, '/Visual/s', run_opt.data_run(end-1:end)];
+opt=struct('verbose',1,'load_sta', 1,'load_params',1,'load_neurons',1,'load_obvius_sta_fits',true);
 
 datarun=load_data(datarun,opt);
 datarun=map_cell_types(datarun, struct('map',[1 2],'verbose',true));
@@ -47,6 +49,11 @@ datarun=map_cell_types(datarun, struct('map',[1 2],'verbose',true));
 % If the trigger interval in LabView is set long enought (~6 seconds for 5
 % second stimuli), then this trigger_iti_thr should be fine.
 datarun{2}=load_stim(datarun{2},'correction_incomplet_run', 0, 'trigger_iti_thr', 0.1); % manually set threshold until got right number of triggers
+
+if strcmp(cells, 'all')
+    cells = datarun{2}.cell_ids;
+end
+
 
 spatial_tested = fliplr(datarun{2}.stimulus.params.SPATIAL_PERIOD);
 temporal_tested = fliplr(datarun{2}.stimulus.params.TEMPORAL_PERIOD);
@@ -67,11 +74,13 @@ for x = 1:length(cells)
     % Get spikes times for the specified cell   
     n = datarun{2}.spikes{cell_indices2(1)}';
     psth_r = [];
-    for z = 1:size(tr,2)
+    for z = 25:size(tr,2) % skip the first 12 trials due to contrast adapation
         ind = find(tr(z) == datarun{2}.triggers);
         spatial = datarun{2}.stimulus.trials(z).SPATIAL_PERIOD;
         temporal = datarun{2}.stimulus.trials(z).TEMPORAL_PERIOD;
         direction = datarun{2}.stimulus.trials(z).DIRECTION;
+        rgb = datarun{2}.stimulus.trials(z).RGB;
+           
         num_triggers = interval/(temporal/120); % Number of times a band passed over one spot
         sub_spacing= datarun{2}.triggers(ind:ind+num_triggers) - datarun{2}.triggers(ind); % How to divide by the trial into the timing of the bands
         start = 0;
@@ -89,9 +98,20 @@ for x = 1:length(cells)
             
         end
         psth_mat{z} = psth_r; % Put all the psths from all the temporal, spatial and direction combinations into a cell array
+               if rgb(1) == 0.48
+                   psth_mat{z} = [];
+               end
+               
         psth_r = [];
         
     end
+    
+
+%     for i = 25:size(tr,2)
+%         figure
+%         plot(psth_mat{i}(:,1), psth_mat{i}(:,2), 'o')
+%     end
+    
     
     % Organize the data into [direction, spatial, temporal, number of spikes in
     % 5 second trial]
@@ -105,7 +125,6 @@ for x = 1:length(cells)
     end
     
     % Plot the data separated out by either temporal or spatial period
-    spat64 = find(by_trial(:,3) == temporal_tested(1));
     fig = figure;
     set(fig,'PaperPositionMode','auto')
     set(fig, 'PaperSize', [12 8], 'PaperPosition', [0 0 12,8]);
@@ -114,84 +133,40 @@ for x = 1:length(cells)
     set(fig, 'PaperPosition', [0 0 12 8]);
     
     
-    for i = spat64
-        subplot(2,3,1)
-        plot(by_trial(i,1), by_trial(i,4), 'k.')
-        hold on
-    end
-    title(['Temporal = ', num2str(temporal_tested(1))])
+    for t = 1:length(temporal_tested)
+            spat64 = find(by_trial(:,3) == temporal_tested(t));
+
+        for i = spat64
+            subplot(2,max(length(temporal_tested), length(spatial_tested)),t)
+            plot(by_trial(i,1), by_trial(i,4), 'k.')
+            hold on
+        end
+    title(['Temporal = ', num2str(temporal_tested(t))])
     xlabel('Direction')
     ylabel('Spikes count during stimulus')
     
     clear spat64
     
-    spat64 = find(by_trial(:,3) == temporal_tested(2));
+    end
+    
+   
+   for t = 1:length(spatial_tested)
+
+    spat64 = find(by_trial(:,2) == spatial_tested(t));
     for i = spat64
-        subplot(2,3,2)
+        subplot(2,max(length(temporal_tested), length(spatial_tested)),t + max(length(temporal_tested), length(spatial_tested)))
         
         plot(by_trial(i,1), by_trial(i,4), 'k.')
         hold on
     end
-    
-    title(['Temporal = ', num2str(temporal_tested(2))])
-    xlabel('Direction')
-    ylabel('Spikes count during stimulus')
-    clear spat64
-    
-    
-    spat64 = find(by_trial(:,3) == temporal_tested(3));
-    for i = spat64
-        subplot(2,3,3)
-        
-        plot(by_trial(i,1), by_trial(i,4), 'k.')
-        hold on
-    end
-    
-    title(['Temporal = ', num2str(temporal_tested(3))])
+    title(['Spatial = ', num2str(spatial_tested(t))])
     xlabel('Direction')
     ylabel('Spikes count during stimulus')
     
     clear spat64
-    
-    spat64 = find(by_trial(:,2) == spatial_tested(1));
-    for i = spat64
-        subplot(2,3,4)
-        
-        plot(by_trial(i,1), by_trial(i,4), 'k.')
-        hold on
-    end
-    title(['Spatial = ', num2str(spatial_tested(1))])
-    xlabel('Direction')
-    ylabel('Spikes count during stimulus')
-    
-    clear spat64
-    
-    spat64 = find(by_trial(:,2) == spatial_tested(2));
-    
-    for i = spat64
-        subplot(2,3,5)
-        
-        plot(by_trial(i,1), by_trial(i,4), 'k.')
-        hold on
-    end
-    title(['Spatial = ', num2str(spatial_tested(2))])
-    xlabel('Direction')
-    ylabel('Spikes count during stimulus')
-    
-    clear spat64
-    
-    
-    spat64 = find(by_trial(:,2) == spatial_tested(3));
-    
-    for i = spat64
-        subplot(2,3,6)
-        
-        plot(by_trial(i,1), by_trial(i,4), 'k.')
-        hold on
-    end
-    title(['Spatial = ', num2str(spatial_tested(3))])
-    xlabel('Direction')
-    ylabel('Spikes count during stimulus')
+   end
+   
+   
     
     suptitle({run_opt.data_set;['Cell ', num2str(cell_to_run)]});
     
@@ -204,5 +179,5 @@ for x = 1:length(cells)
     
     export_fig([filepath, 'Cell_',num2str(cell_to_run)], '-pdf')
     % print(gcf,[filepath, 'Cell_',num2str(cell_to_run)], '-depsc', '-opengl');
-    close(gcf);
+%     close(gcf);
 end
