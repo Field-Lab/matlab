5ed5b54aab1986e7e81a5bf74839b4c6f619d961
Vision <vision@bertha.stanford.edu>
2015-07-19 17:52:16 -0700

    imported postfilterNL subRoutines from develop folder into main code

--- code/projects/glm/glm_execute_OutputNL.m
+++ code/projects/glm/glm_execute_OutputNL.m
@@ -20,6 +20,12 @@ pstar = fittedGLM.rawfit.opt_params;
 [lcif_test.stim.preNL ] = subR_findobj_lcifstim([], pstar, ...
     fittedGLM.GLMType, fittedGLM.GLMPars, [], testmovie, inputstats, glm_cellinfo, t_bin,bins); 
 
+
+t_bin        = t_bin;
+home_sptimes = fitspikes.home';
+home_spbins  = ceil(home_sptimes / t_bin);
+home_spbins  = home_spbins(find(home_spbins < bins) );
+
 display(sprintf('objective value from preNL fitted GLM::  %d',fittedGLM.rawfit.objective_val))
 display(sprintf('objective value subR should be ~ equal::  %d',objval));              
 
@@ -33,47 +39,81 @@ NL_Input.input_test = lcif_test.stim.preNL / std(lcif_fit.stim.preNL);
 
 if strcmp(postfilter_NL.type, 'Logistic_2Par_fixMU')    
     NL_Input.y_int              = exp( lcif_fit.nonstim.components.mu(1) );
-    NL_Input.rawfilteroutput    = baseGLM.lcif_fit.stim;
-    NL_Input.scale_rawtoNLinput = std(baseGLM.lcif_fit.stim);
+    NL_Input.rawfilteroutput    = lcif_fit.stim.preNL;
+    NL_Input.scale_rawtoNLinput = std(lcif_fit.stim.preNL);
     
-    
-
-end
-
-end
-function [lcif_stim] = subR_lcifstim_fittedGLM(pstar, GLMType,GLMPars,fitmovie,inputstats,glm_cellinfo)
-
-if isfield(GLMType, 'specialchange') && GLMType.specialchange
-    GLMPars = GLMParams(GLMType.specialchange_name);
-end
-frames = size(fitmovie,3);
-bins   = frames * GLMPars.bins_per_frame;
-t_bin  = glm_cellinfo.computedtstim / GLMPars.bins_per_frame; % USE THIS tstim!! %
-fittedGLM.t_bin = t_bin;
-fittedGLM.bins_per_frame = GLMPars.bins_per_frame;
-clear bin_size basis_params
-% PREPARE PARAMETERS
-[paramind] =  prep_paramindGP(GLMType, GLMPars); 
-
-% ORGANIZE STIMULUS COVARIATES
-center_coord       = glm_cellinfo.slave_centercoord;
-WN_STA             = double(glm_cellinfo.WN_STA);
-[X_frame,X_bin]    = prep_stimcelldependentGPXV(GLMType, GLMPars, fitmovie, inputstats, center_coord, WN_STA);
-clear WN_STA center_coord
-if GLMType.CONVEX
-    glm_covariate_vec = NaN(paramind.paramcount , bins );  % make sure it crasheds if not filled out properly
-    % Maybe move this inside of the stimulus preparation % 
-    bpf         = GLMPars.bins_per_frame;
-    shifts      = 0:bpf:(GLMPars.stimfilter.frames-1)*bpf;
-    if isfield(GLMPars.stimfilter,'frames_negative')
-        shifts = -(GLMPars.stimfilter.frames_negative)*bpf:bpf:(GLMPars.stimfilter.frames-1)*bpf;
+    if isfield(lcif_fit.nonstim.components, 'ps')
+        NL_Input.fit_lcif_additionaldrive = lcif_fit.nonstim.components.ps;
+    else
+        NL_Input.fit_lcif_additionaldrive = 0;
     end
-    X_bin_shift = prep_timeshift(X_bin,shifts);
-    pstar = pstar';
-    lcif_stim = pstar(paramind.X) *X_bin_shift;
-end
+    NL_Output = subR_LogFixMu_fmincon(NL_Input,home_spbins,t_bin);
+    
+    
 end
 
+end
+
+
+function NLOutput = subR_LogFixMu_fmincon(NL_Input,home_spbins,t_bin)
+% 2015-06-24  AKHeitman
+
+% NL_Input needs: y_int (value with no drive)
+%                input_test(corresponding input to the NL for fit data)
+%                input_fit(corresponding input to the NL for test data) 
+
+optim_struct = optimset(...
+                    'derivativecheck','off','diagnostics','off',...  % 
+                    'display','iter','funvalcheck','off',... 
+                    'MaxIter',100,'TolFun',10^(-6),'TolX',10^(-9) );
+lowerbound = [NL_Input.y_int+1 .1];
+upperbound = [1000         100];
+LOGI_Params0 = [100,1];
+[LOGI_Params_Opt, new_objval, eflag, output] = fmincon(@(LOGI_Params) subR_objval_LOGISTIC...
+    (LOGI_Params, NL_Input.y_int,NL_Input.input_fit, NL_Input.fit_lcif_additionaldrive, home_spbins,t_bin),...
+    LOGI_Params0,[],[],[],[],lowerbound,upperbound,[],optim_struct);
+
+[~, lcif_LOGI_crossvaltest] =  subR_objval_LOGISTIC(LOGI_Params_Opt,...
+    NL_Input.y_int,NL_Input.input_test, 0, [],t_bin); 
+
+[~, lcif_LOGI_fit] =  subR_objval_LOGISTIC(LOGI_Params_Opt,...
+    NL_Input.y_int,NL_Input.input_fit, 0, home_spbins,t_bin); 
+
+
+NLOutput.note_metric = 'Logistic with Null Rate fixed to tonic drive. Driven by linear filter output,normalized to std 1';
+NLOutput.param_string = sprintf('2 Params fit with fmincon,  Optimal Max Rate: %1.1e, Optimal Slope: %1.1e',...
+    LOGI_Params_Opt(1), LOGI_Params_Opt(2) );
+NLOutput.maxrate = LOGI_Params_Opt(1);
+NLOutput.slope   = LOGI_Params_Opt(2);
+NLOutput.fmincon_output = output;
+NLOutput.eflag = eflag;
+NLOutput.crossvaltest_finalrate = exp(lcif_LOGI_crossvaltest);
+NLOutput.new_objval = new_objval;
+NLOutput.fit_rate = exp(lcif_LOGI_fit);
+NLOutput.codename = mfilename('fullpath');
+clear lcif_LOGI_crossvaltest dummy lowerbound upperbound LOGI_Params0
+% Check Code
+%{
+lcif   = baseGLM.lcif_fit.stim + baseGLM.lcif_fit.mu; 
+cif    = exp(lcif);
+objval = -( sum( lcif(home_spbins) ) - t_bin * sum(cif) );
+pars = [10,1];
+objval = objval_LOGISTIC(pars,NL_Input.y_int,NL_Input.input_fit, 0, home_spbins,t_bin)
+%}
+end
+function [objval lcif_LOGI] = subR_objval_LOGISTIC(LOGI_PARAMS, Y_INT, lcif_intoLOGI, lcif_ext, spikebins,t_bin)
+MAX     = LOGI_PARAMS(1);
+RATE    = LOGI_PARAMS(2);
+
+
+OFFSET  = log( (MAX/Y_INT) - 1  ) / RATE;                    
+
+lcif_LOGI = log(MAX ./ (1 + exp(-RATE * (lcif_intoLOGI- OFFSET) )));
+
+lcif   = lcif_LOGI + lcif_ext;
+cif    = exp(lcif);
+objval = -( sum( lcif(spikebins) ) - t_bin * sum(cif) );
+end
 function [lcif_stim, obj_val] = subR_findobj_lcifstim(lcif_external, pstar, GLMType, GLMPars, fitspikes, fitmovie, inputstats, glm_cellinfo, t_bin,bins)
 % AKHEITMAN 2015-07-15
 % subRoutine which will get optimized for inding input NL
@@ -172,10 +212,6 @@ if nargout > 1
 end
 
 end
-
-
-
-
 function [lcif_nonstim] = subR_lcif_nonstim(pstar, GLMType,GLMPars,fitspikes,t_bin,bins)
 % AKHEITMAN 2015-07-15  extract non-stim components of lcif
 
