1d7d6c3a0846e2d33e158f439da93167ee6445d1
Georges Goetz <ggoetz@stanford.edu>
2015-07-30 11:56:38 -0700

    Added rescaling option for STAs calculated from rawmovies.

--- private/ggoetz/io/cell_array_to_vision_sta.m
+++ private/ggoetz/io/cell_array_to_vision_sta.m
@@ -21,14 +21,38 @@ else
     specifyerror = true;
 end
 
+% Check if the matlab STA was between 0 and 255. 
+if max(ta{1}(:)) > 10;
+    rescale_sta = true;
+else
+    rescale_sta = false;
+end
+
+% Check if the matlab STA was RGB or grayscale
+if size(ta{1}, 3) == 1
+    for k = 1:length(ta)
+        ta{k} = repmat(ta{k}, [1, 1, 3]);
+        e_ta{k} = repmat(e_ta{k}, [1, 1, 3]);
+    end
+end
+
 vsta = STA(length(ta), size(ta{1},1), size(ta{1}, 2), r, stixelsz, stixelsz);
 
 for k = 1:length(ta)
+    % Set STA
     cframe = permute(ta{k}, [3, 2, 1]);
+    if rescale_sta
+        cframe = (cframe - 127);
+    end
     vstaframe = STAFrame(size(ta{1}, 2), size(ta{1}, 1), stixelsz, stixelsz);
     vstaframe.setBuffer(cframe(:));
+    
+    % Set error
     if specifyerror
         cerror = permute(e_ta{k}, [3, 2, 1]);
+%         if rescale_sta
+%             cerror = cerror; 
+%         end
         vstaframe.setErrorBuffer(cerror(:));
     end
     vsta.setFrame(k-1, vstaframe);
