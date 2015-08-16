0a05dc063d55412aa0a28c19fceb368ab7377db6
Colleen Rhoades <rhoades@stanford.edu>
2015-08-15 18:41:29 -0700

    prepare to give to alex

---
+++ private/colleen/New Cell Types/Pink noise/fit_cells.m
@@ -0,0 +1,146 @@
+clear
+close all
+
+dataparam.date='2011-10-25-8';
+dataparam.concatname='data006';
+
+
+dataparam.file_name_right = [dataparam.date, '/', dataparam.concatname,'/', dataparam.concatname];
+
+dataparam.cell_type = {'unclassified'};
+
+fitparam.num_frames = 30;
+mark_params.thresh = 2; %threshold for significant stixels;
+num_gauss = 1.2; % number of SD in fit
+
+
+% list specific cell (1), or run for a whole cell type (0)
+select_cells = 0;
+if select_cells == 1
+    dataparam.cell_specification = [245] %ON parasol
+end
+
+%% END OF INPUT
+dataparam.folder = dataparam.cell_type{1};
+% file path to save data and pictures
+dataparam.filepath=['/Users/colleen/Desktop/Fitting/',dataparam.date,'/',dataparam.concatname,'/'];
+if ~exist([dataparam.filepath,dataparam.folder],'dir')
+    mkdir([dataparam.filepath,dataparam.folder]);
+end
+
+% Right Movie
+datarun2.names.rrs_neurons_path=['/Volumes/Analysis/', dataparam.file_name_right, '.neurons'];
+datarun2.names.rrs_params_path=['/Volumes/Analysis/', dataparam.file_name_right, '.params'];
+datarun2.names.rrs_sta_path = ['/Volumes/Analysis/', dataparam.file_name_right, '.sta'];
+
+
+
+%% Load Data2
+slashes = strfind(datarun2.names.rrs_neurons_path, '/');
+dataset = datarun2.names.rrs_neurons_path(slashes(3)+1:slashes(5)-1);
+to_replace = strfind(dataset, '/');
+dataparam.dataset(to_replace) = '-';
+
+opt=struct('verbose',1,'load_params',1,'load_neurons',1,'load_obvius_sta_fits',true, 'load_sta', 1, 'load_sta_params', 1, 'load_all',true);
+opt.load_sta_params.save_rf = 1;
+opt.load_sta_params.frames = 1:fitparam.num_frames;% have to input as a vector list of frames, not the number of frames total, counting backwards
+datarun2=load_data(datarun2,opt);
+
+
+
+
+%% Cell indicies
+
+% Find the type of the inputted cells
+cell_type_index= zeros(1,size(dataparam.cell_type,2));
+for num_cell_types = 1:size(dataparam.cell_type,2)
+    for i = 1:size(datarun2.cell_types,2)
+        right_cell_type = strcmpi(datarun2.cell_types{i}.name, dataparam.cell_type{num_cell_types}); % case insensitive
+        if right_cell_type == 1;
+            cell_type_index(num_cell_types) = i;
+            break
+        end
+        cell_type_index(num_cell_types) = 0;% couldn't find the right cell type
+    end
+    
+end
+
+% Set the cell_specification to all the cell of the inputted type
+if select_cells ~= 1
+    dataparam.cell_specification = datarun2.cell_types{cell_type_index}.cell_ids;
+end
+
+
+cell_indices = get_cell_indices(datarun2, dataparam.cell_specification);
+num_rgcs = length(cell_indices);
+
+paramPath = ['/Volumes/Analysis/', dataparam.date, '/', dataparam.concatname,'/', dataparam.concatname, '.params'];
+
+paramFile = edu.ucsc.neurobiology.vision.io.ParametersFile(paramPath);
+
+for rgc = 1:num_rgcs
+    
+    mark_params.select = 'thresh';
+    
+    fprintf('fitting the STA for cell %d... \n', datarun2.cell_ids(cell_indices(rgc)))
+    
+    % Get the STA from the right movie
+    temp_sta = datarun2.stas.stas{cell_indices(rgc)};
+    [final_fit_params, x,y] = fit_just_spatial(temp_sta, num_gauss, mark_params);
+    h = final_fit_params(1);
+    k = final_fit_params(2);
+    a = num_gauss*final_fit_params(3);
+    b = num_gauss*final_fit_params(4);
+    angle = final_fit_params(5);
+    A = final_fit_params(5);
+    
+    xhat = (x - h)*cos(angle) - (y-k)*sin(angle);
+    yhat = (x - h)*sin(angle) + (y-k)*cos(angle);
+    U = (xhat/a).^2 + (yhat/b).^2;
+    F = A*exp(-U/2);
+    
+    fit_shaped = reshape(F, size(temp_sta,1),size(temp_sta,2));
+
+    rf = rf_from_sta(temp_sta);
+    if size(rf,3) > 1
+        sta_one = rf(:,:,2);
+    else
+        sta_one =rf;
+    end
+    
+    sta = temp_sta;
+
+    paramFile.setCell(datarun2.cell_ids(cell_indices(rgc)), 'SigmaY', b);
+    paramFile.setCell(datarun2.cell_ids(cell_indices(rgc)), 'SigmaX', a);
+    paramFile.setCell(datarun2.cell_ids(cell_indices(rgc)), 'x0', (h));
+    paramFile.setCell(datarun2.cell_ids(cell_indices(rgc)), 'y0', size(sta,1) - (k));
+    
+    paramFile.setCell(datarun2.cell_ids(cell_indices(rgc)), 'Theta',  angle); % set all of them to 0 because of plotting problems
+    sig_stixels = significant_stixels(double(sta), 'select', 'thresh', 'thresh',mark_params.thresh);
+    if sum(full(sig_stixels)) == 0
+        paramFile.setCell(datarun2.cell_ids(cell_indices(rgc)), 'RedTimeCourse', nan(fitparam.num_frames,1));
+        paramFile.setCell(datarun2.cell_ids(cell_indices(rgc)), 'GreenTimeCourse', nan(fitparam.num_frames,1));
+        paramFile.setCell(datarun2.cell_ids(cell_indices(rgc)), 'BlueTimeCourse', nan(fitparam.num_frames,1));
+        
+    else
+        
+        tc = time_course_from_sta(sta,sig_stixels );
+        norm_factor = max(abs(reshape(tc, 1, [])));
+        tc = tc ./ norm_factor;
+        
+        if size(sta, 3) == 3
+            paramFile.setCell(datarun2.cell_ids(cell_indices(rgc)), 'RedTimeCourse', tc(:,1));
+            paramFile.setCell(datarun2.cell_ids(cell_indices(rgc)), 'GreenTimeCourse', tc(:,2));
+            paramFile.setCell(datarun2.cell_ids(cell_indices(rgc)), 'BlueTimeCourse', tc(:,3));
+        else
+            paramFile.setCell(datarun2.cell_ids(cell_indices(rgc)), 'RedTimeCourse', tc(:,1));
+            paramFile.setCell(datarun2.cell_ids(cell_indices(rgc)), 'GreenTimeCourse', tc(:,1));
+            paramFile.setCell(datarun2.cell_ids(cell_indices(rgc)), 'BlueTimeCourse', tc(:,1));
+            %         paramFile.setCell(datarun2.cell_ids(cell_indices(rgc)), 'blueness', 0);
+        end
+    end
+   
+    
+    
+end
+paramFile.close(1);
--- private/colleen/New Cell Types/Pink noise/fit_just_spatial.m
+++ private/colleen/New Cell Types/Pink noise/fit_just_spatial.m
@@ -1,10 +1,5 @@
 
 function [final_fit_params,x ,y] = fit_just_spatial(sta, num_gauss, mark_params)
-% 
-% sta = datarun.stas.stas{1};
-% 
-% figure; imagesc(sta(:,:,2,25))
-
 
 [x,y]=meshgrid(1:size(sta,2),1:size(sta,1));
 x=x(:);
@@ -30,8 +25,8 @@ if sum(full(sig_stixels(:))) == 0
     final_fit_params = nan(6,1);
     return
 end
-          biggestBlob = ExtractNLargestBlobs(full(sig_stixels), 1);
-        real_stix = biggestBlob;
+biggestBlob = ExtractNLargestBlobs(full(sig_stixels), 1);
+real_stix = biggestBlob;
 [i,j] =find(real_stix);
 
 sta_one = sta_one/max(sta_one(:));
@@ -63,55 +58,6 @@ xhat = (x - h)*cos(angle) - (y-k)*sin(angle);
 yhat = (x - h)*sin(angle) + (y-k)*cos(angle);
 U = (xhat/a).^2 + (yhat/b).^2;
 F = A*exp(-U/2);
-                    
-fit_shaped = reshape(F, size(sta_one,1),size(sta_one,2));
-% figure; imagesc(fit_shaped)
-%  drawEllipse(h, k, a, b, -angle)
-% 
-%  axis equal
-% 
-% figure; imagesc(sta_one)
-% 
-%  f = drawEllipse(h, k, a, b, -angle);
-%  set(f, 'LineWidth', 1.5, 'Color', [0 0 0 ])
-%  axis equal
 
 
- 
- 
-% 
-%  f(x,y) = a1*exp(-(x-x0)^2/(2*sigmax^2)-(y-y0)^2/(2*sigmay^2))
-%  
-%  [final_fit_params, fval] = fminsearch(@(fit_params)sta_fit_error(sta,...
-%                               fit_params, input_params(fixed_indices),...
-%                               fit_indices, fixed_indices,...
-%                               verbose, mark_params),...
-%                               input_params(fit_indices),...
-%                               optimset(optim{:}));
-%                           
-%                           
-% % sta_one = sta_one(:,41:80);
-% 
-% % sig_stixels = significant_stixels(sta_one);
-% % [a,b] = find(sig_stixels);
-% 
-% % y = size(rf,1);
-% % x = size(rf,2);
-% 
-% % x1 = sort(repmat([1:x]', [y, 1]));
-% % y1 = repmat([1:y]', [x,1]);
-% 
-% pairs = [a, b];
-% a'*a
-% % [zb, ab, bb, alphab] = fitellipse(pairs)
-% % A = [x1'*x1 x1'*y1 y1'*x1 y1'*y1]
-% % 
-% % b = -log(permute(rf(:,:,2), [2,1]))
-% % sol = A\b
-% % sta_one = squeeze(sta(:,:,2,25));
-% 
-% % sta_reshape = reshape(sta_one, size(sta_one,1)*size(sta_one,2), size(sta_one,3))
-%      % Plot the results
-%       plot(pairs(1,:), pairs(2,:), 'ro')
-%       hold on
-%       plotellipse(zb, ab, bb, alphab, 'b--')
+
