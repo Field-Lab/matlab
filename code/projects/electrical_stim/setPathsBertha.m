328166f0c0a71bc6adee98d26ca06ab7f016a47d
Lauren Grosberg <lauren.grosberg@gmail.com>
2015-09-16 16:17:30 -0700

    Updates matlab search path on /Lab/Volumes/Users/grosberg

--- code/projects/electrical_stim/setPathsBertha.m
+++ code/projects/electrical_stim/setPathsBertha.m
@@ -5,6 +5,7 @@ javaaddpath('/Volumes/Lab/Applications/Vision.app/Contents/Resources/Java/Vision
 % matlab code paths 
 addpath(genpath('/Volumes/Lab/Users/grosberg/matlab/code'));
 addpath(genpath('/Volumes/Lab/Users/grosberg/matlab/private/lgrosberg'));
+addpath(genpath('/Volumes/Lab/Users/grosberg/direwolf_matlab')); 
 
 set(0,'DefaultAxesFontSize',18,'DefaultAxesFontName','Myriad Pro');
 set(0,'DefaultFigureColor','w'); 
