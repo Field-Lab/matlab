cd59463e2ff33d21aa61830cea3ff58d8beb3aa9
Nora <nbrackbill@gmail.com>
2015-11-04 00:25:43 -0800

    worked on amacrine viz

--- private/nora/amacrine/amacrine_wave_dynamics.m
+++ private/nora/amacrine/amacrine_wave_dynamics.m
@@ -4,10 +4,12 @@ datarun_class = load_data('2015-10-06-0/data000-data015-norefit/data000-from-dat
 datarun = load_neurons(datarun);
 datarun_class = load_params(datarun_class);
 datarun_class = load_ei(datarun_class, 'Off Amacrine nc0');
-[x,y]= getElectrodeCoords512;
 cid = get_cell_indices(datarun_class, 'Off Amacrine nc0');
 prepped_data = interleaved_data_prep(datarun, [3600*2 3600], 50, 'cell_spec', 'Off Amacrine nc0', 'datarun_class', datarun_class, 'testmovie_only', 1, 'stimulus_name', 'NSbrownian_3000_A_025.rawMovie');
-%%
+
+
+%% EI approach
+[x,y]= getElectrodeCoords512;
 electrical_activity = zeros(512, 3700);
 trial = 3;
 for i_cell=1:length(cid)
@@ -17,8 +19,6 @@ for i_cell=1:length(cid)
         electrical_activity(:,(spike_frame:spike_frame+64)) = electrical_activity(:,(spike_frame:spike_frame+64))+EI_cell;
     end
 end
-
-%%
 for i=300:460
 scatter(x,y,0.5*abs(electrical_activity(:,i))+1,'filled')
 axis image
@@ -26,3 +26,35 @@ axis off
 title(['Frame ' num2str(i)])
 pause(0.01)
 end
+
+%% surfplot approach
+spike = zeros(1800,length(cid));
+centers = zeros(2, length(cid));
+spikes_bin = zeros(1800,length(cid));
+for i_cell=1:length(cid)
+    spikes = floor(120*cell2mat(prepped_data.testspikes(:,i_cell)));
+    centers(:,i_cell) = flip(datarun_class.vision.sta_fits{cid(i_cell)}.mean);
+    for i = 1:1800
+        spikes_bin(i,i_cell) = sum(spikes == i);
+    end
+end
+spikes_bin = conv2(spikes_bin, gausswin(10), 'same');
+
+%%
+start = 360;
+time = (0:40) + start;
+plot(spikes_bin(time,:)+50*repmat(centers(1,:), [length(time),1]))
+
+%% interp approach
+spike = [0 0 0 0]; 
+%centers = zeros(2, length(cid));
+spikes_bin = zeros(1800,1);
+for i_cell=1:length(cid)
+    spikes = floor(120*cell2mat(prepped_data.testspikes(:,i_cell)));
+    centers(:,i_cell) = flip(datarun_class.vision.sta_fits{cid(i_cell)}.mean);
+    for i = 1:1800
+        spikes_bin(i) = sum(spikes == i);
+    end
+    spike = [ spike; spikes_bin, centers(1)*ones(1800,1), centers(2)*ones(1800,1), (1:1800)']; 
+end
+spikes_bin = conv2(spikes_bin, gausswin(10), 'same');
--- private/nora/max_stim/amacrine_wave_dynamics.m
+++ private/nora/max_stim/amacrine_wave_dynamics.m
@@ -17,15 +17,13 @@ datarun_LES = load_data('2015-10-29-2/map-from-data036-stream/data043/data043');
 cells = [753,2134,2687,3677,4246,5222,6707];
 type = {'on','on','on','off','off','on','off'};
 %}
-%%{
+%{
 datarun = load_data('2015-10-29-7/mapped-from-data001-streamed/data004/data004'); % masked 
 datarun_LES = load_data('2015-10-29-7/mapped-from-data001-streamed/data005/data005'); % LES 
-%cells = [1411,2072,3152,4397,6181,7217];
-cells = [61];
+cells = [1411,2072,3152,4397,6181,7217];
 type = {'on','off','on','off','on','off'};
 %}
-
-%{
+%%{
 datarun = load_data('2015-10-29-7/mapped-from-data001-streamed/data006/data006'); % masked 
 datarun_LES = load_data('2015-10-29-7/mapped-from-data001-streamed/data007/data007'); % LES 
 cells = [1727,2120,3616,4611,5778,7608];
@@ -34,8 +32,8 @@ type = {'off','large','off','large','off','on'};
 
 datarun = load_neurons(datarun);
 datarun_LES = load_neurons(datarun_LES);
-mask = interleaved_data_prep(datarun, 1800, 40, 'cell_spec', cells, 'visual_check', 1);
-LES = interleaved_data_prep(datarun_LES, 1800, 40, 'cell_spec', cells, 'visual_check', 1);
+mask = interleaved_data_prep(datarun, 1800, 40, 'cell_spec', cells, 'visual_check', 0);
+LES = interleaved_data_prep(datarun_LES, 1800, 40, 'cell_spec', cells, 'visual_check', 0);
 
 bins_per_frame = 1;
 bins = 1800*bins_per_frame;
@@ -50,8 +48,8 @@ for i_cell = 1:6
         PSTH_mask(i,i_cell) = sum(spike_frames_mask == i);
     end
 end
-%PSTH_LES = conv2(PSTH_LES, gausswin(30), 'same');
-%PSTH_mask = conv2(PSTH_mask, gausswin(30), 'same');
+PSTH_LES = conv2(PSTH_LES, gausswin(10), 'same');
+PSTH_mask = conv2(PSTH_mask, gausswin(10), 'same');
 
 %%
 for i = 1:100
@@ -66,7 +64,7 @@ interval_time = cumsum(interval/120);
 %%
 hFig1=figure;
 default_colors = get(gca,'ColorOrder');
-set(hFig1, 'Position', [100 100 1000 150])
+set(hFig1, 'Position', [100 100 1800 250])
 for i_cell =1:6
     plot(PSTH_mask(:,i_cell))%,PSTH_LES(:,i), '.');
     hold on
@@ -75,12 +73,12 @@ for i_cell =1:6
     i = 1;
     time = interval_time(1);
     while time < 15
-        plot([120*time 120*time], [0 100], 'Color', default_colors(3,:))
+        plot([120*time 120*time], [0 200], 'Color', default_colors(3,:))
         i = i+1;
         time = interval_time(i);
     end
     hold off
-    axis equal
+    ylim([0 200])
     title([type{i_cell} num2str(cells(i_cell))])
     pause()
 end
@@ -92,28 +90,35 @@ for i_cell = 1:6
     time = 0;
     while time < 15
         try
-            mask_spike_count(i) = sum(PSTH_mask(frame_changes(i):frame_changes(i+1), i_cell));
-            LES_spike_count(i) = sum(PSTH_LES(frame_changes(i):frame_changes(i+1), i_cell));
+            mask_spike_count(i) = max(PSTH_mask(frame_changes(i):frame_changes(i+1), i_cell));
+            % mask_spike_count_error(i) = std(PSTH_mask(frame_changes(i):frame_changes(i+1), i_cell));
+            LES_spike_count(i) = max(PSTH_LES(frame_changes(i):frame_changes(i+1), i_cell));
+            % LES_spike_count_error(i) = std(PSTH_LES(frame_changes(i):frame_changes(i+1), i_cell));
         catch
-            mask_spike_count(i) = sum(PSTH_mask(frame_changes(i):end, i_cell));
-            LES_spike_count(i) = sum(PSTH_LES(frame_changes(i):end, i_cell));
+            mask_spike_count(i) = max(PSTH_mask(frame_changes(i):end, i_cell));
+            % mask_spike_count_error(i) = std(PSTH_mask(frame_changes(i):end, i_cell));
+            LES_spike_count(i) = max(PSTH_LES(frame_changes(i):end, i_cell));
+            % LES_spike_count_error(i) = std(PSTH_LES(frame_changes(i):end, i_cell));
         end
         i = i+1;
         time = frame_changes(i)/120;
-    end
+    end 
     switch type{i_cell}
         case 'on'
-            on = [on; mask_spike_count', LES_spike_count'];
+            on = [on; mask_spike_count', LES_spike_count'];%, mask_spike_count_error', LES_spike_count_error'];
         case 'off'
-            off = [off; mask_spike_count', LES_spike_count'];
+            off = [off; mask_spike_count', LES_spike_count'];%, mask_spike_count_error', LES_spike_count_error'];
         case 'large'
-            large = [large; mask_spike_count', LES_spike_count'];  
+            large = [large; mask_spike_count', LES_spike_count'];%, mask_spike_count_error', LES_spike_count_error'];  
     end
-%     plot(mask_spike_count, LES_spike_count,'+')
+    %errorbarxy(mask_spike_count, LES_spike_count, mask_spike_count_error/sqrt(40),LES_spike_count_error/sqrt(40))
+%     plot(mask_spike_count, LES_spike_count, '.');
 %     hold on
-%     plot([0 500], [0 500])
+%     plot([0 1200], [0 1200])
 %     hold off
 %     axis equal
+%     xlim([0 1200])
+%     ylim([0 1200])
 %     title([type{i_cell} num2str(cells(i_cell))])
 %     xlabel('Original Stimulus');
 %     ylabel('Linear Equivalent Stimulus');
