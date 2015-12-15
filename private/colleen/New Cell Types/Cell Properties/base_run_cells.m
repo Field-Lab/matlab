4cbbeff09edae1171e0406b0faf36ea35a506486
Colleen Rhoades <rhoades@stanford.edu>
2015-12-15 09:40:39 -0800

    read piece from xlsx file and process features, plot PCA results

--- private/colleen/New Cell Types/Cell Properties/base_run_cells.m
+++ private/colleen/New Cell Types/Cell Properties/base_run_cells.m
@@ -1,38 +1,30 @@
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-clear
-close all
-clc
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% INPUTS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% clear
+% close all
+% clc
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% INPUTS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%[~, txt] = xlsread('/Users/colleen/Documents/Large Cell Data.xlsx', 'All large cell pieces');
-txt = {'2005-01-21-3' 'data'	'000';
-'2007-01-23-5' 	'data'	'001';
-'2007-05-01-2' 	'data'	'000';
-'2007-08-21-4' 	'data'	'000';
-'2009-04-13-1' 	'data'	'005';
-'2008-04-08-2' 	'data'	'003';
-'2005-05-24-1' 'data' '000';
-'2005-07-26-0' 'data' '000';
-'2005-04-26-2' 'data' '002';
-
-};
-for j= 7:size(txt,1)
+[~, txt] = xlsread('/Volumes/Lab/Users/crhoades/Test Large Cell Data.xlsx');
+% txt = {'
+% };
+for j= 2:size(txt,1)-1
     % piece = txt(j,1:3);
     run_opts.date=strtrim(txt{j,1}); % one slash at the end
     temp1 = strtrim(txt{j,2});
-   temp2 =  strtrim(txt{j,3});
-    run_opts.concatname=[temp1, temp2]; % Name (or modified name) of run, no slashes
+    temp2 =  strtrim(txt{j,3});
+    run_opts.concatname=temp1; % Name (or modified name) of run, no slashes\
+    run_opts.dataname = temp2;
     
     % Sometimes the data has two versions of the concate name
-    run_opts.file_name = [run_opts.date, '/', run_opts.concatname, '/',  run_opts.concatname];
+    run_opts.file_name = [run_opts.date, '/', run_opts.concatname, '/',run_opts.dataname, '/',run_opts.dataname];
     % run_opts.file_name = [run_opts.date, '/', run_opts.concatname, '/',  run_opts.concatname];
     
     
     run_opts.save_location_root = '/Volumes/Lab/Users/crhoades/Cell Properties/';
     % Where to save
-    run_opts.filepath= [run_opts.save_location_root, run_opts.date, '/', run_opts.concatname, ];
+    run_opts.filepath= [run_opts.save_location_root, run_opts.date, '/', run_opts.concatname, '/', run_opts.dataname];
     
     % run_opts.false_stixels = 0.5; % increase if stixels are size 32
     run_opts.num_frames = 30;
@@ -49,16 +41,21 @@ for j= 7:size(txt,1)
         
         run_opts.refresh = datarun.stimulus.interval/120*1000;
         % cell_indices = get_cell_indices(datarun,run_opts.cell_specification);
-        
-        for iter = 1:size(datarun.cell_types,2)
+        pca_data = [];
+        parfor iter = 1:size(datarun.cell_types,2)
+            clear probabilities;
             run_opts.cell_specification = datarun.cell_types{iter}.name;
             run_opts.save_folder = [run_opts.date, '/', run_opts.concatname, '/', run_opts.cell_specification,  '/'];
             
-            if ~exist([run_opts.save_location_root, run_opts.save_folder])
-                mkdir([run_opts.save_location_root, run_opts.save_folder]);
+            if ~exist([run_opts.filepath,'/', run_opts.cell_specification]);
+
+                mkdir([run_opts.filepath,'/', run_opts.cell_specification]);
             end
             
             cell_ids = get_cell_ids(datarun, run_opts.cell_specification);
+            cell_indices = get_cell_indices(datarun, run_opts.cell_specification);
+
+ 
             rf = zeros(length(cell_ids),1);
             t_zc= zeros(length(cell_ids),1);
             t_p = zeros(length(cell_ids),1);
@@ -66,11 +63,39 @@ for j= 7:size(txt,1)
             bi_ind = zeros(length(cell_ids),1);
             fr = zeros(length(cell_ids),1);
             amp = zeros(length(cell_ids),1);
+            acf_mean=zeros(length(cell_ids),1);
+            acf_var=zeros(length(cell_ids),1);
+            acf_sd=zeros(length(cell_ids),1);
+            acf_end_val=zeros(length(cell_ids),1);
+            acf_peak=zeros(length(cell_ids),1);
+            acf_peak_time=zeros(length(cell_ids),1);
+            acf_rise=zeros(length(cell_ids),1);
+            norm= cell(length(cell_ids),1);
+            cell_counter  = 0;
             for i = 1:length(cell_ids)
-                [rf(i), t_zc(i), t_p(i), t_t(i), bi_ind(i), fr(i), amp(i)] = get_timecourse_prop(datarun, cell_ids(i), run_opts);
+                try
+                    [rf(i), t_zc(i), t_p(i), t_t(i), bi_ind(i), fr(i), amp(i)] = get_timecourse_prop(datarun, cell_ids(i), run_opts);
+                    [probabilities{i}, bins{i}, norm{i}] = autocorrelation(datarun.spikes{cell_indices(i)},0.001, 0.1, datarun.duration);
+                    acf_mean(i) = norm{i}.mean;
+                    acf_var(i) = norm{i}.var;
+                    acf_sd(i) = norm{i}.sd;
+                    acf_end_val(i) = norm{i}.end_value;
+                    acf_peak(i) = norm{i}.peak;
+                    acf_peak_time(i) = norm{i}.peak_time;
+                    acf_rise(i) = norm{i}.rise;
+                    cell_counter = cell_counter +1;
+
+
+                catch
+                    disp([num2str(iter) '/' num2str(i)])
+                end
             end
+            count(iter) = cell_counter;
+            test = cell2mat(probabilities);
+            test2 = reshape(test, length(bins{1}), length(test)/length(bins{1}));
             
-                     output.date = run_opts.date;
+            pca_data = [pca_data, test2];
+            output.date = run_opts.date;
             output.concatname = run_opts.concatname;
             % if sum(strcmp(run_opts.cell_specification, {'ON parasol', 'ON midget', 'OFF midget', 'OFF parasol', 'OFF large 1','OFF large 2', 'OFF large 3', 'OFF large 4' ,'ON large 1', 'ON large 2', 'ON large 3', 'ON large 4'})) ~=1;
             %     if (strfind('ON', run_opts.cell_specification{1}) | strfind('on', run_opts.cell_specification{1}) | strfind('On', run_opts.cell_specification{1})) & (strfind('parasol', run_opts.cell_specification{1}) | strfind('Parasol', run_opts.cell_specification{1}))
@@ -85,13 +110,31 @@ for j= 7:size(txt,1)
             output.parameters.t_t = t_t;
             output.parameters.bi_ind = bi_ind;
             output.parameters.fr = fr;
-            
-                        output.parameters.amp = amp;
+            output.parameters.amp = amp;
+            output.parameters.acf_mean =acf_mean;
+            output.parameters.acf_var =acf_var;
+            output.parameters.acf_sd =acf_sd;
+            output.parameters.acf_end_val =acf_end_val;
+            output.parameters.acf_peak =acf_peak;
+            output.parameters.acf_peak_time =acf_peak_time;
+            output.parameters.acf_rise =acf_rise;
             % end
-            save([run_opts.save_location_root, run_opts.save_folder, 'output.mat'], 'output');
+            save([run_opts.filepath,'/', run_opts.cell_specification, '/', 'output.mat'], 'output');
         end
     catch
         disp([run_opts.date, run_opts.concatname]);
     end
+    [COEFF, SCORE, LATENT, TSQUARED, EXPLAINED, MU] = pca(pca_data', 'NumComponents',2);
+    counter = 1;
+    for iter = 1:size(datarun.cell_types,2)
+        load([run_opts.filepath,'/',  datarun.cell_types{iter}.name, '/', 'output.mat']);
+        cell_ids = get_cell_ids(datarun, datarun.cell_types{iter}.name);
+
+        output.parameters.pca1 = SCORE(counter:counter +count(iter) -1,1);
+        output.parameters.pca2 = SCORE(counter:counter +count(iter) -1,2);
+        save([run_opts.filepath,'/', datarun.cell_types{iter}.name, '/', 'output.mat'], 'output');
+        counter = counter + count(iter);
+    end
+    
     clear datarun
 end
--- private/colleen/New Cell Types/Cell Properties/cell_type_classifier.m
+++ private/colleen/New Cell Types/Cell Properties/cell_type_classifier.m
@@ -1,22 +1,62 @@
 % clustering cell types
 clear
+close all
 %2007-05-01-2 is dark
 %2007-08-21-4 has bad OFF large
 %2009-04-13-1 has super large OFF 1
 %2008-04-08-2 had ON large 2 that don't cross 0
-date = {'2007-01-23-5';'2005-01-21-3';'2007-08-21-4';'2009-04-13-1';'2008-04-08-2';'2005-05-24-1';'2005-07-26-0';'2005-04-26-2'}%'2007-05-01-2'};
- 
- 
+%    '2005-04-14-0'     'data002';; outlier
+   % '2008-04-22-5';    'data003'; outlier
+   
+     %  '2009-04-13-7';     'data000'; outlier
+%          '2008-08-27-6';    'data009';
 
-concate = {'data001';'data000';'data000';'data005'; 'data003';'data000'; 'data000';'data002'}%'data000'};
+   % '2007-08-21-4';     'data000'; only 1 type
+
+%     '2008-04-08-2';     'data003'; only 1 type
+
+%     '2009-12-03-2';     'data000';
+%     '2010-11-22-4';     'data000-nwpca';
+
+%     '2013-05-28-9';      'data000';
+
+[~, txt] = xlsread('/Users/colleen/Documents/Test Large Cell Data.xlsx');
     data= [];
+
+for t= 1:11%size(txt,1)
+    % piece = txt(j,1:3);
+    run_opts.date{t}=strtrim(txt{t,1}); % one slash at the end
+    temp1 = strtrim(txt{t,2});
+    temp2 =  strtrim(txt{t,3});
+    run_opts.concatname=temp1; % Name (or modified name) of run, no slashes\
+    run_opts.dataname{t} = temp2;
+    
+    % Sometimes the data has two versions of the concate name
+    run_opts.file_name{t} = [run_opts.date{t}, '/', run_opts.concatname, '/',  run_opts.dataname{t}];
+    % run_opts.file_name = [run_opts.date, '/', run_opts.concatname, '/',  run_opts.concatname];
+    
+    
+    run_opts.save_location_root = '/Volumes/Lab/Users/crhoades/Cell Properties/';
+    % Where to save
+    run_opts.filepath{t}= [run_opts.save_location_root, run_opts.date{t}, '/', run_opts.concatname, '/', run_opts.dataname{t}, '/'];
+    
+
+
+% 
+% date = {
+%     };
+% 
+% concate = {
+%     };
 episolon = 0.0001;
-for t = 1:size(date,1)
+% for t = 1:size(date,1)-4
     clear match
-    filename = ['/Volumes/Lab/Users/crhoades/Cell Properties/', date{t}, '/', concate{t}];
+    filename = ['/Volumes/Lab/Users/crhoades/Cell Properties/', run_opts.file_name{t}];
     classes= {'OFF Parasol', 'ON Parasol', 'ON Midget', 'OFF Midget', 'ON Large 1', 'ON Large 2', 'ON Large 3', 'ON Large 4', 'OFF Large 1', 'OFF Large 2', 'OFF Large 3', 'OFF Large 4'};
     classes= {'OFF Parasol', 'ON Parasol',  'ON Large 1', 'ON Large 2', 'ON Large 3', 'ON Large 4', 'OFF Large 1', 'OFF Large 2', 'OFF Large 3', 'OFF Large 4'};
-    classes = {'ON Parasol', 'ON large 1', 'ON large 2', 'OFF Parasol', 'OFF large 1', 'OFF large 2'};
+    classes = {'ON Parasol', 'ON large 1', 'ON large 2','OFF Parasol', 'OFF large 1', 'OFF large 2', 'ON LBC', 'ON Large 3', 'ON Large 4'};
+    classes = {'ON Parasol', 'ON large 1', 'ON large 2','OFF Parasol', 'OFF large 1', 'OFF large 2', 'ON large 3', 'ON large 4', 'ON LBC'};
+
     D = dir(filename);
     colors = [];
     col = hsv(size(classes,2));
@@ -30,39 +70,44 @@ for t = 1:size(date,1)
             file_path{c} = [filename, '/', D(match(c,:)).name, '/', 'output.mat'];
             output = load(file_path{c});
             new_data = [c*ones(size(cell2mat(struct2cell(output.output.parameters)'),1),1), cell2mat(struct2cell(output.output.parameters)')];
-            new_data = [new_data(:,1:4), new_data(:,6:end)]; % remove t_t
+            new_data = [new_data(:,1:end-3)]; % remove acf
             if c == 1
                 ref_on_parasol = new_data;
-                    new_data(:,end) = 1;
+                    new_data(:,7) = 1;
+                new_data = [repmat(t, size(new_data,1),1), new_data];
 
                 data = [data; new_data];
             elseif c == 4
                 ref_off_parasol = new_data;
-                    new_data(:,end) = -1;
+                    new_data(:,7) = -1;
+                new_data = [repmat(t, size(new_data,1),1), new_data];
+
                 data = [data; new_data];
             end
             
-            if c == 2 || c == 3
+            if c == 2 || c == 3 ||c ==7 ||c ==8 ||c ==9
                 real_ind = boolean(floor(sum(~isnan(ref_on_parasol),2)/size(ref_on_parasol,2)));
                 new_data(:,2:end) = new_data(:,2:end)./repmat(mean(ref_on_parasol(real_ind, 2:end)), size(new_data,1),1);
-                mean_amp = mean(ref_on_parasol(real_ind, end));
+                mean_amp = mean(ref_on_parasol(real_ind, 8));
                 if mean_amp < 0 
-                    new_data(:,end) = -1;
+                    new_data(:,7) = -1;
                 else
-                    new_data(:,end) = 1;
+                    new_data(:,7) = 1;
                 end
-                
+                new_data = [repmat(t, size(new_data,1),1), new_data];
+
                 data = [data; new_data];
 
             elseif c == 5 ||c ==6
                 real_ind = boolean(floor(sum(~isnan(ref_off_parasol),2)/size(ref_off_parasol,2)));
                 new_data(:,2:end) = new_data(:,2:end)./repmat(mean(ref_off_parasol(real_ind, 2:end)), size(new_data,1),1);
-                 mean_amp = mean(ref_off_parasol(real_ind, end));
+                 mean_amp = mean(ref_off_parasol(real_ind, 8));
                 if mean_amp < 0 
-                   new_data(:,end) = -1;
+                   new_data(:,7) = -1;
                 else
-                    new_data(:,end) = 1;
+                    new_data(:,7) = 1;
                 end
+                new_data = [repmat(t, size(new_data,1),1), new_data];
                 data = [data; new_data];
 
             end
@@ -76,72 +121,108 @@ for t = 1:size(date,1)
         end
 
     end
+   
 end
 
 
-data_large = data(data(:,1) == 2 | data(:,1) == 3 | data(:,1) == 5 | data(:,1) == 6, :);
+data_large = data(data(:,2) == 2 | data(:,2) == 3 | data(:,2) == 5 | data(:,2) == 6 | data(:,2) == 7| data(:,2) == 8 | data(:,2) == 9, :);
 data_rows = boolean(floor(sum(~isnan(data_large),2)/size(data_large,2)));
-norm_data = data_large(:,2:end)./(repmat(std(data_large(data_rows,2:end)), size(data_large,1),1)+episolon);
-norm_data = [data_large(:,1), norm_data];
+% norm_data = data_large(:,3:end)./(repmat(std(data_large(data_rows,3:end)), size(data_large,1),1)+episolon);
+norm_data = data_large;%[data_large(:,1:2), norm_data];
 
 
-[COEFF, SCORE, LATENT, TSQUARED, EXPLAINED, MU] = pca(norm_data(:,2:end), 'NumComponents',3);
+[COEFF, SCORE, LATENT, TSQUARED, EXPLAINED, MU] = pca(norm_data(:,3:end-2), 'NumComponents',3);
 
 test = SCORE;
 % idx = kmeans(norm_data(:,2:end),length(unique(norm_data(:,1))));
 idx = kmeans(norm_data,length(unique(norm_data(:,1))));
 % idx = kmeans(test,4);
 
-figure;
-% subplot(3,1,1)
-gscatter(test(:,1), test(:,2),norm_data(:,1))
-
-l= findobj(gcf,'tag','legend'); 
-set(l,'location','best');
-set(l, 'String', {classes{unique(norm_data(:,1))}})
+figure
+scatter3(test(:,1), test(:,2),test(:,3));
+title('PC 1,2, and 3')
 
 figure;
 % subplot(3,1,1)
-gscatter(test(:,1), test(:,3),norm_data(:,1))
+h = gscatter(test(:,1), test(:,2),norm_data(:,1:2));
 
+S = unique(norm_data(:,1:2), 'rows');
+unique_dates = unique(norm_data(:,1));
+
+unique_classes = unique(norm_data(:,2));
+markers = {'+','o','*','.','square', 'hexagram','diamond','^','v','>','<','pentagram','x'};
+colors = hsv(size(unique(S(:,1)),1));
 l= findobj(gcf,'tag','legend'); 
 set(l,'location','best');
-set(l, 'String', {classes{unique(norm_data(:,1))}})
+for n = 1:length(h)
+   set(h(n), 'Marker', markers{unique_classes == S(n,2)})
+      set(h(n), 'MarkerSize', 9);
+
+   set(h(n), 'MarkerFaceColor', colors(unique_dates == S(n,1), :))
+      set(h(n), 'MarkerEdgeColor', colors(unique_dates == S(n,1), :))
+LegendString{n} = [[run_opts.date{S(n,1)}, ' ', run_opts.dataname{S(n,1)}(1:7)] ' , ' classes{S(n,2)}];
+end
+set(l, 'String', LegendString)
+title('PC 1 and 2')
 
 
 figure;
 % subplot(3,1,1)
-gscatter(test(:,2), test(:,3),norm_data(:,1))
+h= gscatter(test(:,1), test(:,3),norm_data(:,1:2));
 
 l= findobj(gcf,'tag','legend'); 
 set(l,'location','best');
-set(l, 'String', {classes{unique(norm_data(:,1))}})
-
-% subplot(3,1,2)
-% gscatter(test(:,1), test(:,3),norm_data(:,1))
-% subplot(3,1,3)
-% gscatter(test(:,2), test(:,3),norm_data(:,1))
-
+for n = 1:length(h)
+   set(h(n), 'Marker', markers{unique_classes == S(n,2)})
+         set(h(n), 'MarkerSize', 9);
+   set(h(n), 'MarkerFaceColor', colors(unique_dates == S(n,1), :))
+      set(h(n), 'MarkerEdgeColor', colors(unique_dates == S(n,1), :))
+LegendString{n} = [run_opts.date{S(n,1)} ' , ' classes{S(n,2)}];
+end
+set(l, 'String', LegendString)
+title('PC 1 and 3')
 figure;
+% subplot(3,1,1)
+h = gscatter(test(:,2), test(:,3),norm_data(:,1:2));
 
-     gscatter(1:size(idx,1)',idx,norm_data(:,1))
-     
-    changes =  find(diff(norm_data(:,1))~=0);
-    hold on
-    for i = 1:length(changes)
-    plot([changes(i), changes(i)], get(gca, 'ylim'), 'k--')
-    end
-    
-set(gca, 'ylim', [0 length(unique(norm_data(:,1)))+1])
-set(gca, 'xlim', [1 size(idx,1)])
 l= findobj(gcf,'tag','legend'); 
-set(l,'location','northwest');
+set(l,'location','best');
+for n = 1:length(h)
+   set(h(n), 'Marker', markers{unique_classes == S(n,2)})
+   set(h(n), 'MarkerFaceColor', colors(unique_dates == S(n,1), :))
+      set(h(n), 'MarkerEdgeColor', colors(unique_dates == S(n,1), :))       
+      set(h(n), 'MarkerSize', 9);
 
+LegendString{n} = [run_opts.date{S(n,1)} ' , ' classes{S(n,2)}];
+end
+set(l, 'String', LegendString)
+title('PC 2 and 3')
+% figure;
+% 
+%      gscatter(1:size(idx,1)',idx,norm_data(:,1))
+%      
+%     changes =  find(diff(norm_data(:,1))~=0);
+%     hold on
+%     for i = 1:length(changes)
+%     plot([changes(i), changes(i)], get(gca, 'ylim'), 'k--')
+%     end
+%     
+% set(gca, 'ylim', [0 length(unique(norm_data(:,1)))+1])
+% set(gca, 'xlim', [1 size(idx,1)])
+% l= findobj(gcf,'tag','legend'); 
+% set(l,'location','northwest');
+% 
 
 figure;
-gscatter(data_large(:,2), data_large(:,5),data_large(:,1))
+gscatter(data_large(:,3), data_large(:,8),data_large(:,2))
 xlabel('RF Size (relative to parasols')
 ylabel('Biphasic index (relative to parasols')
 set(l,'location','best');
-set(l, 'String', {classes{unique(norm_data(:,1))}})
+% % % set(l, 'String', {classes{unique(norm_data(:,1))}})
+data_large(data_large(:,2) == 2, 2) = 1;
+data_large(data_large(:,2) == 3, 2) = 2;
+data_large(data_large(:,2) == 5, 2) = 3;
+data_large(data_large(:,2) == 6, 2) = 4;
+data_to_save = [data_large(:,1:2) test];
+csvwrite(['/Volumes/Lab/Users/crhoades/Colleen/matlab/private/colleen/New Cell Types/Cell Properties/', 'othertypestest'], data_to_save);
 
