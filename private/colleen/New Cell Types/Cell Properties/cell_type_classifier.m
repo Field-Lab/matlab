542b7df2c05901c5ff21da254161afef68424bee
Colleen Rhoades <rhoades@stanford.edu>
2015-12-18 08:48:14 -0800

    midget normalization

--- private/colleen/New Cell Types/Cell Properties/cell_type_classifier.m
+++ private/colleen/New Cell Types/Cell Properties/cell_type_classifier.m
@@ -6,12 +6,12 @@ close all
 %2009-04-13-1 has super large OFF 1
 %2008-04-08-2 had ON large 2 that don't cross 0
 %    '2005-04-14-0'     'data002';; outlier
-   % '2008-04-22-5';    'data003'; outlier
-   
-     %  '2009-04-13-7';     'data000'; outlier
+% '2008-04-22-5';    'data003'; outlier
+
+%  '2009-04-13-7';     'data000'; outlier
 %          '2008-08-27-6';    'data009';
 
-   % '2007-08-21-4';     'data000'; only 1 type
+% '2007-08-21-4';     'data000'; only 1 type
 
 %     '2008-04-08-2';     'data003'; only 1 type
 
@@ -20,10 +20,10 @@ close all
 
 %     '2013-05-28-9';      'data000';
 
-[~, txt] = xlsread('/Users/colleen/Documents/Test Large Cell Data.xlsx');
-    data= [];
+[~, txt] = xlsread('/Users/colleen/Documents/Large Cell Data ARVO.xlsx');
+data= [];
 
-for t= 1:11%size(txt,1)
+for t= 1:size(txt,1)
     % piece = txt(j,1:3);
     run_opts.date{t}=strtrim(txt{t,1}); % one slash at the end
     temp1 = strtrim(txt{t,2});
@@ -40,23 +40,23 @@ for t= 1:11%size(txt,1)
     % Where to save
     run_opts.filepath{t}= [run_opts.save_location_root, run_opts.date{t}, '/', run_opts.concatname, '/', run_opts.dataname{t}, '/'];
     
-
-
-% 
-% date = {
-%     };
-% 
-% concate = {
-%     };
-episolon = 0.0001;
-% for t = 1:size(date,1)-4
+    
+    
+    %
+    % date = {
+    %     };
+    %
+    % concate = {
+    %     };
+    episolon = 0.0001;
+    % for t = 1:size(date,1)-4
     clear match
     filename = ['/Volumes/Lab/Users/crhoades/Cell Properties/', run_opts.file_name{t}];
     classes= {'OFF Parasol', 'ON Parasol', 'ON Midget', 'OFF Midget', 'ON Large 1', 'ON Large 2', 'ON Large 3', 'ON Large 4', 'OFF Large 1', 'OFF Large 2', 'OFF Large 3', 'OFF Large 4'};
     classes= {'OFF Parasol', 'ON Parasol',  'ON Large 1', 'ON Large 2', 'ON Large 3', 'ON Large 4', 'OFF Large 1', 'OFF Large 2', 'OFF Large 3', 'OFF Large 4'};
-%     classes = {'ON Parasol', 'ON large 1', 'ON large 2'};
-    classes = {'ON Parasol', 'ON large 1', 'ON large 2', 'OFF parasol', 'OF', 'OF', 'ON Large 3', 'ON large 4', 'ON LBC'};
-
+    %     classes = {'ON Parasol', 'ON large 1', 'ON large 2'};
+    classes = {'ON Parasol', 'ON midget', 'ON large 1', 'ON large 2',  'OFF parasol', 'OFF Large 1', 'OFF Large 2', 'ON LBC'};
+    
     D = dir(filename);
     colors = [];
     col = hsv(size(classes,2));
@@ -70,7 +70,7 @@ episolon = 0.0001;
             file_path{c} = [filename, '/', D(match(c,:)).name, '/', 'output.mat'];
             output = load(file_path{c});
             try
-            new_data = [c*ones(size(cell2mat(struct2cell(output.output.parameters)'),1),1), cell2mat(struct2cell(output.output.parameters)')];
+                new_data = [c*ones(size(cell2mat(struct2cell(output.output.parameters)'),1),1), cell2mat(struct2cell(output.output.parameters)')];
             catch
                 output.output.parameters.pca1 = nan(length(output.output.parameters.fr));
                 output.output.parameters.pca2 = nan(length(output.output.parameters.fr));
@@ -79,60 +79,84 @@ episolon = 0.0001;
             new_data = [new_data]; % remove acf
             if c == 1
                 ref_on_parasol = new_data;
-                    new_data(:,8) = 0.25;
+                new_data(:,8) = 0.25;
                 new_data = [repmat(t, size(new_data,1),1), new_data];
-
+                
                 data = [data; new_data(:, 1:9)];
-            elseif c == 4
+            elseif c == 5
                 ref_off_parasol = new_data;
-                    new_data(:,8) = -0.25;
+                new_data(:,8) = -0.25;
                 new_data = [repmat(t, size(new_data,1),1), new_data];
-
+                
                 data = [data; new_data(:, 1:9)];
+                
+            elseif c == 2
+                ref_on_midget = new_data;
+                new_data(:,8) = 0.25;
+                new_data = [repmat(t, size(new_data,1),1), new_data];
+                
+                data = [data; new_data(:, 1:9)];
+                real_ind_on_parasol = boolean(floor(sum(~isnan(ref_on_parasol),2)/size(ref_on_parasol,2)));
+                real_ind_on_midget = boolean(floor(sum(~isnan(ref_on_midget),2)/size(ref_on_midget,2)));
+                midget_parasol_ratio = median(ref_on_parasol(real_ind_on_parasol, [2:7 8:10]))./median(ref_on_midget(real_ind_on_midget, [2:7 8:10]));
+
             end
             
-            if c == 2 || c == 3  || c == 7 || c == 8 || c == 9
+            if c == 3 || c == 8
                 real_ind = boolean(floor(sum(~isnan(ref_on_parasol),2)/size(ref_on_parasol,2)));
-                 new_data(:,[2:7 8:10]) = new_data(:,[2:7 8:10])./repmat(median(ref_on_parasol(real_ind, [2:7 8:10])), size(new_data,1),1);
+                new_data(:,[2:7 8:10]) = new_data(:,[2:7 8:10])./repmat(median(ref_on_parasol(real_ind, [2:7 8:10])), size(new_data,1),1);
                 mean_amp = median(ref_on_parasol(real_ind, 8));
-                if mean_amp < 0 
+                if mean_amp < 0
                     new_data(:,8) = -0.25;
                 else
                     new_data(:,8) =0.25;
                 end
                 new_data = [repmat(t, size(new_data,1),1), new_data];
-
+                
                 data = [data; new_data(:, 1:9)];
-
-            elseif c == 5|| c == 6 
+            elseif c == 4
+                real_ind = boolean(floor(sum(~isnan(ref_on_midget),2)/size(ref_on_midget,2)));
+                new_data(:,[2:7 8:10]) = new_data(:,[2:7 8:10])./repmat(median(ref_on_midget(real_ind, [2:7 8:10])), size(new_data,1),1).*repmat(midget_parasol_ratio, size(new_data,1),1);
+                mean_amp = median(ref_on_midget(real_ind, 8));
+                if mean_amp < 0
+                    new_data(:,8) = -0.25;
+                else
+                    new_data(:,8) =0.25;
+                end
+                new_data = [repmat(t, size(new_data,1),1), new_data];
+                
+                data = [data; new_data(:, 1:9)];
+                
+                
+            elseif c == 6|| c == 7
                 real_ind = boolean(floor(sum(~isnan(ref_off_parasol),2)/size(ref_off_parasol,2)));
-                 new_data(:,[2:7 8:10]) = new_data(:,[2:7 8:10])./repmat(median(ref_off_parasol(real_ind,[2:7 8:10])), size(new_data,1),1);
-                 new_data(:,8) = -1*new_data(:,8);
-                 mean_amp = median(ref_off_parasol(real_ind, 8));
-                if mean_amp < 0 
-                   new_data(:,8) = -0.25;
+                new_data(:,[2:7 8:10]) = new_data(:,[2:7 8:10])./repmat(median(ref_off_parasol(real_ind,[2:7 8:10])), size(new_data,1),1);
+                new_data(:,8) = -1*new_data(:,8);
+                mean_amp = median(ref_off_parasol(real_ind, 8));
+                if mean_amp < 0
+                    new_data(:,8) = -0.25;
                 else
                     new_data(:,8) = 0.25;
                 end
                 new_data = [repmat(t, size(new_data,1),1), new_data];
                 data = [data; new_data(:, 1:9)];
-
+                
             end
             
             %         new_data_unit = new_data/std(new_data);
-%             data = [data; new_data];
-    %         colors = [colors ;repmat(col(counter, :), size(cell2mat(struct2cell(output.output.parameters)'),1),1)];
-
+            %             data = [data; new_data];
+            %         colors = [colors ;repmat(col(counter, :), size(cell2mat(struct2cell(output.output.parameters)'),1),1)];
+            
             %         colors = [colors ;repmat(col(counter, :), size(cell2mat(struct2cell(output.output.parameters)'),1),1)];
             counter = counter+1;
         end
-
+        
     end
-   
+    
 end
 
 
-data_large = data(data(:,2) == 2 | data(:,2) == 3 | data(:,2) == 5 | data(:,2) == 6 | data(:,2) == 7| data(:,2) == 8 | data(:,2) == 9, :);
+data_large = data(data(:,2) == 3 | data(:,2) == 4 | data(:,2) == 6 | data(:,2) == 7| data(:,2) == 8 | data(:,2) == 9, :);
 data_rows = boolean(floor(sum(~isnan(data_large),2)/size(data_large,2)));
 
 data_large(:,3:end) = data_large(:,3:end)-repmat(mean(data_large(data_rows,3:end)), size(data_large,1),1);
@@ -146,15 +170,15 @@ norm_data = data_large;%[data_large(:,1:2), norm_data];
 
 % data_large(data_large(:,2) ~= 2,2) = 1;
 % b= glmfit(data_large(:,3:end), data_large(:,2)-1, 'binomial', 'link', 'logit')
-% 
+%
 % data_large = norm_data;%[data_large(:,1:2), norm_data];
 % data_large(data_large(:,2) ~= 3,2) = 2;
 % b= glmfit(data_large(:,3:end), data_large(:,2)-2, 'binomial', 'link', 'logit')
-% 
+%
 % data_large = norm_data;%[data_large(:,1:2), norm_data];
 % data_large(data_large(:,2) ~= 5,2) = 4;
 % b= glmfit(data_large(:,3:end), data_large(:,2)-4, 'binomial', 'link', 'logit')
-% 
+%
 % data_large = norm_data;%[data_large(:,1:2), norm_data];
 % data_large(data_large(:,2) ~= 6,2) = 5;
 % b= glmfit(data_large(:,3:end), data_large(:,2)-5, 'binomial', 'link', 'logit')
@@ -176,8 +200,8 @@ norm_data = data_large;%[data_large(:,1:2), norm_data];
 % 0.461748422
 % 0.263583893
 % 1];
-% 
-% 
+%
+%
 %  norm_data(:,3:end) = norm_data(:,3:end).*repmat(weights', size(norm_data,1),1);
 
 [COEFF, SCORE, LATENT, TSQUARED, EXPLAINED, MU] = pca(norm_data(:,3:end), 'NumComponents',3);
@@ -214,14 +238,14 @@ markers = {'+','o','*' 'hexagram','diamond','^','v','>','<','pentagram','x','+',
 % colors = hsv(size(unique(S(:,1)),1));
 colors = hsv(length(unique_classes));
 
-l= findobj(gcf,'tag','legend'); 
+l= findobj(gcf,'tag','legend');
 for n = 1:length(h)
-   set(h(n), 'Marker', markers{unique_dates == S(n,1)})
-      set(h(n), 'MarkerSize', 9);
-
-   set(h(n), 'MarkerFaceColor', colors(unique_classes == S(n,2), :))
-      set(h(n), 'MarkerEdgeColor', colors(unique_classes == S(n,2), :))
-LegendString{n} = [[run_opts.date{S(n,1)}, ' ', run_opts.dataname{S(n,1)}(1:7)] ' , ' classes{S(n,2)}];
+    set(h(n), 'Marker', markers{unique_dates == S(n,1)})
+    set(h(n), 'MarkerSize', 9);
+    
+    set(h(n), 'MarkerFaceColor', colors(unique_classes == S(n,2), :))
+    set(h(n), 'MarkerEdgeColor', colors(unique_classes == S(n,2), :))
+    LegendString{n} = [[run_opts.date{S(n,1)}, ' ', run_opts.dataname{S(n,1)}(1:7)] ' , ' classes{S(n,2)}];
 end
 set(l, 'String', LegendString)
 title('PC 1 and 2')
@@ -238,13 +262,13 @@ set(gcf, 'Position', [10 10 1120 840])
 % subplot(3,1,1)
 h= gscatter(test(:,1), test(:,3),norm_data(:,1:2));
 
-l= findobj(gcf,'tag','legend'); 
+l= findobj(gcf,'tag','legend');
 for n = 1:length(h)
-   set(h(n), 'Marker', markers{unique_dates == S(n,1)})
-         set(h(n), 'MarkerSize', 9);
-   set(h(n), 'MarkerFaceColor', colors(unique_classes == S(n,2), :))
-      set(h(n), 'MarkerEdgeColor', colors(unique_classes == S(n,2), :))
-LegendString{n} = [run_opts.date{S(n,1)} ' , ' classes{S(n,2)}];
+    set(h(n), 'Marker', markers{unique_dates == S(n,1)})
+    set(h(n), 'MarkerSize', 9);
+    set(h(n), 'MarkerFaceColor', colors(unique_classes == S(n,2), :))
+    set(h(n), 'MarkerEdgeColor', colors(unique_classes == S(n,2), :))
+    LegendString{n} = [run_opts.date{S(n,1)} ' , ' classes{S(n,2)}];
 end
 set(l, 'String', LegendString)
 title('PC 1 and 3')
@@ -263,14 +287,14 @@ set(gcf, 'Position', [10 10 1120 840])
 % subplot(3,1,1)
 h = gscatter(test(:,2), test(:,3),norm_data(:,1:2));
 
-l= findobj(gcf,'tag','legend'); 
+l= findobj(gcf,'tag','legend');
 for n = 1:length(h)
-   set(h(n), 'Marker', markers{unique_dates == S(n,1)})
-   set(h(n), 'MarkerFaceColor', colors(unique_classes == S(n,2), :))
-      set(h(n), 'MarkerEdgeColor', colors(unique_classes == S(n,2), :))       
-      set(h(n), 'MarkerSize', 9);
-
-LegendString{n} = [run_opts.date{S(n,1)} ' , ' classes{S(n,2)}];
+    set(h(n), 'Marker', markers{unique_dates == S(n,1)})
+    set(h(n), 'MarkerFaceColor', colors(unique_classes == S(n,2), :))
+    set(h(n), 'MarkerEdgeColor', colors(unique_classes == S(n,2), :))
+    set(h(n), 'MarkerSize', 9);
+    
+    LegendString{n} = [run_opts.date{S(n,1)} ' , ' classes{S(n,2)}];
 end
 set(l, 'String', LegendString)
 title('PC 2 and 3')
@@ -282,26 +306,26 @@ set(gca, 'xlim', [xaxis(1), xaxis(2)+2])
 set(l,'location','southeast');
 
 % figure;
-% 
+%
 %      gscatter(1:size(idx,1)',idx,norm_data(:,1))
-%      
+%
 %     changes =  find(diff(norm_data(:,1))~=0);
 %     hold on
 %     for i = 1:length(changes)
 %     plot([changes(i), changes(i)], get(gca, 'ylim'), 'k--')
 %     end
-%     
+%
 % set(gca, 'ylim', [0 length(unique(norm_data(:,1)))+1])
 % set(gca, 'xlim', [1 size(idx,1)])
-% l= findobj(gcf,'tag','legend'); 
+% l= findobj(gcf,'tag','legend');
 % set(l,'location','northwest');
-% 
-% 
+%
+%
 % figure;
 % set(gcf, 'Position', [10 10 1120 840])
 % data_rows = boolean(floor(sum(~isnan(data_large),2)/size(data_large,2)));
 % data_large(data_large(:,2) > 2, 2) = 3;
-% 
+%
 % gscatter(data_large(data_rows,3), data_large(data_rows,4),data_large(data_rows,2))
 % xlabel('RF Size (relative to parasols')
 % ylabel('zc (relative to parasols')
