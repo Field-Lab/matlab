e0404bad293dd438071467294afddcbf20e6f7a2
Colleen Rhoades <rhoades@stanford.edu>
2016-02-16 14:44:20 -0800

    GIT WHY DID YOU DELETE ME??

---
+++ private/colleen/New Cell Types/Cell Properties/ei_properties.m
@@ -0,0 +1,66 @@
+function [maximum, minimum, minimum_ind,  maximum_ind, zc, bi_ind, lobe1, lobe2, ratio_to_neighbors]= ei_properties(datarun, cell, array_size)
+
+% % cell_list = [63 337 482 586 633 785];
+% for i = 1:length(cell_list)
+%     cell= cell_list(i);
+if array_size == 512
+datarun = load_ei(datarun, cell, 'array_type', 512);
+    [xc,yc] = getElectrodeCoords512(); 
+
+else
+    datarun = load_ei(datarun, cell, 'array_type', 519);
+    [xc,yc] = getElectrodeCoords519(); 
+
+end
+
+cell_id = get_cell_indices(datarun, cell);
+
+waveforms = datarun.ei.eis{cell_id};
+[~,ind] = max(waveforms(:));
+[max_electrode,~] = ind2sub(size(waveforms),ind);
+
+% positions = datarun.ei.position;
+
+
+difference = repmat([xc(max_electrode), yc(max_electrode)], size(yc,2),1) - [xc', yc'];
+distance = [[1:size(difference,1)]', sqrt(difference(:,1).^2 + difference(:,2).^2)];
+sorted_distance= sortrows(distance, 2);
+neighbor_ids = sorted_distance(2:7,1);
+
+
+sel_wfs = waveforms(neighbor_ids, :);
+avg_height_neigh = mean(max(abs(sel_wfs')));
+% figure; plot(sel_wfs')
+% hold on
+% plot(waveforms(max_electrode,:), 'linewidth', 2)
+
+
+max_waveform = waveforms(max_electrode, :);
+
+    [max_ , max_ind] = max(max_waveform);
+    [min_ , min_ind] = min(max_waveform);
+    if max_ind < min_ind
+        maximum = min_;
+        minimum = max_;
+        minimum_ind = max_ind;
+        maximum_ind = min_ind;
+    else
+        maximum = max_;
+        minimum = min_;
+        maximum_ind = max_ind;
+        minimum_ind = min_ind;
+    end
+    bi_ind = abs(maximum)/abs(minimum);
+    [~,zc] = crossing(max_waveform);
+    index = zc > minimum_ind & zc < maximum_ind;
+    if sum(index) > 0 && sum(index) < 2
+        zc= zc(index);
+        lobe1 = fwhm(1:round(zc)+1, [max_waveform(1:round(zc)), max_waveform(round(zc))*2]);
+        lobe2 = fwhm(round(zc)-1:size(max_waveform,2), [max_waveform(round(zc))*2, max_waveform(round(zc):size(max_waveform,2))]);
+    else
+        zc = nan;
+        lobe1 = nan;
+        lobe2 = nan;
+    end
+    ratio_to_neighbors = maximum/avg_height_neigh;
+% end
