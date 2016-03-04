7c7b4a7835075d42516c3182cf28e023dcdd9316
Alexandra <alexth.nn@gmail.com>
2016-03-03 17:15:16 -0800

    fixed jitter

--- private/alex/voronoi/jitter_2016_02_17_6.m
+++ private/alex/voronoi/jitter_2016_02_17_6.m
@@ -10,10 +10,12 @@ disp('done inputs loading')
 new_inputs = zeros(800,3,107301);
 for j=1:3
     a = squeeze(inputs(:,j,:));
+    a = reshape(a,20,40, duration);
+    a = permute(a, [2 1 3]);
     tmp = a(:);
     adj = 0;
     for i=1:100000
-        tmp(i*800+1+adj:i*800+2+adj) = -1;
+        tmp([i*800+1+adj,i*800+2+adj]) = -1;
         adj=adj+2;
     end
     tmp(tmp==-1) = [];
@@ -43,38 +45,42 @@ end
 
 load('/Volumes/Analysis/2016-02-17-6/jitter/shifts')
 
-% parts of 20 STAs
+% parts of STAs
 disp('starting sta calculation')
 parts = 50;
 for kkk = 1:parts:length(datarun.cell_ids)
     
     spike_array_tmp = spike_array(kkk:kkk+parts-1,:);
 
-    full_inputs = zeros(320+15,640+15,3,sta_params.length);
+    full_inputs = zeros(640+15,320+15,3,sta_params.length);
     for i=1:sta_params.length-1
-        tmp = reshape(inputs(:,:,i), 20,40, 3);
+        tmp = reshape(inputs(:,:,i), 40,20, 3);
         tmp = imresize(tmp,16, 'method', 'nearest');
         jitterX = shifts(1,i);
         jitterY = shifts(2,i);
-        full_inputs(9+jitterX:320+8+jitterX,9+jitterY:640+8+jitterY,:,1+i) = tmp;
+        full_inputs(9+jitterX:640+8+jitterX,9+jitterY:320+8+jitterY,:,1+i) = tmp;
     end
     
-    sta = zeros(320+15,640+15, 3, sta_params.length,parts);
+    sta = zeros(640+15,320+15, 3, sta_params.length,parts);
+    tic
     for i=sta_params.length:90000
-        tmp = reshape(inputs(:,:,i),20,40, 3);
+        tmp = reshape(inputs(:,:,i),40,20, 3);
         tmp = imresize(tmp,16, 'method', 'nearest');
         jitterX = shifts(1,i);
         jitterY = shifts(2,i);
         
         full_inputs = circshift(full_inputs,-1,4);
-        full_inputs(9+jitterX:320+8+jitterX,9+jitterY:640+8+jitterY,:,sta_params.length) = tmp;
+        full_inputs(9+jitterX:640+8+jitterX,9+jitterY:320+8+jitterY,:,sta_params.length) = tmp;
         
 %         figure
 %         imagesc(full_inputs(:,:,2,5))
 %   
         a = find(spike_array_tmp(:,i));
         sta(:,:,:,:,a) =  sta(:,:,:,:,a) + repmat(full_inputs, 1, 1, 1, 1, length(a));
-        if mod(i,10000)==0
+        if mod(i,5000)==0
+            a = toc;
+            disp(['processed ', int2str(i), ' frames in ', num2str(a), ' s'])
+            tic;
             i
         end
         if mod(i,40000)==0
