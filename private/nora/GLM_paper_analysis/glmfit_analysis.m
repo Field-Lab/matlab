9c32aea0a6bc4baeeee78085dcf5d873ce48b030
Nora <nbrackbill@gmail.com>
2016-03-14 21:30:41 -0700

    added BPS metric code

---
+++ private/nora/GLM_paper_analysis/glmfit_analysis.m
@@ -0,0 +1,22 @@
+dsave = '/Volumes/Lab/Users/Nora/GLMFits/';
+piece = '2016-02-17-1/';
+Analysis_Path = '/Volumes/Analysis/2016-02-17-1/data022-data028';
+datarun_class = load_data([Analysis_Path '/data024/data024'], struct('load_neurons', 0, 'load_params', 1));
+
+for cell_type = {'On Parasol', 'Off Parasol'}
+    cells = get_cell_ids(datarun_class, cell_type);
+    n_cells = length(cells);
+    BPS = zeros(2, n_cells);
+    for i_cell = 1:n_cells
+       load([dsave piece num2str(cells(i_cell)) '.mat']);
+       [~, ~, crm_bps, ~] = rastbps_comp_findPS(fittedGLM.xvalperformance.rasters.recorded,fittedGLM.t_bin,fittedGLM.rawfit.ps_basis); 
+       BPS(1,i_cell) = fittedGLM.xvalperformance.logprob_glm_bpspike/crm_bps;
+       load([dsave piece num2str(cells(i_cell)) 'NSEM.mat']);
+       [~, ~, crm_bps, ~] = rastbps_comp_findPS(fittedGLM.xvalperformance.rasters.recorded,fittedGLM.t_bin,fittedGLM.rawfit.ps_basis); 
+       BPS(2,i_cell) = fittedGLM.xvalperformance.logprob_glm_bpspike/crm_bps;
+    end
+    figure; plot(BPS(1,:), BPS(2,:), '.')
+    clear BPS
+end
+
+
