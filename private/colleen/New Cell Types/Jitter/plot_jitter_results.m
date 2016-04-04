c4438c5f07bb2f929dbc1645e65c76eb04ee97b4
Colleen Rhoades <rhoades@stanford.edu>
2016-04-04 14:58:08 -0700

    Compare to non jitter sig stixels

--- private/colleen/New Cell Types/Jitter/plot_jitter_results.m
+++ private/colleen/New Cell Types/Jitter/plot_jitter_results.m
@@ -54,5 +54,34 @@ axis image
 
 
 
+[~,git_hash_string] = system('git rev-parse HEAD');
+fprintf('Git Hash: %s \n', git_hash_string);
 
 
+%% Mosiac from Vision
+dataparam.date='2016-02-17-6';
+dataparam.concatname='data025';
+dataparam.file_name = [dataparam.date, '/', dataparam.concatname,'/', dataparam.concatname];
+% dataparam.save_path = ['/Volumes/Lab/Users/crhoades/JitterMovie/', dataparam.date, '/', dataparam.concatname, '/'];
+select_cells = 1;
+if select_cells == 1
+dataparam.cell_specification = [482 813 1537 2103 2167 3288 3694 3889 4326 4939 6336 6517];
+    
+end
+dataparam.cell_type = {'all'};
+
+% Right Movie
+datarun.names.rrs_neurons_path=['/Volumes/Analysis/', dataparam.file_name, '.neurons'];
+datarun.names.rrs_params_path=['/Volumes/Analysis/', dataparam.file_name, '.params'];
+datarun.names.rrs_sta_path = ['/Volumes/Analysis/', dataparam.file_name, '.sta'];
+
+opt=struct('verbose',1,'load_params',1,'load_neurons',1,'load_obvius_sta_fits',true, 'load_sta', 1, 'load_sta_params', 1, 'load_all',true);
+opt.load_sta_params.save_rf = 1;
+% opt.load_sta_params.frames = 1:fitparam.num_frames;% have to input as a vector list of frames, not the number of frames total, counting backwards
+datarun=load_data(datarun,opt);
+
+cell_ids = get_cell_indices(datarun, dataparam.cell_specification)
+
+for i = 1:length(cell_ids)
+datarun.vision.stas.sta{cell_ids(i)}
+end
