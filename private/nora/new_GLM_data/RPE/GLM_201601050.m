9cdd7ac6bd1d08a92044480d739493d275709b4d
Nora <nbrackbill@gmail.com>
2016-05-20 12:24:37 -0700

    fixed mapping type in GLM_201601050

--- private/nora/new_GLM_data/RPE/GLM_201601050.m
+++ private/nora/new_GLM_data/RPE/GLM_201601050.m
@@ -9,30 +9,31 @@ cell_type_short = 'OnPar_';
 WN_fit = 'mVision/data013/data013';
 WN_test = 'mVision/data014/data014';
 
+%% mapping cells
 cells = get_cell_ids(datarun_class, cell_type{1});
-
-%%
 ref_data = '/Volumes/Analysis/2016-01-05-0/data015-mVision/data015/';
-
-% fitting 
-datarun_fit = load_data([Analysis_Path WN_fit], struct('load_neurons', 1, 'load_params', 1));
 new_data = '/Volumes/Analysis/2016-01-05-0/mVision/data013/';
 EI_map = crossIdentifyNeuronIDs_NB(ref_data, new_data, cells, [] , 0);
 cells_fit = EI_map(:,2);
-fitmovie = get_WN_movie('/Volumes/Lab/Users/Nora/Stimulus/BW_XML/BW-16-1-0.48-11111.xml', 120*1800);
-monitor_refresh = 100/median(diff(datarun_fit.triggers));
-
-%% testing
-datarun_test = load_data([Analysis_Path WN_test], struct('load_neurons', 1, 'load_params', 1));
 new_data = '/Volumes/Analysis/2016-01-05-0/mVision/data014/';
 EI_map = crossIdentifyNeuronIDs_NB(ref_data, new_data, cells, [] , 0);
 cells_test = EI_map(:,2);
+good_cell_index = ~isnan(cells_fit) & ~isnan(cells_test);
+cells = cells(good_cell_index);
+cells_fit = cells_fit(good_cell_index);
+cells_test = cells_test(good_cell_index);
+
+%% loading stim  
+datarun_fit = load_data([Analysis_Path WN_fit], struct('load_neurons', 1, 'load_params', 1));
+fitmovie = get_WN_movie('/Volumes/Lab/Users/Nora/Stimulus/BW_XML/BW-16-1-0.48-11111.xml', 120*1800);
+monitor_refresh = 100/median(diff(datarun_fit.triggers));
+datarun_test = load_data([Analysis_Path WN_test], struct('load_neurons', 1, 'load_params', 1));
 prepped_data = interleaved_data_prep(datarun_test, 1100, 30,'cell_spec', cells_test,'visual_check', 0);
 testmovie = get_WN_movie('/Volumes/Lab/Users/Nora/Stimulus/BW_XML/BW-16-1-0.48-22222.xml', 1100);
 
 %%
 %
-for i=1%:length(cells)
+for i=1:length(cells)
     %if sum(cell2mat(prepped_data.testspikes(:,i)))>0.8*10^4
     fit_idx = get_cell_indices(datarun_fit, cells_fit(i));
     fitspikes = align_spikes_triggers(datarun_fit.spikes{fit_idx}, datarun_fit.triggers, 100, monitor_refresh);
