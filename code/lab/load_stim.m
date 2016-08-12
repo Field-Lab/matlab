2c3ce27f3ecdf70950b78ae2f5a43b52b224ab1a
xyao <xiaoyany@usc.edu>
2016-08-12 16:51:29 -0400

    change load_stim.m

--- code/lab/load_stim.m
+++ code/lab/load_stim.m
@@ -1,6 +1,10 @@
 function datarun = load_stim(datarun,varargin) 
 %load obvious stimulus files 
 %greschner
+%sravi - 03-2014 - modified function to be compatible with newer version of
+                   %matlab (textscan function not reading the string in stimulus file - use fileread)
+%xyao - 08-2016 - modified function to be able to read BACK_RGB and RGB as
+                   %variable parameters
 
 % SET UP OPTIONAL ARGUMENTS
     p = inputParser;
@@ -10,24 +14,32 @@ function datarun = load_stim(datarun,varargin)
     p.addParamValue('trigger_iti_thr', 2);
     p.addParamValue('plot', 0);
     p.addParamValue('correction_incomplet_run', 1);
-
+    p.addParamValue('user_defined_trigger_interval', [], @isnumeric); % Caution, this might not work if frames were dropped
+    p.addParamValue('user_defined_trigger_set', [], @isnumeric);
+    
     p.parse(varargin{:});
     params = p.Results;
     
   
 
 clear s
-            fid=fopen(datarun.names.stimulus_path);
-            
-            file=textscan(fid, '%s');
-            fclose(fid);
+
+            %textscan modification here 
+            text = fileread(datarun.names.stimulus_path); %returns contents of file as matlab string
+            if(isspace(text(end)))
+                text(end) = [];  %removes space character at the end of stimulus files
+            end
+            file=textscan(text, '%s'); %read the string
             l=file{1};
 
-            %torse first line 
+            %parse first line, a.k.a header parameters
             t=1;
             j=2;
             while t
                 if ~isempty(strfind(l{j},')'));
+                        t=t-1;
+                end
+                if ~isempty(strfind(l{j},'))'));
                         t=t-1;
                 end
                 if ~isempty(strfind(l{j},'('));
@@ -117,26 +129,44 @@ clear s
                     s.trial_list(j)=length(s.combinations);
                 end
             end
-
-                       
+                                   
             t=fieldnames(s.combinations);
             for j=1:length(t)
                 if length(s.combinations(1).(t{j}))==1
                     s.params.(t{j})=sort(unique([s.combinations.(t{j})]));
                 else
-                    s.params.(t{j})=s.combinations(1).(t{j});
-                    tt=0;
-                    for jj=2:length(s.combinations)
-                        for jjj=1:size(s.params.(t{j}),1)  
-                            if all(s.combinations(jj).(t{j})(jjj,:)==s.params.(t{j}))
-                                tt=1;
-                            end
-                        end
-                        if ~tt
-                            s.combinations(jj).(t{j})(jj,:)=s.params.(t{j});
-                        end
+                    %gdf modified this 'else' statement: Martin's old code
+                    %is commented out below. 2013-04-22
+                    % get parameter values
+%                     temp_vals = zeros(1,length(s.combinations));
+%                     for cnd = 1:length(s.combinations);
+%                         temp_vals(cnd) = s.combinations(cnd).(t{j})(1);
+%                     end
+%                     temp_vals = sort(temp_vals, 'ascend');
+%                     s.params.(t{j}) = temp_vals;
+                        
+                    % martin's old code -- gdf doesn't think it does the
+                    % right thing
+%                     s.params.(t{j})=s.combinations(1).(t{j});
+%                     tt=0;
+%                     for jj=2:length(s.combinations)
+%                         for jjj=1:size(s.params.(t{j}),1)  
+%                             if all(s.combinations(jj).(t{j})(jjj,:)==s.params.(t{j}))
+%                                 tt=1;
+%                             end
+%                         end
+%                         if ~tt
+%                             s.combinations(jj).(t{j})(jj,:)=s.params.(t{j});
+%                         end
+%                     end
+                    % xy modified the else statement. Works for rgb and
+                    % back_rgb
+                    for cnd = 1:length(s.combinations)
+                        temp(cnd,:) = s.combinations(cnd).(t{j});
                     end
-                end
+                    s.params.(t{j}) = unique(temp, 'rows');
+                    s.params.(t{j}) = mat2cell(s.params.(t{j}), ones(size(s.params.(t{j}), 1), 1), size(s.params.(t{j}), 2))';
+                 end
             end
             
             s.repetitions=length(s.trials)/length(s.combinations);
@@ -157,14 +187,21 @@ end
 disp(sprintf('load_stim: %d stimulus combinations   %d trials   %d repetitions',length(s.combinations),length(s.trials),s.repetitions));
 disp(s.params)
 
+if ~isempty(p.Results.user_defined_trigger_set);
+    triggers = datarun.triggers(p.Results.user_defined_trigger_set);
+elseif ~isempty(p.Results.user_defined_trigger_interval)
+    % define new stimulus trigger interval
+    stim_interval = p.Results.user_defined_trigger_interval; % units are seconds
+    % find the triggers that are closest to occuring subsequent to these intervals
+    stim_trigger_indices = find(mod(datarun.triggers,stim_interval) < 0.1);
+    % set the triggers
+    triggers = datarun.triggers(stim_trigger_indices)';
 
-
-
-
-%trigger%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-if params.find_trigger
+    %trigger%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+elseif params.find_trigger && isempty(p.Results.user_defined_trigger_interval)
     %finds change in inter trigger interval
         tt=datarun.triggers(2:end)-datarun.triggers(1:end-1);
+        
         thr=std(tt)*params.trigger_iti_thr;
         ttt=datarun.triggers(1);
         alt=tt(1);
@@ -218,20 +255,3 @@ end
     datarun.stimulus.(t{i})=getfield(s,t{i});
  end
                
-
- 
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
